90329b9bd51c1328a72463d472874608cb22772c
jhardoin <jacob_ardoin@icloud.com>
2023-06-09 16:04:27 -0500

    Sum without the highest and lowest

---
+++ kyu8/Sum without the highest and lowest.js
@@ -0,0 +1,12 @@
+function sumArray(array) {
+    if (array == null) return 0;
+    if (array.length <= 2) return 0;
+    else {
+        array = array.sort(function(a,b) {return a - b;});
+        let total = 0;
+        for (let i = 1; i < array.length - 1; i++) {
+            total += array[i];
+        }
+        return total;
+    }
+}
