a04ee3e3058ae4aa5840554a0986df27ea43a5fc
jhardoin <jacob_ardoin@icloud.com>
2023-06-26 14:02:22 -0500

    L1 Bartender Drinks

---
+++ kyu8/L1 Bartender Drinks.js
@@ -0,0 +1,9 @@
+function getDrinkByProfession(param){
+    if(param.toLowerCase() === "jabroni"	) return "Patron Tequila";
+    if(param.toLowerCase() === "school counselor"	) return "Anything with Alcohol";
+    if(param.toLowerCase() === "programmer"	) return "Hipster Craft Beer";
+    if(param.toLowerCase() === "bike gang member"	) return "Moonshine";
+    if(param.toLowerCase() === "politician"	) return "Your tax dollars";
+    if(param.toLowerCase() === "rapper")	 return "Cristal";
+    else return "Beer"
+  }
