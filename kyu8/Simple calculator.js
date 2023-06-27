2889b7ce9f82c63903f5a321f43955feca08abef
jhardoin <jacob_ardoin@icloud.com>
2023-06-27 13:26:09 -0500

    Simple Calculator

---
+++ kyu8/Simple calculator.js
@@ -0,0 +1,7 @@
+function calculator(a,b,sign){
+    if(sign == '+')  return a + b;
+    if(sign == '-')  return a - b;
+    if(sign == '*')  return a * b;
+    if(sign == '/')  return a / b;
+    else return  "unknown value";
+}
