8f4b1a30264268601331506ca6db1d7b3893deca
jhardoin <jacob_ardoin@icloud.com>
2023-07-14 13:38:18 -0500

    Implement array prototype filter

---
+++ kyu8/Implement array prototype filter.js
@@ -0,0 +1,11 @@
+Array.prototype.filter = function (func) {
+    let result = []
+    
+    for(let i = 0; i < this.length; i++){
+      if(func(this[i])){
+        result.push(this[i])
+      }
+    }
+    return result
+  }
+  
