8ad704db5fa73db3020ea09b14abd74da3acf15a
jhardoin <jacob_ardoin@icloud.com>
2023-07-30 14:21:09 -0500

    The Coupon Code

---
+++ kyu7/The Coupon Code.js
@@ -0,0 +1,4 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    if (enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)) return true
+    else return false
+}
