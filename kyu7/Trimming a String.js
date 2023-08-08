7100f37f159a7e7a46fc9249763470cdfd3437fe
jhardoin <jacob_ardoin@icloud.com>
2023-08-08 17:01:42 -0500

    Trimming a String

--- kyu7/Trimming a String.js
+++ kyu7/Trimming a String.js
@@ -1,2 +1,2 @@
 const trim = (str, size) =>
-  str.length <= size ? str : str.slice(0, size - (size > 3 ? 3 : 0)) + '...';
+  str.length <= size ? str : str.slice(0, size - (size > 3 ? 3 : 0)) + '...'
