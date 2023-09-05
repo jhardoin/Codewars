1e819290800a5a747373f52399b3a0d6144cd3ce
jhardoin <jacob_ardoin@icloud.com>
2023-09-04 20:16:47 -0500

    A needle in the haystack

--- kyu8/A Needle in the Haystack.js
+++ kyu8/A Needle in the Haystack.js
@@ -2,3 +2,8 @@ function findNeedle(haystack) {
     let needle = haystack.indexOf('needle');
     return `found the needle at position ${needle}`
   }
+
+  function findNeedle(haystack) {
+    let position = haystack.indexOf('needle')
+    return `found the needle at position ${position}`
+  }
