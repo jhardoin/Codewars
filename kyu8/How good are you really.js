9ec0f6c8f335a97379b71ce91a019c61ce60fcd2
jhardoin <jacob_ardoin@icloud.com>
2023-09-05 17:48:57 -0500

    How good are you really

--- kyu8/How good are you really.js
+++ kyu8/How good are you really.js
@@ -2,3 +2,5 @@ function betterThanAverage(classPoints, yourPoints) {
     let avgClass = (classPoints.reduce((a, b) => a + b))/classPoints.length;
     return (yourPoints > avgClass) ? true : false;
     }
+
+    const betterThanAverage = (cP, yP) => (cP.reduce((a,b) => a + b) + yP) / cP.length <= yP
