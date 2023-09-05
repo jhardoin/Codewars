5ef1b3f6228877f652b456f0c43d1bf10335320c
jhardoin <jacob_ardoin@icloud.com>
2023-09-05 17:19:27 -0500

    Calculate Average

--- kyu8/Calculate Average.js
+++ kyu8/Calculate Average.js
@@ -6,3 +6,5 @@ function findAverage(array) {
       return (array.reduce((acc, cur) => acc + cur))/array.length;
     }
   }
+
+  const findAverage = array => array.length == 0 ? 0 : array.reduce((a,b) => a + b, 0)/array.length
