65ab34e9c58abe6ed1117b38271deefa2fb7dca4
jhardoin <jacob_ardoin@icloud.com>
2023-09-05 17:55:07 -0500

    Fake Binary

--- kyu8/Fake Binary.js
+++ kyu8/Fake Binary.js
@@ -1,3 +1,12 @@
 function fakeBin(x){
     return x.split('').map(item => item >= 5 ? 1 : 0).join('');
   }
+
+  function fakeBin(x){
+    let result = []
+    for(let i = 0; i < x.length; i++){
+      if(x[i] < 5) result.push(0)
+      else result.push(1)
+    }
+    return result.join('')
+  }
