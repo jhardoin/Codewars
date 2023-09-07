6a6e1e87e065670d925b5fe2918b2956fef8ebaa
jhardoin <jacob_ardoin@icloud.com>
2023-09-06 20:49:18 -0500

    Count by X

--- kyu8/Count by X.js
+++ kyu8/Count by X.js
@@ -4,3 +4,11 @@ function countBy(x, n) {
       z.push(x * i);
     return z;
   }
+
+  function countBy(x, n) {
+    let z = []
+    for(let i = x; z.length < n; i += x){
+      z.push(i)
+    }
+    return z;
+  }
