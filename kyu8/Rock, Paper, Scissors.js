208465711c452316ddfd5bf2f044f3b0ce8125d2
jhardoin <jacob_ardoin@icloud.com>
2023-09-07 20:54:30 -0500

    rps

--- kyu8/Rock, Paper, Scissors.js
+++ kyu8/Rock, Paper, Scissors.js
@@ -9,3 +9,5 @@ const rps = (p1, p2) => {
     return "Draw!"
   }
 };
+
+const rps = (p1, p2) => p1 == p2 ? 'Draw!' : (p1 == 'scissors' && p2 == 'paper') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'rock' && p2 == 'scissors') ? 'Player 1 won!' : 'Player 2 won!'
