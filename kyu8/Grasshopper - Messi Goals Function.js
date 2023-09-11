6d1a805819c06c3bca946b180bee45c6599eaf4f
jhardoin <jacob_ardoin@icloud.com>
2023-09-10 20:04:51 -0500

    Grasshopper

--- kyu8/Grasshopper - Messi Goals Function.js
+++ kyu8/Grasshopper - Messi Goals Function.js
@@ -1,3 +1,5 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
+
+  const goals = (la, co, cl) =>  la + co + cl
