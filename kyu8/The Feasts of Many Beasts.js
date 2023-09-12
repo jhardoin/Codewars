2d6662d9b7d2433432c5bdf60d6e016dd60b063a
jhardoin <jacob_ardoin@icloud.com>
2023-09-11 21:19:40 -0500

    Feast of many beasts

--- kyu8/The Feasts of Many Beasts.js
+++ kyu8/The Feasts of Many Beasts.js
@@ -1,1 +1,3 @@
 const feast = (beast, dish) => beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length -1]
+
+const feast = (b, d) => (b[0] == d[0]) && (b[b.length - 1] == d[d.length - 1])
