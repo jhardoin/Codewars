63984a46a2599242c4d04715bd03d0ca63c62d20
jhardoin <jacob_ardoin@icloud.com>
2023-09-12 18:07:18 -0500

    Switch it up

--- kyu8/Switch it up.js
+++ kyu8/Switch it up.js
@@ -31,3 +31,8 @@ function switchItUp(number){
           return 'Zero';
     }
   }
+
+  function switchItUp(number){
+    let nums = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}
+    return nums[number]
+}
