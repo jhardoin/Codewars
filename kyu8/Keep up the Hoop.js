9dbf1513b9a79ed120273cb461b12bf18892a61e
jhardoin <jacob_ardoin@icloud.com>
2023-09-13 17:11:45 -0500

    Keep up the hoop

--- kyu8/Keep up the Hoop.js
+++ kyu8/Keep up the Hoop.js
@@ -1,1 +1,5 @@
 let hoopCount = (n) => (n >= 10) ? "Great, now move on to tricks" : "Keep at it until you get it"
+
+function hoopCount (n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+ }
