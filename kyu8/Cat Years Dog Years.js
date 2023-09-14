5636275f57dad11ccb866995a202932cf8baccc7
jhardoin <jacob_ardoin@icloud.com>
2023-09-14 14:18:14 -0500

    Cat Years dog years

--- kyu8/Cat Years Dog Years.js
+++ kyu8/Cat Years Dog Years.js
@@ -3,3 +3,9 @@ const humanYearsCatYearsDogYears = (humanYears) => {
     if (humanYears === 2) return [humanYears, 24, 24]
     if (humanYears > 2) return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5]
     }
+
+const humanYearsCatYearsDogYears = humanYears => {
+    let catYears = (humanYears > 0 ? 15 : 0) + (humanYears > 1 ? 9 : 0) + (humanYears > 2 ? (humanYears - 2) * 4 : 0)
+    let dogYears = (humanYears > 0 ? 15 : 0) + (humanYears > 1 ? 9 : 0) + (humanYears > 2 ? (humanYears - 2) * 5 : 0)
+    return [humanYears, catYears, dogYears]
+      }
