04b7253d547bf9fc22da74fbcf03e2375460754a
jhardoin <jacob_ardoin@icloud.com>
2023-09-14 14:51:50 -0500

    Powers of 2

--- kyu8/Powers of 2.js
+++ kyu8/Powers of 2.js
@@ -5,3 +5,13 @@ function powersOfTwo(n){
     }
     return newArry
   }
+
+  function powersOfTwo(n){
+    let result = []
+    let counter = 1
+    for(let i = 0; i < n + 1; i++){
+      result.push(counter)
+      counter *= 2
+    }
+    return result
+  }
