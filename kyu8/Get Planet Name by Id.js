f3a1db3d13677ee31b39cf541a86c05cfdb78a15
jhardoin <jacob_ardoin@icloud.com>
2023-09-14 13:48:02 -0500

    Get planet name by id

--- kyu8/Get Planet Name by Id.js
+++ kyu8/Get Planet Name by Id.js
@@ -11,3 +11,16 @@ function getPlanetName(id){
       case 8: return name = 'Neptune'
     }
   }
+
+function getPlanetName(id){
+  switch(id){
+    case 1: return 'Mercury'
+    case 2: return 'Venus'
+    case 3: return 'Earth'
+    case 4: return 'Mars'
+    case 5: return 'Jupiter'
+    case 6: return 'Saturn'
+    case 7: return 'Uranus'
+    case 8: return 'Neptune'
+  }
+}
