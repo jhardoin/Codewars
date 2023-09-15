02946792603d170db8a19256da0fca2595786015
jhardoin <jacob_ardoin@icloud.com>
2023-09-15 13:51:24 -0500

    Is it a pallindrome

--- kyu8/Is it a pallindrome.js
+++ kyu8/Is it a pallindrome.js
@@ -1,1 +1,3 @@
 const isPalindrome = (x) => x.toLowerCase() === x.toLowerCase().split('').reverse().join('')
+
+const isPalindrome = x => x.toLowerCase() == x.split('').reverse().join('').toLowerCase()
