3fcf3e06db099cd04eb778311948612f16d68f70
jhardoin <jacob_ardoin@icloud.com>
2023-09-15 13:44:12 -0500

    Correct the mistakes of the character

--- kyu8/Correct the mistakes of the character recognition software.js
+++ kyu8/Correct the mistakes of the character recognition software.js
@@ -3,3 +3,12 @@ function correct(string) {
                  .replace(/0/g, 'O')
                  .replace(/1/g, 'I')
 }
+
+function correct(string){
+    let arr = string.split('')
+      let result = []
+    for(let i = 0; i < arr.length; i++){
+      arr[i] === '5' ? result.push('S') : arr[i] === '0' ? result.push('O') : arr[i] === '1' ? result.push('I') : result.push(arr[i])
+    }
+    return result.join('')
+  }
