e8bed32a4d1c293bcf2d6b46162a64c7a721406c
jhardoin <jacob_ardoin@icloud.com>
2023-09-17 15:34:34 -0500

    I love a little

--- kyu8/I love a little, alot, passionately.js
+++ kyu8/I love a little, alot, passionately.js
@@ -1,3 +1,8 @@
 const phrases = [ "I love you", "a little", "a lot", "passionately", "madly", "not at all",]
 
 const howMuchILoveYou = (nbPetals) => phrases[((nbPetals - 1) % 6)] 
+
+function howMuchILoveYou(nbPetals) {
+    let response = { 1 : "I love you", 2 : "a little", 3 : "a lot", 4 : "passionately", 5 : "madly", 0 : "not at all"}
+    return response[nbPetals % 6]
+}
