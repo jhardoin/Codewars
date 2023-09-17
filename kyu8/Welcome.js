16a1e8e248df5d727aafe683bc3fbc4e33e9acff
jhardoin <jacob_ardoin@icloud.com>
2023-09-16 19:50:24 -0500

    Welcome

--- kyu8/Welcome.js
+++ kyu8/Welcome.js
@@ -20,3 +20,26 @@ function greet(language) {
       
      return (greetings[language] !== undefined) ? greetings[language] : greetings['english']
     }
+
+    function greet(language) {
+        let wel = {
+     "english": "Welcome"
+    ,"czech": "Vitejte"
+    ,"danish": "Velkomst"
+    ,"dutch": "Welkom"
+    ,"estonian": "Tere tulemast"
+    ,"finnish": "Tervetuloa"
+    ,"flemish": "Welgekomen"
+    ,"french": "Bienvenue"
+    ,"german": "Willkommen"
+    ,"irish": "Failte"
+    ,"italian": "Benvenuto"
+    ,"latvian": "Gaidits"
+    ,"lithuanian": "Laukiamas"
+    ,"polish": "Witamy"
+    ,"spanish": "Bienvenido"
+    ,"swedish": "Valkommen"
+    ,"welsh": "Croeso"
+      }
+    return wel.hasOwnProperty(language)  ? wel[language] : wel["english"]
+    }
