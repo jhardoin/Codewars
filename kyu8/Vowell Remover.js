44044d4e0e99e84d569c9b4be3bde516824ce37e
jhardoin <jacob_ardoin@icloud.com>
2023-09-18 19:30:03 -0500

    Vowell Remover

--- kyu8/Vowell Remover.js
+++ kyu8/Vowell Remover.js
@@ -1,3 +1,12 @@
 function shortcut (string) {
     return string.replace(/[aeiou]/gi, '')
   }
+
+  function shortcut (string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let result = []
+    for(let i = 0; i < string.length; i++){
+      if(!vowels.includes(string[i])) result.push(string[i])
+    }
+    return result.join('')
+  }
