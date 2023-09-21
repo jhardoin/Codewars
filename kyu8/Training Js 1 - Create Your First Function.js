006c057d922bbfec5db14b10f467da9c1455b224
jhardoin <jacob_ardoin@icloud.com>
2023-09-20 20:50:35 -0500

    Training JS 1

--- kyu8/Training Js 1 - Create Your First Function.js
+++ kyu8/Training Js 1 - Create Your First Function.js
@@ -2,3 +2,8 @@ const helloWorld = (somewords) => {
     let str = "Hello World!";
     console.log(str)
     }
+
+    function helloWorld(){
+        let str = 'Hello World!'
+        console.log(str)
+      }
