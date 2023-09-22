9589a5022d6cffc7b1fcbf1173d844665bb6db53
jhardoin <jacob_ardoin@icloud.com>
2023-09-21 20:02:58 -0500

    Lario and Muigi

--- kyu8/Lario and Muigi pipe problem.js
+++ kyu8/Lario and Muigi pipe problem.js
@@ -4,3 +4,11 @@ function pipeFix(numbers){
       newNumbers.push(i);
     return newNumbers;
   }
+
+  function pipeFix(numbers){
+    let result = []
+    for(let i = numbers[0]; i <= numbers[numbers.length - 1]; i++){
+      result.push(i)
+    }
+    return result
+  }
