09e0ec6ee3dfe175b17eabfe606fe647f25d61ed
jhardoin <jacob_ardoin@icloud.com>
2023-09-22 22:16:34 -0500

    A wolf in sheeps clothing

--- kyu8/A wolf in sheeps clothing.js
+++ kyu8/A wolf in sheeps clothing.js
@@ -2,3 +2,9 @@ function warnTheSheep(queue) {
     let n = queue.reverse().indexOf('wolf')
     return (n < 1) ?"Pls go away and stop eating my sheep" : `Oi! Sheep number ${n}! You are about to be eaten by a wolf!`;
   }
+
+  function warnTheSheep(queue) {
+    let num = queue.reverse().indexOf('wolf')
+      return num > 0 ? `Oi! Sheep number ${num}! You are about to be eaten by a wolf!`:
+      "Pls go away and stop eating my sheep"
+  }
