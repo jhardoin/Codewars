4ec99913f6fd2e628684f2d6fdc66e96f9695904
jhardoin <jacob_ardoin@icloud.com>
2023-09-23 22:00:37 -0500

    Well of ideas

--- kyu8/Well of ideas.js
+++ kyu8/Well of ideas.js
@@ -4,3 +4,8 @@ function well(x){
      if(numOfGoods > 0) return 'Publish!'
      else return 'Fail!'
    }
+
+   function well(x){
+    let filtered = x.filter(e=> e=='good').length
+    return filtered > 2 ? 'I smell a series!' : filtered > 0 ? 'Publish!' : 'Fail!'
+  }
