54d68c1b9758072a7ac7b3af600ea80c126e0b7e
jhardoin <jacob_ardoin@icloud.com>
2023-09-23 21:57:47 -0500

    Determine offspring sex

--- kyu8/Determine offspring sex.js
+++ kyu8/Determine offspring sex.js
@@ -2,3 +2,7 @@ function chromosomeCheck(sperm) {
     let sonOrDaughter = ((sperm == 'XX') ? 'daughter' : 'son');
     return `Congratulations! You're going to have a ${sonOrDaughter}.`
   }
+
+  function chromosomeCheck(sperm) {
+    return sperm == 'XY' ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter."
+  }
