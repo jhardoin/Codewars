7078b347b83454304ae4b627b4fc4b82ac777401
jhardoin <jacob_ardoin@icloud.com>
2023-09-25 14:10:24 -0500

    Multiplication table

--- kyu8/Multiplication table for number.js
+++ kyu8/Multiplication table for number.js
@@ -1,3 +1,11 @@
 function multiTable(number) {
     return [...Array(10)].map((element, index) => `${index + 1} * ${number} = ${(index + 1) * (number)}`).join('\n');
   }
+
+  function multiTable(number) {
+    let result = []
+    for(let i = 1; i <= 10; i++){
+    result.push(`${i} * ${number} = ${i * number}`)
+    }
+    return result.join('\n')
+  }
