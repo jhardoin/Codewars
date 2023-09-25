df2456e719baefe09f38a0958e421b8b5fb8154b
jhardoin <jacob_ardoin@icloud.com>
2023-09-25 14:37:37 -0500

    No zeros for heros

--- kyu8/No Zeros for Heros.js
+++ kyu8/No Zeros for Heros.js
@@ -1,1 +1,9 @@
 const noBoringZeros = (n) => n === 0 ? 0 : parseInt(n.toString().replace(/0+$/, ''))
+
+function noBoringZeros(n) {
+    let arr = [...n.toString()]
+    while(arr[arr.length - 1] == 0){
+      arr.pop()
+    }
+    return +arr.join('')
+  }
