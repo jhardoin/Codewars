e18475891b747bf2e39df5c5952b5af7472b79d2
jhardoin <jacob_ardoin@icloud.com>
2023-09-27 18:05:43 -0500

    Get the middle character

--- kyu7/Get the middle character.js
+++ kyu7/Get the middle character.js
@@ -7,3 +7,5 @@ function getMiddle(s) {
       return s[middleNum - 1] + s[middleNum]
     }
   }
+
+  const getMiddle = s => s.length % 2 ? (s[Math.floor(s.length/2) ]) : s[s.length/2 - 1] + s[s.length/2]
