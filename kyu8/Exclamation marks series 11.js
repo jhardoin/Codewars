8bfef10caa7b787b08a3530c21bf39f3247e6c53
jhardoin <jacob_ardoin@icloud.com>
2023-09-27 17:32:08 -0500

    Exlamation Marks series

--- kyu8/Exclamation marks series 11.js
+++ kyu8/Exclamation marks series 11.js
@@ -1,3 +1,8 @@
 function replace(s){
     return s.replace(/[aeiou]/gi, '!')
   }
+
+  function replace(s){
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    return [...s].map(e => vowels.includes(e.toLowerCase()) ? '!' : e).join('')
+  }
