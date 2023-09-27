023eef0717ffb1ed44aaa9506e7d97c1a09ec963
jhardoin <jacob_ardoin@icloud.com>
2023-09-26 19:39:21 -0500

    exclamation marks series 1

--- kyu8/Exclamation Marks Series 1.js
+++ kyu8/Exclamation Marks Series 1.js
@@ -1,1 +1,5 @@
 const remove = string => string.endsWith('!') ?  string.slice(0,-1) : string;
+
+function remove (string) {
+    return string[string.length - 1] == '!' ? string.split('').slice(0, string.length - 1).join('') : string
+  }
