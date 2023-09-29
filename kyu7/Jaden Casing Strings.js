ceac2ae6e251b2234cf67f3b5a91cc7440c7a4d4
jhardoin <jacob_ardoin@icloud.com>
2023-09-28 19:46:43 -0500

    Jaden Casing

--- kyu7/Jaden Casing Strings.js
+++ kyu7/Jaden Casing Strings.js
@@ -3,3 +3,7 @@ String.prototype.toJadenCase = function () {
                                   ( w[0].toUpperCase() + w.slice(1) ) );
     return arr.join(' ')
     }
+
+    String.prototype.toJadenCase = function () {
+        return this.split(' ').map(e => e[0].toUpperCase() + e.slice(1)).join(' ')
+      };
