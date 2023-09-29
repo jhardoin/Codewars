7136574465ece784ace5044d63f9771fe9303433
jhardoin <jacob_ardoin@icloud.com>
2023-09-28 19:49:12 -0500

    Exes and Ohs

--- kyu7/Exes and Ohs.js
+++ kyu7/Exes and Ohs.js
@@ -3,3 +3,7 @@ function XO(str) {
   
     return( (arrStr.filter(e => e === 'x').length) === (arrStr.filter(e => e === 'o').length) ) ? true : false
 }
+
+function XO(str) {
+    return [...str].filter(e => e.toLowerCase() == 'x').length == [...str].filter(e => e.toLowerCase() == 'o').length
+}
