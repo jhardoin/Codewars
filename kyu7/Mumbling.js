3b60ef6c3535290264f4ff5bc053870d8b4fc7ee
jhardoin <jacob_ardoin@icloud.com>
2023-09-28 20:04:43 -0500

    Mumbling

--- kyu7/Mumbling.js
+++ kyu7/Mumbling.js
@@ -7,3 +7,7 @@ function accum(s) {;
     let arrMult = newArr.split(',').filter(e => e.length > 0);
     return arrMult.map(e => e[0].toUpperCase() + e.slice(1)).join('-')
 }
+
+function accum(s) {
+	return [...s].map((e,i) => e.repeat(i + 1)).map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join('-')
+}
