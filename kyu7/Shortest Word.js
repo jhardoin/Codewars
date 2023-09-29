c456e0486062f8ebfbb7130b8e14c0c9ae9f95f4
jhardoin <jacob_ardoin@icloud.com>
2023-09-28 19:59:33 -0500

    shortest word

--- kyu7/Shortest Word.js
+++ kyu7/Shortest Word.js
@@ -2,3 +2,5 @@ function findShort(s){
     let numArry = s.split(' ').map(e => e.length);
     return Math.min(...numArry);
   }
+
+  const findShort = s => Math.min(...s.split(' ').map(e => e.length))
