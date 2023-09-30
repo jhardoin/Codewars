58d01fe16ebe26f983436dc7ac4dd7f63bbbdba9
jhardoin <jacob_ardoin@icloud.com>
2023-09-29 20:55:50 -0500

    Is this a triangle

--- kyu7/Is this a triange.js
+++ kyu7/Is this a triange.js
@@ -2,3 +2,5 @@ function isTriangle(a,b,c){
     let sortNums = [a, b, c].sort((a, b) => a - b);
      return sortNums[0] + sortNums[1] > sortNums[2];
    }
+
+   const isTriangle = (a,b,c) => a + b > c && a + c > b && b + c > a
