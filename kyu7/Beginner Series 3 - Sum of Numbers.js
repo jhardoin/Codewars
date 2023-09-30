151576a8ff0a64e24b6a23ee0b27a3aebe54a8b1
jhardoin <jacob_ardoin@icloud.com>
2023-09-29 20:46:26 -0500

    Beginner Series 3

--- kyu7/Beginner Series 3 - Sum of Numbers.js
+++ kyu7/Beginner Series 3 - Sum of Numbers.js
@@ -3,3 +3,11 @@ function getSum(a, b){
     let max = Math.max(a, b);
     return (max - min + 1) * (max + min) / 2
   }
+
+  function getSum(a, b){
+    let result = []
+    for(let i = Math.min(a, b); i <= Math.max(a, b); i++){
+      result.push(i)
+    }
+      return result.reduce((a,b) => a + b, 0)
+  }
