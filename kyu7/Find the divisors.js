e1204d4c9f623e04babf3ca7835515bb975c9a52
jhardoin <jacob_ardoin@icloud.com>
2023-10-02 20:01:52 -0500

    Find the divisors

--- kyu7/Find the divisors.js
+++ kyu7/Find the divisors.js
@@ -3,3 +3,8 @@ function divisors(integer) {
     let filtered = arr.filter(e => integer % e == 0 && e !== 1)
     return filtered.length > 1 ? filtered : `${integer} is prime`
   };
+
+  function divisors(integer) {
+    let arr = Array.from({length:integer - 2}, (v,i) => i + 2).filter(e => !(integer % e))
+    return arr.length > 0 ? arr : `${integer} is prime`
+  }
