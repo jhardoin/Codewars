3386b3e4eaf1092d7c314b9d15980a340f8d8f2e
jhardoin <jacob_ardoin@icloud.com>
2023-10-03 19:19:34 -0500

    Make a function that does arithmetic

--- kyu7/Make a function that does arithmetic.js
+++ kyu7/Make a function that does arithmetic.js
@@ -4,3 +4,7 @@ function arithmetic(a, b, operator){
     else if(operator == 'multiply') return a * b
     else return a / b
   }
+
+  function arithmetic(a, b, operator){
+    return (operator == 'add' ? a + b: operator == 'subtract' ? a - b : operator == 'multiply' ? a * b : a/b)
+  }
