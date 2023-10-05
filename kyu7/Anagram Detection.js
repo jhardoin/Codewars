b10f3b881b0a182170f6e01f2a5743fcf010e89e
jhardoin <jacob_ardoin@icloud.com>
2023-10-04 20:36:23 -0500

    anagram

--- kyu7/Anagram Detection.js
+++ kyu7/Anagram Detection.js
@@ -1,1 +1,3 @@
 const isAnagram = (test, original) => test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('')
+
+var isAnagram = (test, original) => test.toLowerCase().split('').sort().join('') == original.toLowerCase().split('').sort().join('')
