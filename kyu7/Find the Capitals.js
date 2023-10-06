9a77111fb6b1ed74e822e4964705e158742423b9
jhardoin <jacob_ardoin@icloud.com>
2023-10-05 20:33:24 -0500

    Find capitals

--- kyu7/Find the Capitals.js
+++ kyu7/Find the Capitals.js
@@ -3,3 +3,10 @@ var capitals = function (word) {
       word.split('').map((e, i) => e == e.toLowerCase() ? [] : nums.push(i))
     return nums
     }
+
+    var capitals = function (word) {
+      let cap = [];
+      [...word].map((e,i) => e == e.toUpperCase() ? cap.push(i) : '')
+      return cap
+                               
+    };
