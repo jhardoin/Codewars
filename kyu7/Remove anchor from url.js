1797cf74253636415d59be56a71bb2ed6c93751c
jhardoin <jacob_ardoin@icloud.com>
2023-10-05 20:24:36 -0500

    Remove anchor from url

--- kyu7/Remove anchor from url.js
+++ kyu7/Remove anchor from url.js
@@ -1,1 +1,11 @@
 const removeUrlAnchor = url => url.replace(/#.*/g, '')
+
+function removeUrlAnchor(url){
+    let arr = url.split('')
+    let arr2 = []
+    for(let i = 0; i < url.length; i++){
+      if(arr[i] == '#') return arr2.join('')
+      arr2.push(arr[i])
+    }
+    return arr2.join('')
+  }
