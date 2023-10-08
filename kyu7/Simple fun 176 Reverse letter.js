c10f4a2ff18acdf141a03d5917c2221e8bd25375
jhardoin <jacob_ardoin@icloud.com>
2023-10-07 20:42:03 -0500

    Simple fun 176

--- kyu7/Simple fun 176 Reverse letter.js
+++ kyu7/Simple fun 176 Reverse letter.js
@@ -1,1 +1,10 @@
 const reverseLetter = str => str.replace(/[^a-zA-Z]/g, '').split('').reverse().join('')
+
+function reverseLetter(str) {
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let arr = []
+    for(let i = 0; i < str.length; i++){
+      if(letters.includes(str[i])) arr.push(str[i])
+    }
+    return arr.reverse().join('')
+  }
