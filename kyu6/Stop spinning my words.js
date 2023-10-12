5917154ac626bf9804382a79b1f304faf7e57b6d
jhardoin <jacob_ardoin@icloud.com>
2023-10-11 23:08:10 -0500

    Stop spinning my words

--- kyu6/Stop spinning my words.js
+++ kyu6/Stop spinning my words.js
@@ -3,3 +3,5 @@ function spinWords(string){
     let mapped = arr.map(e => e.length >= 5 ? e.split('').reverse().join('') : e).join(' ')
     return mapped
   }
+
+  const spinWords = string => string.split(' ').map(e => e.length >= 5 ? [...e].reverse().join('') : e).join(' ')
