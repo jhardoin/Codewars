a70375a0bd59596adf431f3f11e3558e93c2513d
jhardoin <jacob_ardoin@icloud.com>
2023-10-13 21:35:03 -0500

    Find the parity outlier

--- kyu6/Find the Parity Outlier.js
+++ kyu6/Find the Parity Outlier.js
@@ -3,3 +3,7 @@ const findOutlier = (integers) => {
     let even = integers.filter(e => e % 2 == 0);
     return odd.length == 1 ? odd[0] : even[0]
   }
+
+  function findOutlier(integers){
+    return integers.filter(e => e % 2 == 0).length == 1 ? +integers.filter(e => e % 2 == 0) : +integers.filter(e => e % 2 !== 0) 
+}
