dc7d1ff30e8859d02568d39cbfa07c251583c841
jhardoin <jacob_ardoin@icloud.com>
2023-10-13 21:55:24 -0500

    Persistent bugger

--- kyu6/Persistent Bugger.js
+++ kyu6/Persistent Bugger.js
@@ -7,3 +7,12 @@ function persistence(num) {
   }
  return result
 }
+
+function persistence(num) {
+  let count = 0
+   while (num.toString().length > 1){
+     num = [...num.toString()].reduce((a,b) => +a * +b)
+     count++
+   }
+  return count
+}
