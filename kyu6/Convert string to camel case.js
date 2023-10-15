687840518aaf15205f38b9ac401b0806feab3f21
jhardoin <jacob_ardoin@icloud.com>
2023-10-14 22:23:40 -0500

    Convert string to camel case

--- kyu6/Convert string to camel case.js
+++ kyu6/Convert string to camel case.js
@@ -3,3 +3,9 @@ function toCamelCase(str){
     let camelArr = arr.map((e) => e == arr[0]  ? e : e[0].toUpperCase() + e.slice(1)) 
     return camelArr.join('')
   }
+
+  function toCamelCase(str){
+    if(str.length == 0) return ''
+    let j = str.replace(/[_-]/g, ' ').split(' ').map(e => e[0].toUpperCase() + e.slice(1)).join('')
+    return str[0] + j.slice(1)
+  }
