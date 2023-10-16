a917c38e18c99b3ed6003b85b18bd7f5a21389d7
jhardoin <jacob_ardoin@icloud.com>
2023-10-15 21:48:02 -0500

    Detect Panagram

--- kyu6/Detect Pangram.js
+++ kyu6/Detect Pangram.js
@@ -6,3 +6,8 @@ function isPangram(string){
   }
 
   const isPangram = s => Array.from(new Set(s.toLowerCase().replace(/[^a-z]/g, ''))).length == 26
+
+  const isPangram = s => {
+    let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+     return new Set([...s.split(' ').join('').toLowerCase()].filter(e => letters.includes(e))).size == 26
+  }
