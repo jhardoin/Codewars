67169b63671974c370a2cf1e44a878d649ed1f89
jhardoin <jacob_ardoin@icloud.com>
2023-10-15 22:02:23 -0500

    Your order

--- kyu6/Your order, please.js
+++ kyu6/Your order, please.js
@@ -3,3 +3,9 @@ function order(words) {
                 .sort((a, b) => /\d/.exec(a) - /\d/.exec(b))
                 .join(' ');
   }
+
+  function order(words){
+    let nums = '1234567890'
+    let order = words.split(' ').map(e => [...e].filter(e => nums.includes(...e))).flat()
+    return order.map((e,i) => [e, words.split(' ')[i]]).sort().map(e => e[1]).join(' ')
+  }
