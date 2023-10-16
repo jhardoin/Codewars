78549c115e1152794a4f6331e80872a2f9b57c5a
jhardoin <jacob_ardoin@icloud.com>
2023-10-15 22:38:18 -0500

    Find unique

--- kyu6/Find the unique number.js
+++ kyu6/Find the unique number.js
@@ -3,3 +3,8 @@ function findUniq(arr) {
     return (arr[0] !== arr[1]) ? arr[0] : arr[arr.length - 1]
   }
   
+
+  function findUniq(arr) {
+    arr.sort((a, b) => a - b);
+    return arr[0] !== arr[1] ? arr[0] : arr[arr.length - 1]
+  }
