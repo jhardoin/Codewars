76aa466a49ee469a9d32c74813e3b7a2a2968277
jhardoin <jacob_ardoin@icloud.com>
2023-10-15 22:21:15 -0500

    Split Strings

--- kyu6/Split Strings.js
+++ kyu6/Split Strings.js
@@ -7,3 +7,11 @@ function solution(str){
    return result.map(e => e.length < 2 ? e + '_' : e)
    
  }
+
+ function solution(str){
+  let result = []
+  for(let i = 0; i < str.length; i += 2){
+    result.push(str[i] + (str[i + 1] == undefined ? '_' : str[i + 1]))
+  }
+return result
+}
