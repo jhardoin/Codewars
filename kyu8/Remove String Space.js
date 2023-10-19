f76d06b91bdc28b7d75607219b876144dff0b8fd
jhardoin <jacob_ardoin@icloud.com>
2023-10-18 20:31:43 -0500

    Remove string space

--- kyu8/Remove String Space.js
+++ kyu8/Remove String Space.js
@@ -3,3 +3,11 @@ function noSpace(x){
    }
 
    const noSpace = x => x.split(' ').join('')
+
+   function noSpace(x){
+    let arr = []
+    for(let i = 0; i < x.length; i++){
+      if(x[i] !== ' ') arr.push(x[i])
+    }
+    return arr.join('')
+  }
