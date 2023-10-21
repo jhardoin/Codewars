c94064cdc9060b7b64880f8bf60f2d16bae61bd3
jhardoin <jacob_ardoin@icloud.com>
2023-10-20 20:08:38 -0500

    Abbrieviate a two word name

--- kyu8/Abbrieviate a Two Word Name.js
+++ kyu8/Abbrieviate a Two Word Name.js
@@ -6,3 +6,9 @@ function abbrevName(name){
 
 
 const abbrevName = n => n.toUpperCase().split(' ')[0][0] + '.' + n.toUpperCase().split(' ')[1][0]
+
+function abbrevName(name){
+    let n = name.split(' ')
+    return n[0][0].toUpperCase() + '.' + n[1][0].toUpperCase()
+}
+
