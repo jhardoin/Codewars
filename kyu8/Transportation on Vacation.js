a209b5cf22e807fdcc0b2474da04195d46f79182
jhardoin <jacob_ardoin@icloud.com>
2023-10-25 09:39:32 -0500

    Transportation on Vacation

--- kyu8/Transportation on Vacation.js
+++ kyu8/Transportation on Vacation.js
@@ -11,3 +11,5 @@ function rentalCarCost(d) {
   }
 
   const rentalCarCost = d => d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40
+
+  const rentalCarCost = d => d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40
