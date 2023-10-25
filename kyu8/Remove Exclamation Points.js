7d661800ff57b0c2feb4588e1e626742bb015fd7
jhardoin <jacob_ardoin@icloud.com>
2023-10-25 09:44:10 -0500

    Remove Exclamation Points

--- kyu8/Remove Exclamation Points.js
+++ kyu8/Remove Exclamation Points.js
@@ -3,3 +3,11 @@ function removeExclamationMarks(s) {
 }
 
 const removeExclamationMarks = s => [...s].filter(e => e !== '!').join('')
+
+function removeExclamationMarks(s) {
+  let arr = []
+  for(let i = 0; i < s.length; i++){
+    if(s[i] !== '!') arr.push(s[i])
+  }
+  return arr.join('')
+}
