450ea1a92bd0c02e2a37bd6b09f0a9a42915882e
jhardoin <jacob_ardoin@icloud.com>
2023-10-26 13:09:43 -0500

    Grasshopper

--- kyu8/Grasshopper - Grade Book.js
+++ kyu8/Grasshopper - Grade Book.js
@@ -11,3 +11,8 @@ function getGrade (s1, s2, s3) {
   let av = (s1 + s2 + s3)/3
   return av >= 90 ? 'A' : av >= 80 ? 'B' : av >= 70 ? 'C' : av >= 60 ? 'D' : 'F'
 }
+
+function getGrade (s1, s2, s3) {
+  let x = (s1+s2+s3)/3
+  return x >= 90 ? 'A' : x >= 80 ? 'B' : x >= 70 ? 'C' : x >= 60 ? 'D' : 'F'
+}
