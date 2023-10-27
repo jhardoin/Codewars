44cd8cb77edd6bca19304dcdb1498950c4aac834
jhardoin <jacob_ardoin@icloud.com>
2023-10-27 17:11:24 -0500

    Array plus array

--- kyu8/Array plus Array.js
+++ kyu8/Array plus Array.js
@@ -3,3 +3,5 @@ function arrayPlusArray(arr1, arr2) {
   }
 
   const arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((a,b) => a + b,0)
+
+  const arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((a,b) => a + b, 0)
