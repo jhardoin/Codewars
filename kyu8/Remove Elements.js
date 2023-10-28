7242417f1d928ba181f8381150477f8260c11b19
jhardoin <jacob_ardoin@icloud.com>
2023-10-28 12:47:30 -0500

    Remove elements

--- kyu8/Remove Elements.js
+++ kyu8/Remove Elements.js
@@ -1,3 +1,5 @@
 const removeEveryOther = (arr) => arr.filter((item, index) => index % 2 === 0)
 
 const removeEveryOther = arr => arr.filter((e,i) => !(i % 2))
+
+const removeEveryOther = arr => arr.filter((e,i) => !(i % 2))
