fbe927bb82969259c934a4a5ed58a5c9f9f03d64
jhardoin <jacob_ardoin@icloud.com>
2023-10-28 12:52:12 -0500

    Will there be enough space

--- kyu8/Will there be enough space.js
+++ kyu8/Will there be enough space.js
@@ -1,3 +1,5 @@
 const enough = (cap, on, wait) => (on + wait >= cap) ? (on + wait) - cap : 0
 
 const enough = (cap, on, wait) => (cap - on - wait) > 0 ? 0 : Math.abs(cap-on-wait)
+
+const enough = (cap, on, wait) => on + wait <= cap ? 0 : Math.abs(cap - on - wait)
