90a7752cc3d582a48e2bcfa994742ae7f84d5692
jhardoin <jacob_ardoin@icloud.com>
2023-10-29 13:50:55 -0500

    Find the first

--- kyu8/Find the first non-consecutive number.js
+++ kyu8/Find the first non-consecutive number.js
@@ -13,3 +13,10 @@ function firstNonConsecutive (arr) {
     }
     return null
   }
+
+  function firstNonConsecutive (arr) {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i + 1] - arr[i] > 1) return arr[i + 1]
+    }
+    return null
+  }
