0b74d5f18358af799328d18ca94d8b058d5ca021
jhardoin <jacob_ardoin@icloud.com>
2023-10-31 13:30:52 -0500

    Grasshopper

--- kyu8/Grasshopper - Messi Goals.js
+++ kyu8/Grasshopper - Messi Goals.js
@@ -3,3 +3,6 @@ var championsLeagueGoals = 10;
 var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+var laLigaGoals = 43, championsLeagueGoals = 10, copaDelReyGoals = 5
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
