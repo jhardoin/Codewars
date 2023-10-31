117396a3316037041afc97292f8aa784c41a9795
jhardoin <jacob_ardoin@icloud.com>
2023-10-31 13:44:32 -0500

    Difference of Volumes of Cuboids

--- kyu8/Difference of Volumes of Cuboids.js
+++ kyu8/Difference of Volumes of Cuboids.js
@@ -4,3 +4,5 @@ function findDifference(a, b) {
     let reduced = [a.reduce((a,b) => a * b), b.reduce((a,b) => a*b)]
     return Math.max(...reduced) - Math.min(...reduced)
   }
+
+  const findDifference = (a, b) => Math.abs((a.reduce((a,b) => a * b)) - (b.reduce((a,b) => a * b)))
