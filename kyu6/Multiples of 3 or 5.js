dd99489f9914e46d490c71b2593956b6780d0734
jhardoin <jacob_ardoin@icloud.com>
2023-11-01 14:54:46 -0500

    Multiples of 3 or 5

--- kyu6/Multiples of 3 or 5.js
+++ kyu6/Multiples of 3 or 5.js
@@ -1,1 +1,7 @@
 const solution = n => Array.from({length:n - 1}, (e,i) => i + 1).filter(e => !(e % 3) || !(e % 5)).reduce((a,b)=> a + b, 0)
+
+function solution(number){
+    return Array.from({length:number - 1}, (_,i) => i + 1)
+      .filter(e => !(e % 3) || !(e % 5))
+      .reduce((a,b) => a + b, 0)
+  }
