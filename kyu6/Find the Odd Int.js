0d671a8b0c1cb321cdb044ac261e0708152dfb65
jhardoin <jacob_ardoin@icloud.com>
2023-11-01 15:14:35 -0500

    Find the odd int

--- kyu6/Find the Odd Int.js
+++ kyu6/Find the Odd Int.js
@@ -1,1 +1,9 @@
 const findOdd = (A) => A.reduce((a, b) => a ^ b)
+
+function findOdd(A) {
+    let reduced = A.reduce((a, b) => {
+      a[b] = (a[b] || 0) + 1
+      return a;
+    }, {})
+    return +Object.entries(reduced).filter(e => e[1] % 2)[0][0]
+  }
