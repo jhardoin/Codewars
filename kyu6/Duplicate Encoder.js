b86fb9e8fbfd2a782790636fdbad5295600a0bb0
jhardoin <jacob_ardoin@icloud.com>
2023-11-02 17:08:45 -0500

    Duplicate encoder

--- kyu6/Duplicate Encoder.js
+++ kyu6/Duplicate Encoder.js
@@ -1,1 +1,3 @@
 const duplicateEncode = word => [...word.toLowerCase()].map((e,i,a) => a.indexOf(e) == a.lastIndexOf(e) ? '(' : ')').join('')
+
+const duplicateEncode = word => [...word.toLowerCase()].map((e,i,a) => a.indexOf(e) == a.lastIndexOf(e) ? '(' : ')').join('')
