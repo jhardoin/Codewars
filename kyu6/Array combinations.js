d0d8426d5c4a0f7e39796f97d895c76f3ed8ffb1
jhardoin <jacob_ardoin@icloud.com>
2023-11-06 17:13:05 -0600

    Array combinations

--- kyu6/Array combinations.js
+++ kyu6/Array combinations.js
@@ -1,3 +1,5 @@
 const solve = arr => arr.map(e => [...new Set(e)].length).reduce((a,b) => a * b)
 
 const solve = arr => arr.map(e => new Set(e).size).reduce((a,b) => a * b, 1)
+
+const solve = arr => arr.map(e => new Set(e).size).reduce((a,b) => a * b)
