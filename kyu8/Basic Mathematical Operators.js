41c437bc0e7db008832961870b8ad95abcbb2dd6
jhardoin <jacob_ardoin@icloud.com>
2023-11-13 16:10:10 -0600

    Basic Mathematical Operators

--- kyu8/Basic Mathematical Operators.js
+++ kyu8/Basic Mathematical Operators.js
@@ -5,3 +5,5 @@ function basicOp(operation, value1, value2) {
   function basicOp(operation, value1, value2){
     return operation == '+' ? value1 + value2 : operation == '-' ? value1 - value2 : operation == '*' ? value1 * value2 : value1 / value2
   }
+
+  const basicOp = (operation, value1, value2) => eval(value1+operation+value2)
