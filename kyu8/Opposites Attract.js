778786b05977ac2362d7930b6d1786998ffbf1bf
jhardoin <jacob_ardoin@icloud.com>
2023-11-14 15:52:05 -0600

    Opposites Attract

--- kyu8/Opposites Attract.js
+++ kyu8/Opposites Attract.js
@@ -10,3 +10,5 @@ function lovefunc(flower1, flower2){
   }
 
   const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2
+
+  const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2
