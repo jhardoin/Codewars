03093bac7801b03812addc45bdcb7b5a2d67d468
jhardoin <jacob_ardoin@icloud.com>
2023-11-16 16:14:45 -0600

    Are you playing banjo

--- kyu8/Are you playing Banjo.js
+++ kyu8/Are you playing Banjo.js
@@ -15,3 +15,7 @@ function areYouPlayingBanjo(name) {
   }
 
   const areYouPlayingBanjo = name => name.toLowerCase().startsWith('r') ? `${name} plays banjo` : `${name} does not play banjo`
+
+  function areYouPlayingBanjo(name) {
+    return name.toLowerCase()[0] == 'r' ? `${name} plays banjo` : `${name} does not play banjo`
+  }
