c768d10a559d6d0640c30a1d9b89c23e0d9949b3
jhardoin <jacob_ardoin@icloud.com>
2023-11-17 13:53:20 -0600

    Find maximum and minimum values of a list

--- kyu8/Find maximum and minimum values of a list.js
+++ kyu8/Find maximum and minimum values of a list.js
@@ -3,3 +3,10 @@ var max = list => Math.max(...list)
 
 const min = list => Math.min(...list)
 const max = list => Math.max(...list)
+
+var min = function(list){
+    return Math.min(...list);
+}
+var max = function(list){
+    return Math.max(...list);
+}
