a83b3be8b8a7171635a2e820fe57ffa62e4a072a
jhardoin <jacob_ardoin@icloud.com>
2023-11-19 18:04:06 -0600

    Is n divisible by x and y

--- kyu8/Is n divisible by x and y.js
+++ kyu8/Is n divisible by x and y.js
@@ -8,3 +8,5 @@ function isDivisible(n, x, y) {
   const isDivisible = (n, x, y) => !(n % x) && !(n % y)
 
   const isDivisible = (n, x, y) => n % x == 0 && n % y == 0
+
+  const isDivisible = (n, x, y) => !(n % x) && !(n % y)
