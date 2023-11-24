799067a4ad913e8969c7615ad8a1c0915937c920
jhardoin <jacob_ardoin@icloud.com>
2023-11-24 17:40:19 -0600

    Sum without the highest and lowest

--- kyu8/Sum without the highest and lowest.js
+++ kyu8/Sum without the highest and lowest.js
@@ -15,3 +15,7 @@ function sumArray(array) {
     if(array == null || array.length <= 2) return 0
     return array.sort((a,b) => a - b).slice(1, array.length - 1).reduce((a,b) => a + b)
   }
+
+  function sumArray(array) {
+    return array == null || array.length < 3  ? 0 : array.sort((a,b) => a - b).slice(1, array.length - 1).reduce((a,b) => a + b, 0)
+  }
