423aac53feff2e5cc1c40ca13ca6baab3f0f1f51
jhardoin <jacob_ardoin@icloud.com>
2023-11-28 21:16:45 -0600

    Grasshopper

--- kyu8/Grasshopper - Terminal Game Move Function.js
+++ kyu8/Grasshopper - Terminal Game Move Function.js
@@ -3,3 +3,7 @@ const move = (position, roll) => position + roll * 2
 const move = (position, roll) => position + (roll * 2)
 
 const move = (position, roll) => position + roll * 2
+
+function move (position, roll) {
+    return position + roll * 2
+  }
