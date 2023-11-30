5c3eff6805c29d81abaf7d928eba4a5e7c3c0c45
jhardoin <jacob_ardoin@icloud.com>
2023-11-29 19:10:32 -0600

    Alternating Case

--- kyu8/Alternating Case.js
+++ kyu8/Alternating Case.js
@@ -9,3 +9,8 @@ String.prototype.toAlternatingCase = function () {
   String.prototype.toAlternatingCase = function () {
     return [...this].map(e => e == e.toUpperCase() ? e.toLowerCase() : e.toUpperCase()).join('')
   }
+
+  String.prototype.toAlternatingCase = function () {
+    return [...this].map(e => e == e.toUpperCase() ? e.toLowerCase() : e.toUpperCase())
+                    .join('')
+  }
