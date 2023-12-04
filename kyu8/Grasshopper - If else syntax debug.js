3cef803b24a2bb07880895e81c5deaa823211ae7
jhardoin <jacob_ardoin@icloud.com>
2023-12-03 18:37:23 -0600

    Grasshopper

--- kyu8/Grasshopper - If else syntax debug.js
+++ kyu8/Grasshopper - If else syntax debug.js
@@ -4,3 +4,11 @@ const checkAlive = health => {
     if(health <= 0) return false
     else return true
   }
+
+  function checkAlive (health) {
+    if (health <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
