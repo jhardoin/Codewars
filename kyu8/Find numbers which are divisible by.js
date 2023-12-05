3292d3fdedc35dda72980e7ff7d0d5cd589f1881
jhardoin <jacob_ardoin@icloud.com>
2023-12-04 19:21:55 -0600

    Find numbers which are divisible by

--- kyu8/Find numbers which are divisible by.js
+++ kyu8/Find numbers which are divisible by.js
@@ -3,3 +3,7 @@ function divisibleBy(numbers, divisor){
   }
 
   const divisibleBy = (numbers, divisor) => numbers.filter(e => !(e % divisor))
+
+  function divisibleBy(numbers, divisor){
+    return numbers.filter(e => e % divisor == 0)
+  }
