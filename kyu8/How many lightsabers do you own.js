011099bfb719222c21435ea7d75621a5d6b63d8c
jhardoin <jacob_ardoin@icloud.com>
2023-12-04 19:20:42 -0600

    How many lightsabers do you own

--- kyu8/How many lightsabers do you own.js
+++ kyu8/How many lightsabers do you own.js
@@ -1,3 +1,7 @@
 const howManyLightsabersDoYouOwn = (name) => (name === 'Zach') ? 18 : 0
 
 const howManyLightsabersDoYouOwn = name => name == 'Zach' ? 18 : 0
+
+function howManyLightsabersDoYouOwn(name) {
+    return name == 'Zach' ? 18 : 0
+  }
