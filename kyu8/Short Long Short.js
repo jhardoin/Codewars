c6f915f226c1b6ee0a03333c5d4bb2ba4f6236fa
jhardoin <jacob_ardoin@icloud.com>
2023-12-05 19:05:30 -0600

    Short long short

--- kyu8/Short Long Short.js
+++ kyu8/Short Long Short.js
@@ -4,3 +4,7 @@ function solution(a, b){
   }
 
   const solution = (a, b) => a.length > b.length ? b + a + b : a + b + a
+
+  function solution(a, b) {
+    return a.length < b.length ? a + b + a : b + a + b
+  }
