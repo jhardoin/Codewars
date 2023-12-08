c58f9d1f9d1b9dc8e70659fcb51374591f09c669
jhardoin <jacob_ardoin@icloud.com>
2023-12-07 18:00:04 -0600

    Supper duper easy

--- kyu8/Super duper easy.js
+++ kyu8/Super duper easy.js
@@ -1,3 +1,7 @@
 const problem = (x) => (typeof x === 'number') ? x * 50 + 6 : "Error"
 
 const problem = x => typeof x == 'string' ? 'Error' : x * 50 + 6
+
+function problem(x){
+    return typeof x == 'string' ? 'Error' : x * 50 + 6
+}
