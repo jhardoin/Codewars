5b6331f6b41607b19291a27f1a9cbef8552b129b
jhardoin <jacob_ardoin@icloud.com>
2023-12-08 20:36:17 -0600

    Wide-mouthed frog

--- kyu8/Wide-mouthed frog.js
+++ kyu8/Wide-mouthed frog.js
@@ -1,3 +1,7 @@
 const mouthSize = (animal) => (animal.toLowerCase() === 'alligator') ? 'small' : 'wide'
 
 const mouthSize = animal => animal.toLowerCase() == 'alligator' ? 'small' : 'wide'
+
+function mouthSize(animal) {  
+    return animal.toLowerCase() == 'alligator' ? 'small' : 'wide'
+}
