eeab5472f0cf8e104f48bd7487d1ec64136c5d43
jhardoin <jacob_ardoin@icloud.com>
2023-12-10 21:12:19 -0600

    Hellow name or world

--- kyu8/Hello, Name, or World.js
+++ kyu8/Hello, Name, or World.js
@@ -6,3 +6,7 @@ function hello(name) {
   function hello(name) {
     return name ? 'Hello, ' + name[0].toUpperCase() + name.slice(1).toLowerCase() + '!' : 'Hello, World!'
   }
+
+  function hello(name) {
+    return name ? `Hello, ${name[0].toUpperCase() + name.toLowerCase().slice(1)}!` : 'Hello, World!'
+  }
