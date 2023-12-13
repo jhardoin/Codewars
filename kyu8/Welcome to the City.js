bfa09e2edebfdaf76faff715c770b2a8ff05f196
jhardoin <jacob_ardoin@icloud.com>
2023-12-12 20:45:19 -0600

    Welcome to the city

--- kyu8/Welcome to the City.js
+++ kyu8/Welcome to the City.js
@@ -3,3 +3,8 @@ function sayHello( name, city, state ) {
   }
 
   const sayHello = (name, city, state) =>`Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+
+  function sayHello( name, city, state ) {
+    name = name.join(' ')
+     return `Hello, ${name}! Welcome to ${city}, ${state}!`
+  }
