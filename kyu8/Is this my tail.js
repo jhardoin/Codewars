eae9259f4ac5723a541cf5ac8b38f708da0df6df
jhardoin <jacob_ardoin@icloud.com>
2023-12-13 20:29:19 -0600

    Is this my tail

--- kyu8/Is this my tail.js
+++ kyu8/Is this my tail.js
@@ -1,3 +1,7 @@
 const correctTail = (body, tail) => body.slice(-1) == tail
 
 const correctTail = (bod, tail) => bod.endsWith(tail)
+
+function correctTail(bod, tail){
+    return bod[bod.length - 1] == tail
+  }
