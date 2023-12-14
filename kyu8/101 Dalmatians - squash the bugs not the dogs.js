a43aeba8364b9f6f888d870ab68cbde155637f0e
jhardoin <jacob_ardoin@icloud.com>
2023-12-14 15:04:29 -0600

    101 dalmatians

--- kyu8/101 Dalmatians - squash the bugs not the dogs.js
+++ kyu8/101 Dalmatians - squash the bugs not the dogs.js
@@ -6,3 +6,10 @@ function howManyDalmatians(number)  {
     if(number == 101) return dogs[3]
     else return dogs[2]
   }
+
+  function howManyDalmatians(number) {
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+    var respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number < 101 ? dogs[2] : dogs[3]
+  return respond
+  }
+  
