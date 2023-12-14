f5444fd75c2935a737c3b8e254485846eddc817c
jhardoin <jacob_ardoin@icloud.com>
2023-12-14 14:42:26 -0600

    generate range of integers

--- kyu8/Generate range of integers.js
+++ kyu8/Generate range of integers.js
@@ -1,3 +1,7 @@
 function generateRange(min, max, step){
     return Array.from({length: (max - min) / step + 1}, (v, i) => min + i * step )
 }
+
+function generateRange(min, max, step){
+    return Array.from({length: (max - min)/step + 1}, (e,i) => (i * step) + min)
+}
