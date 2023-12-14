6526fc0f308553a3757923128d1aca7301f23a2d
jhardoin <jacob_ardoin@icloud.com>
2023-12-13 20:27:04 -0600

    Exlamation marks series 11

--- kyu8/Exclamation marks series 11.js
+++ kyu8/Exclamation marks series 11.js
@@ -6,3 +6,8 @@ function replace(s){
     let vowels = ['a', 'e', 'i', 'o', 'u']
     return [...s].map(e => vowels.includes(e.toLowerCase()) ? '!' : e).join('')
   }
+
+  function replace(s){
+    let vowel = ['a','e','i','o','u','A','E','I','O','U']
+    return [...s].map(e => vowel.includes(e) ? '!' : e).join('')
+  }
