049c45cbccf8b32705b0b86e54b49422b2ec90f1
jhardoin <jacob_ardoin@icloud.com>
2023-12-13 20:34:42 -0600

    Grasshopper

--- kyu8/Grasshopper - Debug.js
+++ kyu8/Grasshopper - Debug.js
@@ -11,3 +11,13 @@ function weatherInfo (temp) {
     if (c > 0) return (c + " is above freezing temperature")
     else return (c + " is freezing temperature")
   }
+
+  function weatherInfo (temp) {
+    var c = (temp - 32) * 5/9
+    if (c > 0)
+      return (c + " is above freezing temperature")
+    else
+      return (c + " is freezing temperature")
+  }
+  
+  
