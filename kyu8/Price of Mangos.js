2cb0c4d3267ef9f72a812971ed06387e311ab366
jhardoin <jacob_ardoin@icloud.com>
2023-12-14 14:59:46 -0600

    Price of Mangos

--- kyu8/Price of Mangos.js
+++ kyu8/Price of Mangos.js
@@ -1,1 +1,6 @@
 const mango = (quantity, price) => (quantity < 3) ? quantity * price: (quantity - Math.floor(quantity/3)) * price;
+
+function mango(quantity, price){
+    return quantity % 3 == 0 ? 2/3 * quantity * price : 
+    ((Math.floor(quantity/3) * 2) + quantity % 3) * price
+  }
