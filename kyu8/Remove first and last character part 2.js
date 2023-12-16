63143ff19e53f1bbfa7632e65b3649c76c9d065e
jhardoin <jacob_ardoin@icloud.com>
2023-12-15 21:10:56 -0600

    first and last

--- kyu8/Remove first and last character part 2.js
+++ kyu8/Remove first and last character part 2.js
@@ -2,3 +2,8 @@ function array(string) {
     let strArry = string.replace(/,/g, ' ').split(' ');
     return (strArry.length <= 2) ? null : strArry.slice(1, -1).join(' ')
   }
+
+  function array(string) {
+    if(string.split(',').length < 3) return null
+    else return string.split(',').slice(1,string.split(',').length - 1).join(' ')
+  }
