e228a2e6d89b3262b996b8b042ddfac493107afb
jhardoin <jacob_ardoin@icloud.com>
2023-12-17 22:00:06 -0600

    Simple Fun 1

--- kyu8/Simple Fun 1 Seats in Theater.js
+++ kyu8/Simple Fun 1 Seats in Theater.js
@@ -1,3 +1,7 @@
 function seatsInTheater(nCols, nRows, col, row) {
     return (nCols * nRows) - ((nRows * (col - 1)) + ((nCols - (col - 1)) * row));
   }
+
+  function seatsInTheater(nCols, nRows, col, row) {
+    return (nRows - row) * (nCols - col + 1)
+  }
