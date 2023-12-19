4c14a4e91a0b1de7ea047e9641ca9c6bc19bcff2
jhardoin <jacob_ardoin@icloud.com>
2023-12-19 16:58:57 -0600

    List Filtering

--- kyu7/List Filtering.js
+++ kyu7/List Filtering.js
@@ -4,3 +4,6 @@ const filter_list = l => l.filter(e => typeof e == 'number')
 
 const filter_list = l => l.filter(e => typeof e == 'number')
 
+function filter_list(l) {
+    return l.filter(e => typeof e == 'number')
+}
