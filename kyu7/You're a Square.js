849edab941e05844dbdb119daab3ea04adba8e9a
jhardoin <jacob_ardoin@icloud.com>
2023-12-19 16:56:21 -0600

    You're a square

--- kyu7/You're a Square.js
+++ kyu7/You're a Square.js
@@ -11,3 +11,7 @@ let isSquare = (n) => {
   var isSquare = n => Math.sqrt(n) % 1 == 0
 
   const isSquare = n =>  Math.sqrt(n) % 1 == 0
+
+  var isSquare = function(n){
+    return Math.sqrt(n) % 1 == 0
+  }
