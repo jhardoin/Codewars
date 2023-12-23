40737bb28cfd011aaae95b069f146918992ab96f
jhardoin <jacob_ardoin@icloud.com>
2023-12-22 19:48:26 -0600

    Friend or foe

--- kyu7/Friend or Foe.js
+++ kyu7/Friend or Foe.js
@@ -1,3 +1,7 @@
 const friend = friends => friends.filter(e => e.length == 4)
 
 const friend = friends => friends.filter(e => e.length == 4)
+
+function friend(friends){
+    return friends.filter(e => e.length == 4)
+  }
