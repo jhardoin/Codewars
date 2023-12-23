168c618d0a65452b839493b8d7c63a9a0741a706
jhardoin <jacob_ardoin@icloud.com>
2023-12-23 16:53:43 -0600

    Reverse words

--- kyu7/Reverse Words.js
+++ kyu7/Reverse Words.js
@@ -1,3 +1,7 @@
 const reverseWords = str => str.split(' ').map(e => e.split('').reverse().join('')).join(' ')
 
 const reverseWords = str => str.split(' ').map(e => [...e].reverse().join('')).join(' ')
+
+function reverseWords(str) {
+    return str.split(' ').map(e => [...e].reverse().join('')).join(' ')
+  }
