fa95c3885ac0f5567e0266984fedd84c174f08ea
jhardoin <jacob_ardoin@icloud.com>
2023-12-25 17:00:03 -0600

    Breaking chocolate problem

--- kyu7/Breaking chocolate problem.js
+++ kyu7/Breaking chocolate problem.js
@@ -1,3 +1,7 @@
 const breakChocolate = (n,m) => (n * m < 2) ? 0 : (n * m) - 1
 
 const breakChocolate = (n,m) => n && m ? n * m - 1 : 0
+
+function breakChocolate(n,m) {
+    return n * m > 0 ? n * m - 1 : 0
+  }
