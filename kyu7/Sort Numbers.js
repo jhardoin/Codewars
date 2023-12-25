87d09efd8b6f358c8b8e3c9b01b97cf18dc1bc54
jhardoin <jacob_ardoin@icloud.com>
2023-12-25 16:58:35 -0600

    Sort Numbers

--- kyu7/Sort Numbers.js
+++ kyu7/Sort Numbers.js
@@ -1,3 +1,7 @@
 const solution = nums => (nums == null) ? [] : nums.sort((a,b) => a - b)
 
 const solution = nums => nums ? nums.sort((a,b) => a - b) : []
+
+function solution(nums){
+    return nums ? nums.sort((a,b) => a - b) : []
+  }
