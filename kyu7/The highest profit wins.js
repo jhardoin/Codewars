d5fa0325026e924336ea9f85e387ce78ea60bf78
jhardoin <jacob_ardoin@icloud.com>
2023-12-24 18:07:43 -0600

    The highest profit wins

--- kyu7/The highest profit wins.js
+++ kyu7/The highest profit wins.js
@@ -1,3 +1,7 @@
 const minMax = arr => [Math.min(...arr), Math.max(...arr)]
 
 const minMax = arr => [Math.min(...arr), Math.max(...arr)];
+
+function minMax(arr){
+    return [Math.min(...arr), Math.max(...arr)]
+  }
