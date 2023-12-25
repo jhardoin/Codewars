014eb9ae9ae31d426d7554c63139458def91d4fb
jhardoin <jacob_ardoin@icloud.com>
2023-12-25 16:57:23 -0600

    Find the stray number

--- kyu7/Find the Stray Number.js
+++ kyu7/Find the Stray Number.js
@@ -1,3 +1,7 @@
 const stray = numbers => numbers.reduce((a,b) => a ^ b)
 
 const stray = numbers => numbers.reduce((a,b) => a ^ b)
+
+function stray(numbers) {
+    return +numbers.filter(e => numbers.indexOf(e) == numbers.lastIndexOf(e)).join()
+  }
