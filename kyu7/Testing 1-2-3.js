cd6c83aaf75ab089b0d1939044806b2675cecd42
jhardoin <jacob_ardoin@icloud.com>
2023-12-24 18:13:13 -0600

    Testing 123

--- kyu7/Testing 1-2-3.js
+++ kyu7/Testing 1-2-3.js
@@ -1,3 +1,7 @@
 const number = arr => arr.map((e,i) => `${i + 1}: ${e}`)
 
 const number = array => array.map((e,i) => `${i + 1}` + ': ' + e)
+
+var number=function(array){
+    return array.map((e,i) => (i + 1) + ': ' + e)
+  }
