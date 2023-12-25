4982914604c606e3975da3887fb645cd17edfece
jhardoin <jacob_ardoin@icloud.com>
2023-12-24 18:06:29 -0600

    Odd or even

--- kyu7/Odd or Even.js
+++ kyu7/Odd or Even.js
@@ -1,3 +1,7 @@
 const oddOrEven = a => a.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
 
 const oddOrEven = array => array.reduce((a,b) => a + b, 0) % 2 ? 'odd' : 'even'
+
+function oddOrEven(array) {
+    return array.reduce((a,b) => a + b, 0) % 2 ? 'odd' : 'even'
+ }
