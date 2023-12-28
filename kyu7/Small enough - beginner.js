6d36189dc38701e3926f5ecb6f6d87fff4c660d1
jhardoin <jacob_ardoin@icloud.com>
2023-12-27 18:49:36 -0600

    Small enough

--- kyu7/Small enough - beginner.js
+++ kyu7/Small enough - beginner.js
@@ -1,3 +1,7 @@
 const smallEnough = (a, limit) => a.every(e => e <= limit)
 
 const smallEnough = (a, limit) => a.every(e => e <= limit)
+
+function smallEnough(a, limit){
+    return a.every(e => e <= limit)
+  }
