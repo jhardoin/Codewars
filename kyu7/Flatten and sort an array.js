4cac02ad257583d7686edaf0fa3a526383b74c1c
jhardoin <jacob_ardoin@icloud.com>
2023-12-31 20:44:46 -0600

    Flatten and sort an array

--- kyu7/Flatten and sort an array.js
+++ kyu7/Flatten and sort an array.js
@@ -1,3 +1,7 @@
 const flattenAndSort = array => [].concat(...array).sort((a,b) => a - b)
 
 const flattenAndSort = array => array.flat().sort((a,b) => a - b)
+
+function flattenAndSort(array) {
+    return array.flat().sort((a,b)=> a - b)
+  }
