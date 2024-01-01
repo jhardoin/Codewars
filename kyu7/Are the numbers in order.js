75a5aa9cb45676687a23d82cfecec321c6037594
jhardoin <jacob_ardoin@icloud.com>
2023-12-31 20:32:47 -0600

    are the numbes in order

--- kyu7/Are the numbers in order.js
+++ kyu7/Are the numbers in order.js
@@ -3,3 +3,7 @@ const inAscOrder = arr => JSON.stringify(arr) === JSON.stringify(arr.sort((a,b) 
 function inAscOrder(arr) {
     return JSON.stringify(arr) == JSON.stringify(arr.sort((a,b) => a - b))
   }
+
+  function inAscOrder(arr) {
+    return arr.every((e,i) => i == 0 || arr[i] > arr[i-1])
+  }
