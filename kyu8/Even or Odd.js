a4991cbd38c7e2a75c5f11dfcabd13641797e383
jhardoin <jacob_ardoin@icloud.com>
2024-01-01 19:16:05 -0600

    Even or odd

--- kyu8/Even or Odd.js
+++ kyu8/Even or Odd.js
@@ -8,3 +8,7 @@ function evenOrOdd(number) {
   const evenOrOdd = n => n % 2 ? 'Odd' : 'Even'
 
   const evenOrOdd = number => number % 2 ? 'Odd' : 'Even'
+
+  function evenOrOdd(number) {
+    return number % 2 ? 'Odd' : 'Even'
+  }
