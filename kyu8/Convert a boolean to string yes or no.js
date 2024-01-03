c0584d2f7a8c798501077a459671b0509353bfc3
jhardoin <jacob_ardoin@icloud.com>
2024-01-02 19:15:56 -0600

    Convert a boolean to string yes or no

--- kyu8/Convert a boolean to string yes or no.js
+++ kyu8/Convert a boolean to string yes or no.js
@@ -3,3 +3,7 @@ const boolToWord = bool => bool == true ? 'Yes' : 'No'
 const boolToWord = bool => bool ? 'Yes' : 'No'
 
 const boolToWord = bool => bool ? 'Yes' : 'No'
+
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No'
+  }
