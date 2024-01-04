6900f3bbda4cc40c20b3d2adbcf2d8599a5315af
jhardoin <jacob_ardoin@icloud.com>
2024-01-03 19:05:36 -0600

    Remove first and last

--- kyu8/Remove first and Last.js
+++ kyu8/Remove first and Last.js
@@ -7,3 +7,7 @@ const removeChar = s => s.slice(1, -1)
 const removeChar = str => str.slice(1, str.length - 1)
 
 const removeChar = str => [...str].slice(1, str.length - 1).join('')
+
+function removeChar(str){
+    return str.slice(1,-1)
+  }
