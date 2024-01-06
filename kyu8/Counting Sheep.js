f6a7b694c9886c9fdfd41029ef77ff3c5397605c
jhardoin <jacob_ardoin@icloud.com>
2024-01-05 20:35:36 -0600

    counting sheep

--- kyu8/Counting Sheep.js
+++ kyu8/Counting Sheep.js
@@ -3,3 +3,7 @@ const countSheeps = arrayOfSheep => arrayOfSheep.filter(e => e == true).length
 const countSheeps = sheep => sheep.filter(e => e == true).length
 
 const countSheeps = sheep => sheep ? sheep.filter(e => e == true).length : 0
+
+function countSheeps(sheep) {
+    return sheep.filter(e => e == true).length
+}
