e88b8d7100494e80ad44c79c3e7231b28724da8c
jhardoin <jacob_ardoin@icloud.com>
2024-01-08 18:57:02 -0600

    sum arrays

--- kyu8/Sum Arrays.js
+++ kyu8/Sum Arrays.js
@@ -7,3 +7,7 @@ const sum = numbers => numbers.reduce((a,b) => a + b, 0)
 const sum = numbers => numbers.reduce((a,b) => a + b, 0)
 
 const sum = numbers => numbers.reduce((a,b) => a + b, 0)
+
+function sum (numbers) {
+    return numbers.reduce((a,b) => a + b, 0)
+}
