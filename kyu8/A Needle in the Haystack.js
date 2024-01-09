0ec8a142ea686677fb6f041b1b6a11f91977810e
jhardoin <jacob_ardoin@icloud.com>
2024-01-08 19:01:03 -0600

    needle

--- kyu8/A Needle in the Haystack.js
+++ kyu8/A Needle in the Haystack.js
@@ -11,3 +11,7 @@ function findNeedle(haystack) {
   const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`
 
   const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`
+
+  function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+  }
