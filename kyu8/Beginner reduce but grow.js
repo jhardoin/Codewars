d6ace9ba1b3cc1c61d937fb900c8df2b87d28aba
jhardoin <jacob_ardoin@icloud.com>
2024-01-10 18:25:38 -0600

    Reduce

--- kyu8/Beginner reduce but grow.js
+++ kyu8/Beginner reduce but grow.js
@@ -3,3 +3,5 @@ const grow = x => x.reduce((a,b) => a * b)
 const grow = x => x.reduce((a,b) => a * b)
 
 const grow = x => x.reduce((a,b) => a * b, 1)
+
+const grow = x => x.reduce((a,b) => a * b, 1)
