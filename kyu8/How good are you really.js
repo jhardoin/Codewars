e21ec0718caeecffbd754763c6a5081e9c424695
jhardoin <jacob_ardoin@icloud.com>
2024-01-10 18:27:17 -0600

    How  good

--- kyu8/How good are you really.js
+++ kyu8/How good are you really.js
@@ -3,11 +3,15 @@ function betterThanAverage(classPoints, yourPoints) {
     return (yourPoints > avgClass) ? true : false;
     }
 
-    const betterThanAverage = (cP, yP) => (cP.reduce((a,b) => a + b) + yP) / cP.length <= yP
+const betterThanAverage = (cP, yP) => (cP.reduce((a,b) => a + b) + yP) / cP.length <= yP
 
-    const betterThanAverage = (classPoints, yourPoints) => yourPoints > classPoints.reduce((a,b) => a + b)/classPoints.length
+const betterThanAverage = (classPoints, yourPoints) => yourPoints > classPoints.reduce((a,b) => a + b)/classPoints.length
 
-    function betterThanAverage(classPoints, yourPoints) {
+function betterThanAverage(classPoints, yourPoints) {
         return yourPoints > classPoints.reduce((a,b) => a + b, 0)/classPoints.length
+      }
+      
+function betterThanAverage(classPoints, yourPoints) {
+        return classPoints.reduce((a,b) => a + b, 0)/classPoints.length < yourPoints
       }
       
