424fa575d34ebdf8debc91f9b430b6dd151b3cea
jhardoin <jacob_ardoin@icloud.com>
2024-01-13 21:07:03 -0600

    quarter of the year

--- kyu8/Quarter of the Year.js
+++ kyu8/Quarter of the Year.js
@@ -18,3 +18,7 @@ const quarterOf = (month) => {
   const quarterOf = month => month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4
 
   const quarterOf = month => month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4
+
+  const quarterOf = (month) => {
+    return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4
+  }
