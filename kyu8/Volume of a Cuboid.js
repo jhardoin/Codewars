902ec84167a9fa4352fa478116e3f240f74a3630
jhardoin <jacob_ardoin@icloud.com>
2024-01-14 22:00:24 -0600

    volume of a cuboid

--- kyu8/Volume of a Cuboid.js
+++ kyu8/Volume of a Cuboid.js
@@ -21,3 +21,9 @@ class Kata {
       return length * width * height
     }
   }
+
+  class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
