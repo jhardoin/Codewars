6ccab23ea34b05c9d970a2d8e58aa90abff22c3a
jhardoin <jacob_ardoin@icloud.com>
2024-01-17 17:35:23 -0600

    int

--- kyu8/Parse nice int from char problem.js
+++ kyu8/Parse nice int from char problem.js
@@ -5,3 +5,7 @@ const getAge = inputString => +inputString[0]
 const getAge = inputString => +inputString[0] 
 
 const getAge = inputString => +inputString[0]
+
+function getAge(inputString){
+    return +inputString[0]
+}
