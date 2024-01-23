a2e03d3d70853f90074430a3f649f8e8dfe5904b
jhardoin <jacob_ardoin@icloud.com>
2024-01-22 20:13:30 -0600

    character recognition software

--- kyu8/Correct the mistakes of the character recognition software.js
+++ kyu8/Correct the mistakes of the character recognition software.js
@@ -18,3 +18,7 @@ function correct(string){
   function correct(string){
     return [...string].map(e => e === '0' ? 'O' : e === '5' ? 'S' : e === '1' ? 'I' : e).join('')
   }
+
+  function correct(string){
+    return [...string].map(e => e === '0' ? 'O' : e === '5' ? 'S' : e === '1' ? 'I' : e).join('')
+  }
