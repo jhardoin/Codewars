6163d27383dca334887fffc27c941299440e5f86
jhardoin <jacob_ardoin@icloud.com>
2024-01-26 15:34:42 -0600

    Capitalizstion and Mutability

--- kyu8/Capitalization and Mutability.js
+++ kyu8/Capitalization and Mutability.js
@@ -7,3 +7,7 @@ function capitalizeWord(word) {
   function capitalizeWord(word) {
     return word[0].toUpperCase() + word.slice(1)
   }
+
+  function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1)
+  }
