ad9df0df4ce4bac0b3f6f42386ed2699af2b1fcf
jhardoin <jacob_ardoin@icloud.com>
2024-01-27 17:27:58 -0600

    xor

--- kyu8/Exclusive or xor logical operator.js
+++ kyu8/Exclusive or xor logical operator.js
@@ -1,3 +1,7 @@
 let xor = (a, b) => ((a === true && b === false) || (a === false && b === true)) ? true : false
 
 const xor = (a, b) => (!a && b) || (a && !b)
+
+function xor(a, b) {
+    return a ^ b ? true : false
+}
