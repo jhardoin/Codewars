f62dc5d1866ce884069ab585ab13bcf8a8dc8806
jhardoin <jacob_ardoin@icloud.com>
2024-01-29 17:09:19 -0600

    Merge

--- kyu8/Merge two sorted arrays into one.js
+++ kyu8/Merge two sorted arrays into one.js
@@ -10,3 +10,7 @@ function mergeArrays(arr1, arr2) {
   function mergeArrays(arr1, arr2) {
     return [...new Set(arr1.concat(arr2))].sort((a,b) => a-b)
   }
+
+  function mergeArrays(arr1, arr2) {
+    return [...new Set(arr1.concat(arr2).sort((a,b) => a - b))]
+  }
