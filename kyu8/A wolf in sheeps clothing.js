7b53251ca55c6ceb9ecbfb6d11eb915cb7aaf026
jhardoin <jacob_ardoin@icloud.com>
2024-01-30 17:00:49 -0600

    wolf

--- kyu8/A wolf in sheeps clothing.js
+++ kyu8/A wolf in sheeps clothing.js
@@ -8,3 +8,6 @@ function warnTheSheep(queue) {
       return num > 0 ? `Oi! Sheep number ${num}! You are about to be eaten by a wolf!`:
       "Pls go away and stop eating my sheep"
   }
+  function warnTheSheep(queue) {
+    return queue.length - 1 - queue.indexOf('wolf') > 0 ? `Oi! Sheep number ${queue.length - 1 - queue.indexOf('wolf')}! You are about to be eaten by a wolf!` : 'Pls go away and stop eating my sheep'
+  }
