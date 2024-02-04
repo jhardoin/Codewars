23d7056f5f94231a43a45cbd71a38bdfb32129ca
jhardoin <jacob_ardoin@icloud.com>
2024-02-04 17:59:55 -0600

    find the remainder

--- kyu8/Find the remainder.js
+++ kyu8/Find the remainder.js
@@ -3,3 +3,7 @@ const remainder = (n, m) => (n > m) ? n % m : m % n
 function remainder(n, m){
     return n > m ? n % m : m % n
   }
+
+  function remainder(n, m){
+    return Math.max(n,m) % Math.min(n,m)
+  }
