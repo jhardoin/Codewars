bdeb772b2a4550e6397e844cd16d0ae6325cd55c
jhardoin <jacob_ardoin@icloud.com>
2024-02-05 13:49:11 -0600

    surface area

--- kyu8/Surface Area and Volume of a Box.js
+++ kyu8/Surface Area and Volume of a Box.js
@@ -9,3 +9,8 @@ function getSize(width, height, depth) {
         return [2 * (height * width + width * depth + height * depth), width * height * depth]
       }
       
+
+      const getSize = (w, h, d) => [
+        (w * h + w * d + h * d) * 2,
+        w * h * d
+      ]
