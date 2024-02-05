71ebc61ac87d2ece6c86257cfee56f461026430c
jhardoin <jacob_ardoin@icloud.com>
2024-02-05 13:50:30 -0600

    alan partridge

--- kyu8/Alan Partridge II - Apple Turnover.js
+++ kyu8/Alan Partridge II - Apple Turnover.js
@@ -4,3 +4,7 @@ const  apple  = x => (x * x > 1000) ? 'It\'s hotter than the sun!!' :  'Help you
 function apple(x){
     return x ** 2 > 1000 ? "It's hotter than the sun!!" : 'Help yourself to a honeycomb Yorkie for the glovebox.'
 }
+
+function apple(x){
+    return x ** 2 > 1000 ? "It's hotter than the sun!!" : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
