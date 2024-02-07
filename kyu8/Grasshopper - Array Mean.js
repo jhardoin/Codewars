892afe60a25b0f6f7f440926f6387408bcfdc579
jhardoin <jacob_ardoin@icloud.com>
2024-02-06 21:14:23 -0600

    Array mean

--- kyu8/Grasshopper - Array Mean.js
+++ kyu8/Grasshopper - Array Mean.js
@@ -3,3 +3,7 @@ const findAverage = (nums) => (nums.reduce((a, c) => a + c))/nums.length
 var findAverage = function (nums) {
     return nums.reduce((a,b) => a + b)/nums.length
   }
+
+  var findAverage = function (nums) {
+    return nums.reduce((a,b) => a + b, 0)/nums.length
+  }
