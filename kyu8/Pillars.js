ed6179bef133cfff2ef1a8ba56bb7144dca79763
jhardoin <jacob_ardoin@icloud.com>
2024-02-07 21:24:12 -0600

    pillars

--- kyu8/Pillars.js
+++ kyu8/Pillars.js
@@ -3,3 +3,7 @@ const pillars = (numPill, dist, width) => (numPill <= 1) ? 0 : (dist * 100) * (n
 function pillars(numPill, dist, width) {
     return numPill > 1 ? ((numPill - 2) * width) + ((numPill - 1) * dist * 100) : 0
   }
+
+  function pillars(num_pill, dist, width) {
+    return num_pill <= 1 ? 0 : (num_pill - 1) * dist * 100 + (num_pill - 2) * width;
+  }
