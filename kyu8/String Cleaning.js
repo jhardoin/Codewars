17f61768aef145b86101630197948fc15aa71459
jhardoin <jacob_ardoin@icloud.com>
2024-02-07 21:26:19 -0600

    string cleaning

--- kyu8/String Cleaning.js
+++ kyu8/String Cleaning.js
@@ -3,3 +3,7 @@ const stringClean = (s) => s.replace(/[0123456789]/g, '')
 function stringClean(s){
     return s.split('').filter(e => e != parseInt(e)).join('')
   }
+
+  function stringClean(s){
+    return [...s].filter(e => !'1234567890'.includes(e)).join('')
+  }
