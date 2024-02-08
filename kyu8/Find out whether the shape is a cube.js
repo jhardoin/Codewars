a9352cee75036d9dff5f4db113a145ecef473caa
jhardoin <jacob_ardoin@icloud.com>
2024-02-07 21:29:45 -0600

    find out wheter the shape is a cube

--- kyu8/Find out whether the shape is a cube.js
+++ kyu8/Find out whether the shape is a cube.js
@@ -1,1 +1,5 @@
 const cubeChecker = (volume, side) => (volume === 0) ? false : (side ** 3) === Math.abs(volume)
+
+var cubeChecker = function(volume, side){
+    return volume > 0 && side > 0 ? Math.cbrt(volume) == side : false
+  }
