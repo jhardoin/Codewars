28a2ad9e9ec7ea2be9dcb75d6e97a4dc2f8204eb
jhardoin <jacob_ardoin@icloud.com>
2024-02-09 21:33:21 -0600

    Multiple of index

--- kyu8/Multiple of Index.js
+++ kyu8/Multiple of Index.js
@@ -1,1 +1,5 @@
 const multipleOfIndex = (array) => array.filter((element, index) => element % index == 0 || element === 0)
+
+function multipleOfIndex(array) {
+    return array.filter((e,i) => e % i === 0 || e == 0)
+  }
