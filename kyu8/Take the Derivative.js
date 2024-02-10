908f7bb58c344eb041f4122bbb532e2177670152
jhardoin <jacob_ardoin@icloud.com>
2024-02-09 21:30:57 -0600

    Take the derivative

--- kyu8/Take the Derivative.js
+++ kyu8/Take the Derivative.js
@@ -1,3 +1,7 @@
 function derive(coefficient,exponent) {
     return `${coefficient * exponent}x^${exponent -1}`
   }
+
+  function derive(coefficient,exponent) {
+    return `${coefficient * exponent}x^${exponent - 1}`
+}
