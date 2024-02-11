de554582a0d3f0e1fa5bce81bacb7c64e5de57f7
jhardoin <jacob_ardoin@icloud.com>
2024-02-10 20:25:29 -0600

    return the day

--- kyu8/Return the Day.js
+++ kyu8/Return the Day.js
@@ -8,3 +8,16 @@ function whatday(num) {
     if(num === 7) return "Saturday";
     else return "Wrong, please enter a number between 1 and 7"
 }
+
+function whatday(num) { 
+    let days =
+    {1 : "Sunday",
+    2 : "Monday",
+    3 : "Tuesday",
+    4 : "Wednesday",
+    5 : "Thursday",
+    6 : "Friday",
+    7 : "Saturday"}
+    
+    return num > 0 && num < 8 ? days[num] : 'Wrong, please enter a number between 1 and 7'
+    }
