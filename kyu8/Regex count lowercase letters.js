f885af7e243b2b8297d36531b9e234dd32fd58e7
jhardoin <jacob_ardoin@icloud.com>
2024-02-11 15:26:04 -0600

    count lowercase letters

--- kyu8/Regex count lowercase letters.js
+++ kyu8/Regex count lowercase letters.js
@@ -1,1 +1,5 @@
 const lowercaseCount = str => (str.match(/[a-z]/g) || []).length
+
+function lowercaseCount(str){
+    return [...str].filter(e => 'abcdefghijklmnopqrstuvwxyz'.includes(e)).length
+}
