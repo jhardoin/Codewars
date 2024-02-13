01372523aacf0ca4e5428d329c15937bb56a4469
jhardoin <jacob_ardoin@icloud.com>
2024-02-12 22:12:27 -0600

    define a card suit

--- kyu8/Define a Card Suit.js
+++ kyu8/Define a Card Suit.js
@@ -4,3 +4,8 @@ function defineSuit(card) {
     if (card.endsWith('♥')) return 'hearts';
     if (card.endsWith('♠')) return 'spades';
     }
+
+    function defineSuit(card) {
+        let suit = { '♣' : 'clubs', '♦' : 'diamonds', '♥' : 'hearts', '♠' : 'spades'}
+        return card.length == 2 ? suit[card[1]] : suit[card[2]]
+      }
