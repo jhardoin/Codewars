8ea649becf1d6aac9a8b987861b8d9063fdcc815
jhardoin <jacob_ardoin@icloud.com>
2024-02-14 17:32:46 -0600

    conditional

--- kyu8/Training Js 8 - Conditional statement - Switch.js
+++ kyu8/Training Js 8 - Conditional statement - Switch.js
@@ -7,3 +7,21 @@ function howManydays(month){
     }
     return days
   }
+
+  function howManydays(month){
+    let days = 0
+    switch (month){
+        case 4: 
+        case 6:
+        case 9:
+        case 11:
+          days = 30;
+            break
+        case 2:
+          days = 28;
+            break
+        default: 
+          days = 31;
+    }
+    return days
+  }
