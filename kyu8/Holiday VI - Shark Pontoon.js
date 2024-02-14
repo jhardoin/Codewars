273d973061e28ca363bdda82fdf8307bbc8e9696
jhardoin <jacob_ardoin@icloud.com>
2024-02-13 22:16:52 -0600

    shark pontoon

--- kyu8/Holiday VI - Shark Pontoon.js
+++ kyu8/Holiday VI - Shark Pontoon.js
@@ -1,1 +1,5 @@
 const shark = (pD, sD, yS, sS, d) => (d == true && (pD/yS < (sD/sS * 2)) || (pD/yS < sD/sS)) ? 'Alive!' : "Shark Bait!";
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    return youSpeed/pontoonDistance > sharkSpeed/sharkDistance/(dolphin ? 2 : 1) ? 'Alive!' : 'Shark Bait!'
+    }
