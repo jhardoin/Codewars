6263bc1c68f22c707506b0e57f27786d904e96d6
jhardoin <jacob_ardoin@icloud.com>
2024-02-14 17:17:26 -0600

    array madness

--- kyu8/Speed Code 2 - Array Madness.js
+++ kyu8/Speed Code 2 - Array Madness.js
@@ -1,1 +1,5 @@
 const arrayMadness = (a, b) => a.map(e => e ** 2).reduce((x, y) => x + y) > b.map(e => e ** 3).reduce((x, y) => x + y)
+
+function arrayMadness(a, b) {
+    return a.reduce((a,b) => a + (b ** 2), 0) > b.reduce((a,b) => a + (b ** 3), 0)
+  }
