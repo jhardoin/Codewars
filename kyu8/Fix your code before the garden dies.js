669c95bc8b1a1c1d9f613830c7ab89d46dc405d0
jhardoin <jacob_ardoin@icloud.com>
2024-02-15 16:43:39 -0600

    garden

--- kyu8/Fix your code before the garden dies.js
+++ kyu8/Fix your code before the garden dies.js
@@ -1,1 +1,9 @@
 const rainAmount = (mm) => (mm < 40) ? `You need to give your plant ${40 - mm}mm of water` : "Your plant has had more than enough water for today!"
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${Math.abs(mm - 40)}mm of water`
+    };
+  
+    return "Your plant has had more than enough water for today!"
+}
