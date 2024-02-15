58daa250cea55e84b2beba8cd4656bc0d78927d5
jhardoin <jacob_ardoin@icloud.com>
2024-02-15 16:37:02 -0600

    is it a digit

--- kyu8/Regexp basics - Is it a digit.js
+++ kyu8/Regexp basics - Is it a digit.js
@@ -1,3 +1,7 @@
 String.prototype.digit = function() {
     return (/^\d$/).test(this)
   };
+
+  String.prototype.digit = function() {
+    return this.length == 1 ? '0123456789'.includes(this) : false
+  };
