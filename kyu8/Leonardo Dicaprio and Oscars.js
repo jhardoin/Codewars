18ed6f2df8ca1a10d767cabda3100d816859f1eb
jhardoin <jacob_ardoin@icloud.com>
2024-02-16 16:07:49 -0600

    leo

--- kyu8/Leonardo Dicaprio and Oscars.js
+++ kyu8/Leonardo Dicaprio and Oscars.js
@@ -4,3 +4,10 @@ function leo(oscar){
     if(oscar < 88) return "When will you give Leo an Oscar?"
     else return "Leo got one already!"
   }
+
+  function leo(oscar){
+    return oscar == 88 ? "Leo finally won the oscar! Leo is happy" :
+           oscar == 86 ? "Not even for Wolf of wallstreet?!" :
+           oscar < 88 && oscar !== 86 ? "When will you give Leo an Oscar?" :
+            "Leo got one already!"
+  }
