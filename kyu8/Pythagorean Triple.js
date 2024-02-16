bddeb33793507141c4ee067a6b04cf6dc9ac29f9
jhardoin <jacob_ardoin@icloud.com>
2024-02-16 16:04:54 -0600

    Pythagorean triple

--- kyu8/Pythagorean Triple.js
+++ kyu8/Pythagorean Triple.js
@@ -3,3 +3,8 @@ function isPythagoreanTriple(integers) {
     let [a, b, c] = integers;
     return c**2 === a**2 + b**2;
   }
+
+  function isPythagoreanTriple(integers) {
+    integers.sort((a,b) => a - b)
+    return integers[0] ** 2 + integers[1] ** 2 == integers[2] ** 2
+  }
