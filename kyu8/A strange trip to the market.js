1d43dbf4282b3babe361ecdc31b030ea5ee4458f
jhardoin <jacob_ardoin@icloud.com>
2024-02-17 15:01:14 -0600

    strange trip to the market

--- kyu8/A strange trip to the market.js
+++ kyu8/A strange trip to the market.js
@@ -1,1 +1,5 @@
 const isLockNessMonster = (s) => /tree fiddy|3.50|three fifty/.test(s)
+
+function isLockNessMonster(s) {
+    return s.includes('tree fiddy') || s.includes('3.50') || s.includes('three fifty')
+  }
