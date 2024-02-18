a6f578355c0bf0de61d12bff7b523f02a061dd7d
jhardoin <jacob_ardoin@icloud.com>
2024-02-18 17:18:48 -0600

    tip calculator

--- kyu8/Tip Calculator.js
+++ kyu8/Tip Calculator.js
@@ -7,3 +7,14 @@ function calculateTip(amount, rating) {
      if(rat == 'terrible') return amount * 0
      else return "Rating not recognised"
 }
+
+function calculateTip(amount, rating) {
+    let r = rating.toLowerCase()
+    if(amount == 0) return 0
+    return r == 'excellent' ? Math.ceil(amount * .2) : 
+    r == 'great' ? Math.ceil(amount * .15) : 
+    r == 'good' ? Math.ceil(amount * .1) :
+    r == 'poor' ? Math.ceil(amount * .05) : 
+    r == 'terrible' ? amount * 0 :
+    'Rating not recognised'
+  }
