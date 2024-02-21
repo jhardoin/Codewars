c6a1b95d968253ca684f7ad830544487e96ab9aa
jhardoin <jacob_ardoin@icloud.com>
2024-02-20 18:54:10 -0600

    find the integral

--- kyu8/Find the Integral.js
+++ kyu8/Find the Integral.js
@@ -1,1 +1,5 @@
 const integrate = (coefficient, exponent) => `${coefficient/(exponent + 1)}x^${exponent + 1}`
+
+function integrate(coefficient, exponent) {
+    return `${coefficient/(exponent + 1)}x^${exponent + 1}`
+}
