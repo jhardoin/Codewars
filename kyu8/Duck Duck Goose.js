40359decc5d13f0f82915f5b438c806f0e271e59
jhardoin <jacob_ardoin@icloud.com>
2024-02-21 14:52:25 -0600

    duck duck goose

--- kyu8/Duck Duck Goose.js
+++ kyu8/Duck Duck Goose.js
@@ -1,1 +1,5 @@
 const duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name
+
+function duckDuckGoose(players, goose) {
+    return players[(goose-1)%players.length].name
+  }
