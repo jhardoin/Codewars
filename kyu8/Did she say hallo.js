d6dbd1a6372b25356e9e0549ff094aa8a345ca6e
jhardoin <jacob_ardoin@icloud.com>
2024-02-21 14:40:39 -0600

    did she say hallo

--- kyu8/Did she say hallo.js
+++ kyu8/Did she say hallo.js
@@ -1,1 +1,7 @@
 const validateHello = (greetings) =>(/hello|ciao|salut|hallo|hola|ahoj|czesc/gi).test(greetings)
+
+function validateHello(greetings) {
+    let hi = ['hello','ciao','salut','hallo','hola','ahoj','czesc']
+    let greet = greetings.toLowerCase()
+    return hi.some(e => greet.includes(e))
+  }
