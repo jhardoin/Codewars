be301da2ed7feac180b7a99501935be03364fc36
jhardoin <jacob_ardoin@icloud.com>
2024-02-21 14:48:01 -0600

    ufc

--- kyu8/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
+++ kyu8/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
@@ -1,1 +1,6 @@
 const quote = fighter => fighter.toLowerCase() == 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+var quote = function(fighter) {
+    return fighter.toLowerCase() == 'conor mcgregor' ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" :
+    "I am not impressed by your performance."
+}
