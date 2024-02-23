fc5d42cdf96d2ec54a07c8756b83c6c194d376e0
jhardoin <jacob_ardoin@icloud.com>
2024-02-22 21:31:56 -0600

    who ate the cookie

--- kyu8/Who ate the cookie.js
+++ kyu8/Who ate the cookie.js
@@ -3,3 +3,9 @@ function cookie(x){
     if(typeof x == 'number') return "Who ate the last cookie? It was Monica!"
     return "Who ate the last cookie? It was the dog!"
   }
+
+  function cookie(x){
+    return typeof x == 'string' ? `Who ate the last cookie? It was Zach!` :
+            typeof x == 'number' ? `Who ate the last cookie? It was Monica!` :
+            'Who ate the last cookie? It was the dog!'
+  }
