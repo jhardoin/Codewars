1f2f04a2fda574eaccb1265ad5b5a7ff3cc93bd6
jhardoin <jacob_ardoin@icloud.com>
2024-02-23 20:36:23 -0600

    switch

--- kyu8/Switch Case - Bug Fixing 6.js
+++ kyu8/Switch Case - Bug Fixing 6.js
@@ -11,3 +11,14 @@ function evalObject(value){
     }
     return result;
   }
+
+  function evalObject(value){
+    switch(value.operation){
+      case'+': return value.a + value.b;
+      case'-': return value.a - value.b;
+      case'/': return value.a / value.b;
+      case'*': return value.a * value.b;
+      case'%': return value.a % value.b;
+      case'^': return Math.pow(value.a, value.b);
+    }
+  }
