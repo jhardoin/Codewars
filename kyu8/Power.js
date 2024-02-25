6f26d8ad3d18824925e364c1e0a76e6743149179
jhardoin <jacob_ardoin@icloud.com>
2024-02-24 21:49:36 -0600

    Power

--- kyu8/Power.js
+++ kyu8/Power.js
@@ -1,1 +1,9 @@
 const numberToPower = (number, power) => (power < 1) ? power + 1 : Array.from({ length: power}).map(e => number).reduce((a,b) => a * b)
+
+function numberToPower(number, power){
+    let sum = 1 
+    for(let i = 1; i <= power; i++){
+      sum *= number
+    }
+    return sum
+  }
