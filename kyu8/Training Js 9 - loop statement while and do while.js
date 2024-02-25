010bf4550069c9fad18dc8ab588ab6e0b0b9aed8
jhardoin <jacob_ardoin@icloud.com>
2024-02-24 21:58:26 -0600

    js 9

--- kyu8/Training Js 9 - loop statement while and do while.js
+++ kyu8/Training Js 9 - loop statement while and do while.js
@@ -12,3 +12,14 @@ function padIt(str,n){
     }
     return str
   }
+
+
+  function padIt(str,n){
+    let str2 = [str]
+    let i = 0
+    while(i < n){
+      i % 2 == 0 ? str2.unshift('*') : str2.push('*')
+      i++
+    }
+    return str2.join('')
+  }
