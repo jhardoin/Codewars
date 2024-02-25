35d6c13bab89182db627788fbb30813b8314469b
jhardoin <jacob_ardoin@icloud.com>
2024-02-25 16:30:58 -0600

    pirates

--- kyu8/Pirates are the canons ready.js
+++ kyu8/Pirates are the canons ready.js
@@ -1,1 +1,5 @@
 const cannonsReady = (gunners) => (Object.values(gunners)).includes('nay') ? 'Shiver me timbers!' : 'Fire!'
+
+const cannonsReady = (gunners) => {
+    return Object.values(gunners).every(e => e == 'aye') ? 'Fire!' : 'Shiver me timbers!'
+  }
