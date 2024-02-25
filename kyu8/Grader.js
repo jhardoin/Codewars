7689a3c40988ed302c839154aa392ed4ffa08061
jhardoin <jacob_ardoin@icloud.com>
2024-02-25 16:33:09 -0600

    grader

--- kyu8/Grader.js
+++ kyu8/Grader.js
@@ -5,3 +5,10 @@ function grader(score) {
     if(score >= .7) return 'C'
     else return 'D'
   }
+
+  function grader(score) {
+    return score > 1 || score < .6 ? 'F' :
+           score >= .9 ? 'A' :
+           score >= .8 ? 'B' :
+           score >= .7 ? 'C' : 'D'
+  }
