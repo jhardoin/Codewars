b0bda81c4c178a1c52fbae69232b3a7bad363312
jhardoin <jacob_ardoin@icloud.com>
2024-02-26 20:01:54 -0600

    barking mad

--- kyu8/Barking Mad.js
+++ kyu8/Barking Mad.js
@@ -13,3 +13,16 @@ function Dog (breed) {
   scoobydoo.bark = function() {
     return "Woof";
   };
+
+  class Dog {
+    constructor(breed) {
+        this.breed = breed
+    }
+    
+    bark() {
+        return "Woof"
+    }
+}
+
+let snoopy = new Dog("Beagle")
+let scoobydoo = new Dog("Great Dane")
