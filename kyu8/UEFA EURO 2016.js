9b7c14d4ca6c36e946c71a4d95a9482a782a450b
jhardoin <jacob_ardoin@icloud.com>
2024-02-26 20:04:02 -0600

    uefa

--- kyu8/UEFA EURO 2016.js
+++ kyu8/UEFA EURO 2016.js
@@ -3,3 +3,7 @@ function uefaEuro2016(teams, scores){
     if(scores[0]>scores[1]) return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
     else return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
   }
+
+  function uefaEuro2016(teams, scores){
+    return "At match " + teams[0] + " - " + teams[1] + ", " + ((scores[0] > scores[1]) ? teams[0] + " won!" : (scores[0] < scores[1]) ? teams[1] +" won!":  "teams played draw.");
+   };
