6155e8d06125482b2a4369c17e643f3b9e151b93
jhardoin <jacob_ardoin@icloud.com>
2024-02-28 21:42:40 -0600

    bugs

--- kyu8/Fix the Bugs (Syntax) - My First Kata.js
+++ kyu8/Fix the Bugs (Syntax) - My First Kata.js
@@ -1,1 +1,9 @@
 const myFirstKata = (a, b) => (typeof(a) !== "number" || typeof(b) !== "number") ? false : (a % b) + (b % a)
+
+function myFirstKata(a, b) {
+    if (typeof a !== "number" || typeof(b) !== "number") {
+      return false;
+    } else {
+      return a % b + b % a;
+    }
+  }
