06c428b9eb163c04fc6d32b2ea71c65dba1c947d
jhardoin <jacob_ardoin@icloud.com>
2024-02-28 21:39:36 -0600

    find the slope

--- kyu8/Find the Slope.js
+++ kyu8/Find the Slope.js
@@ -1,1 +1,6 @@
 const slope = p => (p[2] - p[0] == 0) ? 'undefined' : ((p[3] - p[1]) / (p[2] - p[0])).toString()
+
+function slope([ x1, y1, x2, y2 ]) {
+    let slope = (y2 - y1) / (x2 - x1);
+    return Number.isFinite(slope) ? `${slope}` : 'undefined';
+  }
