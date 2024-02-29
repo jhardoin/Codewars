822a454b18f4e1b3bb2e33f2f38337ab3687fdcc
jhardoin <jacob_ardoin@icloud.com>
2024-02-28 21:46:49 -0600

    terminal game

--- kyu8/Grasshopper - Terminal Game 1.js
+++ kyu8/Grasshopper - Terminal Game 1.js
@@ -5,3 +5,11 @@ function Hero (name) {
     this.damage = 5;
     this.experience = 0;
     }
+
+    function Hero (name) {
+        this.name = name || 'Hero';
+        this.position = '00';
+        this.health = 100;
+        this.damage = 5;
+        this.experience = 0;
+      }
