b0b27742fbcaab1518142c1a9ace9d0361597669
jhardoin <jacob_ardoin@icloud.com>
2024-02-29 21:54:33 -0600

    two highest values

--- kyu8/Return two highest values in list.js
+++ kyu8/Return two highest values in list.js
@@ -2,3 +2,7 @@ function twoHighest(arr) {
     let noDupeArr = [...new Set(arr)].sort((a,b) => b - a)
     return noDupeArr.slice(0, 2)
   }
+
+  function twoHighest(arr) {
+    return [...new Set(arr)].sort((a,b) => b - a).slice(0,2)
+  }
