ff94f021948c244813b10eaf4c7b5d07309f7c11
jhardoin <jacob_ardoin@icloud.com>
2024-03-03 15:17:19 -0600

    sum

--- kyu7/Sum of two lowest positive integers.js
+++ kyu7/Sum of two lowest positive integers.js
@@ -12,3 +12,8 @@ function sumTwoSmallestNumbers(numbers) {
     let sorted = numbers.sort((a,b) => a - b)
     return sorted[0] + sorted[1]
   }
+
+  function sumTwoSmallestNumbers(numbers) {  
+    let sorted = numbers.sort((a,b) => a - b)
+    return sorted[0] + sorted[1]
+  }
