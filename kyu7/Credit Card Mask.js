1b3ec8ec51def561a887b3c7ee9a4384cd94d884
jhardoin <jacob_ardoin@icloud.com>
2024-03-04 21:33:58 -0600

    credit card mask

--- kyu7/Credit Card Mask.js
+++ kyu7/Credit Card Mask.js
@@ -1,3 +1,8 @@
 const maskify = cc => cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4)
 
 const maskify = cc => cc.length < 4 ? cc : '#'.repeat(cc.length - 4) + cc.slice(cc.length - 4)
+
+function maskify(cc) {
+    return cc < 5 ? cc : [...cc].map((e,i) => i < cc.length - 4 ? '#' : e).join('')
+   }
+   
