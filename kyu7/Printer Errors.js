e5126dc056db70f11d03be38f5a8a2ba0a4d8187
jhardoin <jacob_ardoin@icloud.com>
2024-03-06 16:04:19 -0600

    Printer errors

--- kyu7/Printer Errors.js
+++ kyu7/Printer Errors.js
@@ -4,3 +4,7 @@ function printerError(s) {
     let printer = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',]
     return `${[...s].filter(e => !printer.includes(e)).length}/${s.length}`
   }
+
+  function printerError(s) {
+    return `${(s.length - [...s].filter(e => 'abcdefghijklm'.includes(e)).length)}/${s.length}`
+}
