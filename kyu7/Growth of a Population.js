a65847701a6a96ce22c843bf0a746a7ec8a4f2f5
jhardoin <jacob_ardoin@icloud.com>
2024-03-06 15:58:23 -0600

    growth of a population

--- kyu7/Growth of a Population.js
+++ kyu7/Growth of a Population.js
@@ -4,3 +4,12 @@ function nbYear(p0, percent, aug, p) {
     }
   return count
 }
+
+function nbYear(p0, percent, aug, p) {
+  let years = 0
+  while(p0 < p){
+    p0 += Math.floor(p0 * percent/100) + aug
+    years++
+  }
+return years
+}
