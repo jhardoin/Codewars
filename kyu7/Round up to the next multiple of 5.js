db938a85dfe2e2842346a3f71cd2c45131c3815d
jhardoin <jacob_ardoin@icloud.com>
2024-03-13 18:06:12 -0500

    Round up

--- kyu7/Round up to the next multiple of 5.js
+++ kyu7/Round up to the next multiple of 5.js
@@ -3,3 +3,8 @@ const roundToNext5 = n => Math.ceil(n / 5) * 5
 function roundToNext5(n){
     return Math.ceil(n/5) * 5
   }
+
+  function roundToNext5(n){
+    while(n % 5 !== 0)n++
+    return n
+  }
