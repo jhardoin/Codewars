d533f9035e81b5fee6a79f78df99d1e086a8e3c1
jhardoin <jacob_ardoin@icloud.com>
2024-03-12 20:37:44 -0500

    Find the middle element

--- kyu7/Find the middle element.js
+++ kyu7/Find the middle element.js
@@ -8,3 +8,7 @@ const gimme = triplet => triplet.map((e,i) => [e,i]).sort((a,b) => a[0] - b[0])[
 function gimme(a) {
     return a.indexOf(a.concat().sort((a, b) => a - b)[1])
   }
+
+  function gimme (triplet) {
+    return triplet.indexOf([...triplet].sort((a, b) => a - b)[1])
+  }
