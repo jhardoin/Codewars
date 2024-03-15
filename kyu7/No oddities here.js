d8fa76bed9c6465d901f0051a2f746f57acc4a89
jhardoin <jacob_ardoin@icloud.com>
2024-03-14 19:31:27 -0500

    no oddities here

--- kyu7/No oddities here.js
+++ kyu7/No oddities here.js
@@ -5,3 +5,7 @@ const noOdds = values => values.filter(e => !(e % 2))
 function noOdds( values ){
     return values.filter(e => !(e % 2))
 }
+
+function noOdds( values ){
+    return values.filter(e => e % 2 == 0)
+  }
