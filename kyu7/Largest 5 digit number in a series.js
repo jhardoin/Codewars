cf779e40e204d9ba19712e6a5aa860f6a07a33df
jhardoin <jacob_ardoin@icloud.com>
2024-03-14 19:47:02 -0500

    Largest 5 digit number in a series

--- kyu7/Largest 5 digit number in a series.js
+++ kyu7/Largest 5 digit number in a series.js
@@ -3,3 +3,7 @@ const solution = d =>  Math.max( ...d.split('').map( (x,i,a)=> +(a.slice(i, i+5)
 function solution(digits){
     return +digits.split('').map((v,i,arr)=>arr.slice(i,i+5).join('')).sort((a,b)=>+b-+a)[0]
   }
+
+  function solution(digits){
+    return +digits.split('').map((e,i,a)=>a.slice(i,i+5).join('')).sort((a,b)=>+b-+a)[0]
+  }
