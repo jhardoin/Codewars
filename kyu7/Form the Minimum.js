90e7bee0a66fe19a9f4a683aab78348435286ddc
jhardoin <jacob_ardoin@icloud.com>
2024-03-17 17:14:37 -0500

    form the minimum

--- kyu7/Form the Minimum.js
+++ kyu7/Form the Minimum.js
@@ -1,3 +1,7 @@
 const minValue = v => Number(Array.from(new Set(v)).sort((a,b) => a - b).join(''))
 
 const minValue = values => +[...new Set(values)].sort((a,b) => a - b).join('')
+
+function minValue(values){
+    return +[...new Set(values)].sort((a,b) => a - b).join('')
+  }
