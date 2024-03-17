3bd6535ca1e2324f526be675b53d65aa91a262b7
jhardoin <jacob_ardoin@icloud.com>
2024-03-16 21:53:34 -0500

    Javascript array filter

--- kyu7/Javascript array filter.js
+++ kyu7/Javascript array filter.js
@@ -1,3 +1,7 @@
 const getEvenNumbers = numbersArray => numbersArray.filter(e => e % 2 == 0)
 
 const getEvenNumbers = numbersArray => numbersArray.filter(e => e % 2 == 0)
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(e => e % 2 == 0)
+  }
