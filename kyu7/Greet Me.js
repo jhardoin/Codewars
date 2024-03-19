ffe2536029a2a63e0195002fa1919f2de89676e6
jhardoin <jacob_ardoin@icloud.com>
2024-03-18 21:38:52 -0500

    Greet me

--- kyu7/Greet Me.js
+++ kyu7/Greet Me.js
@@ -1,1 +1,5 @@
 const greet = name => `Hello ${name[0].toUpperCase() + name.toLowerCase().slice(1)}!`
+
+var greet = function(name) {
+    return `Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+  };
