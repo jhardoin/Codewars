9de746330ab84f86a7aac1f3ad2be3f7c50a97fc
jhardoin <jacob_ardoin@icloud.com>
2024-03-18 21:37:21 -0500

    predict your age

--- kyu7/Predict your age.js
+++ kyu7/Predict your age.js
@@ -2,3 +2,7 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
     let ageSqr = [age1 * age1, age2 * age2, age3 * age3, age4 * age4, age5 * age5, age6 * age6, age7 * age7, age8 * age8]
     return Math.floor((Math.sqrt(ageSqr.reduce((a,b) => a + b)))/2)
   }
+
+  function predictAge(...arr){
+    return Math.floor(Math.sqrt(arr.map(a => a * a).reduce((b,c) => b + c)) / 2)
+  }
