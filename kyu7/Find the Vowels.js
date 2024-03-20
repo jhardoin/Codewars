2e8c9c868242c143c9e4f362884f5d31849c844b
jhardoin <jacob_ardoin@icloud.com>
2024-03-19 19:51:15 -0500

    find the vowels

--- kyu7/Find the Vowels.js
+++ kyu7/Find the Vowels.js
@@ -3,3 +3,7 @@ function vowelIndices(word){
     let arr = word.toLowerCase().split('').map((e, i) => (vowels.includes(e) ? i + 1 : -1))
     return arr.filter(e => e !== -1)
   }
+
+  function vowelIndices(word){
+    return [...word].map((e,i) => 'aeiouyAEIOUY'.includes(e) ? i + 1 : -1).filter(e => e > 0)
+  }
