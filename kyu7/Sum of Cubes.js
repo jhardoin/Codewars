17a83f83d5bd104fe2e7d2eb72732d75ba966d09
jhardoin <jacob_ardoin@icloud.com>
2024-03-19 19:40:39 -0500

    sum of cubes

--- kyu7/Sum of Cubes.js
+++ kyu7/Sum of Cubes.js
@@ -1,1 +1,5 @@
 const sumCubes = n => Array.from({length: n}, (v,i) => i + 1).map(e => e ** 3).reduce((a,b) => a+b)
+
+function sumCubes(n){
+    return Array.from({length : n + 1}, (e,i) => i ** 3).reduce((a,b) => a + b, 0)
+  }
