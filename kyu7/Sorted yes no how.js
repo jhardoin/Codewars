af41475063a00fd4f822e688bb5f62cd202ca751
jhardoin <jacob_ardoin@icloud.com>
2024-03-19 19:47:08 -0500

    sorted yes no how

--- kyu7/Sorted yes no how.js
+++ kyu7/Sorted yes no how.js
@@ -7,3 +7,12 @@ function isSortedAndHow(array) {
       return 'no';
     }
   }
+
+  function isSortedAndHow(array) {
+    const ascendingSorted = [...array].sort((a, b) => a - b);
+    const descendingSorted = [...array].sort((a, b) => b - a);
+
+  return JSON.stringify(array) == JSON.stringify(ascendingSorted) ? 'yes, ascending' :
+         JSON.stringify(array) == JSON.stringify(descendingSorted) ? 'yes, descending' : 'no'
+ 
+}
