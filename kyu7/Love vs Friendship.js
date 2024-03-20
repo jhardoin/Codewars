6615d53509b5e86cee9c20f18990cd8b23b10b40
jhardoin <jacob_ardoin@icloud.com>
2024-03-20 16:18:30 -0500

    love vs friendship

--- kyu7/Love vs Friendship.js
+++ kyu7/Love vs Friendship.js
@@ -1,1 +1,5 @@
 const wordsToMarks = s => s.split('').map(e => e.charCodeAt(0) - 96).reduce((a,b) => a + b)
+
+function wordsToMarks(string){
+    return [...string].map(e => e.charCodeAt() - 96).reduce((a,b) => a + b, 0)
+  }
