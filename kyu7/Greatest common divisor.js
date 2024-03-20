c63a476c45c31255b86a1ebc6b99390d4afc92b4
jhardoin <jacob_ardoin@icloud.com>
2024-03-20 16:25:11 -0500

    greatest common divisor

--- kyu7/Greatest common divisor.js
+++ kyu7/Greatest common divisor.js
@@ -2,3 +2,11 @@ function mygcd(x,y){
     if(y == 0) return x
     else return mygcd(y, x % y)
   }
+
+  function mygcd(x,y){
+    for(let i = x ; i > 0;i -= 1){
+      if (x%i === 0 && y%i===0){
+        return i
+      }
+    } 
+  }
