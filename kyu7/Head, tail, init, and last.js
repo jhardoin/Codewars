64648c3f6806b4793517bebab7b72b35122d922a
jhardoin <jacob_ardoin@icloud.com>
2024-03-22 14:45:39 -0500

    head tail init and last

--- kyu7/Head, tail, init, and last.js
+++ kyu7/Head, tail, init, and last.js
@@ -2,3 +2,8 @@ const head = x => x[0]
 const tail = x => x.slice(1)
 const init = x => x.slice(0, x.length - 1)
 const last = x => x[x.length-1]
+
+let head = arr => arr[0];
+let tail = arr => arr.slice(1);
+let init = arr => arr.slice(0, arr.length-1);
+let last = arr => arr[arr.length-1];
