ba3ad8eb323a0d49f38e214b508b2d627b49dc67
jhardoin <jacob_ardoin@icloud.com>
2024-03-23 19:44:17 -0500

    my language skills

--- kyu7/My language skills.js
+++ kyu7/My language skills.js
@@ -1,3 +1,7 @@
 function myLanguages(results) {
     return Object.keys(results).filter(e => results[e] > 59).sort((a,b) => results[b] - results[a])
   }
+
+  function myLanguages(results) {
+    return Object.keys(results).filter(e => results[e] > 59).sort((a,b) => results[b] - results[a]);
+  }
