517aacdc21b7c258090e6b73937db5be85177b93
jhardoin <jacob_ardoin@icloud.com>
2024-03-23 19:41:09 -0500

    Switcheroo

--- kyu7/Switcheroo.js
+++ kyu7/Switcheroo.js
@@ -1,1 +1,5 @@
 const switcheroo = x => x.split('').map(e => e == 'a' ? 'b' : e == 'b' ? 'a' : e).join('')
+
+function switcheroo(x){
+    return [...x].map(e => e == 'a' ? 'b' : e == 'b' ? 'a' : e).join('')
+  }
