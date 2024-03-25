73f49e94c7ce04e441dc9b32d6fd349890055b5f
jhardoin <jacob_ardoin@icloud.com>
2024-03-24 22:01:36 -0500

    Minimize sum of array

--- kyu7/Minimize Sum of Array.js
+++ kyu7/Minimize Sum of Array.js
@@ -1,3 +1,16 @@
 function minSum(arr) {
     return arr.sort((a,b) => a - b).reduce((prev,curr) => prev + curr * arr.pop() ,0)
   }
+
+  function minSum(arr) {
+    arr = arr.sort((a, b) => a - b);
+    let i = 0;
+    let j = arr.length - 1;
+    let sum = 0;
+    while (i < j ) {
+      sum += arr[i] * arr[j];
+      i++;
+      j--;
+    }
+    return sum;
+  }
