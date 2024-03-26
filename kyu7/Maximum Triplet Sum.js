08ea6e9f550f523b76ac9cec3acc774d45e2b869
jhardoin <jacob_ardoin@icloud.com>
2024-03-25 21:42:38 -0500

    maximum triplet sum

--- kyu7/Maximum Triplet Sum.js
+++ kyu7/Maximum Triplet Sum.js
@@ -1,1 +1,6 @@
 const maxTriSum = n => [...new Set(n)].sort((a,b) => b - a).slice(0, 3).reduce((a,b) => a + b)
+
+function maxTriSum(numbers){
+    let a = [...new Set(numbers)].sort((a,b) => b - a)
+    return a[0] + a[1] + a[2]
+  }
