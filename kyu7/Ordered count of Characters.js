1ce1c8a4a201794985d891e5dbaffcdc8bb0a6f0
jhardoin <jacob_ardoin@icloud.com>
2024-03-29 20:22:48 -0500

    Ordered count of characters

--- kyu7/Ordered count of Characters.js
+++ kyu7/Ordered count of Characters.js
@@ -2,3 +2,8 @@ const orderedCount = (text) =>{
     const arr = [...new Set([...text])]
     return arr.map(el => [el, [...text].filter(e => e === el).length]);
   }
+
+  const orderedCount = function (text) {
+    let a = [...new Set(text)]
+    return a.map(e => [e, [...text].filter(el => el == e).length])
+  }
