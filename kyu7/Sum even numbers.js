279a87508ac03c4752cbc4520116cd72a1bf8b1d
jhardoin <jacob_ardoin@icloud.com>
2024-03-31 22:26:22 -0500

    sum even numbers

--- kyu7/Sum even numbers.js
+++ kyu7/Sum even numbers.js
@@ -1,1 +1,6 @@
 const sumEvenNumbers = input => input.filter(e => e % 2 == 0).reduce((a,b) => a + b, 0)
+
+function sumEvenNumbers(input) {
+    return input.reduce((a,b) => a + (b % 2 == 0 ? b : 0), 0)
+  }
+  
