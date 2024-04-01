4b30a3c771bd76a44b14de4924e6710dad2630f3
jhardoin <jacob_ardoin@icloud.com>
2024-03-31 22:31:35 -0500

    sum of array singles

--- kyu7/Sum of array singles.js
+++ kyu7/Sum of array singles.js
@@ -10,3 +10,8 @@ function repeats(arr){
   const repeats = arr => arr.filter(e => arr.indexOf(e) == arr.lastIndexOf(e)).reduce((a,b) => a + b, 0)
 
   const repeats = arr => arr.filter(e => arr.indexOf(e) == arr.lastIndexOf(e)).reduce((a,b) => a + b, 0)
+
+  function repeats(arr){
+    let a = [...new Set(arr)]
+    return a.map(e => [e, arr.filter(el => el == e).length]).reduce((a,b) => a + (b[1] == 1 ? b[0] : 0), 0)
+  };
