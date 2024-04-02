0ef69ec37982e42e0f961d74d41efa8dce0bd4be
jhardoin <jacob_ardoin@icloud.com>
2024-04-01 22:23:52 -0500

    area of a circle

--- kyu7/Area of a circle.js
+++ kyu7/Area of a circle.js
@@ -1,1 +1,8 @@
 const circleArea = radius => radius <= 0 ? error : Math.PI * radius ** 2
+
+function circleArea(radius) {
+    if (radius <=0) {
+      throw new Error("Error")
+    }
+    return Math.PI*(radius * radius);
+  }
