0e4804750abe71eab35a24c88bd3d8e5f2afc827
jhardoin <jacob_ardoin@icloud.com>
2024-04-01 22:21:52 -0500

    max diff

--- kyu7/max diff - easy.js
+++ kyu7/max diff - easy.js
@@ -1,1 +1,5 @@
 const maxDiff = list => list.length < 2 ? 0 : Math.max(...list) - Math.min(...list)
+
+function maxDiff(list) {
+    return list.length > 0 ? Math.max(...list) - Math.min(...list) : 0
+  }
