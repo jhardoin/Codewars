3a120748aa111479254f14e2b20f45dd739d4470
jhardoin <jacob_ardoin@icloud.com>
2024-04-03 21:52:32 -0500

    alphabetical addition

---
+++ kyu7/Alphabetical Addition.js
@@ -0,0 +1,6 @@
+function addLetters(...letters) {
+    if (letters.length === 0) return 'z';
+    const sum = letters.map(letter => letter.charCodeAt(0) - 96).reduce((a, b) => a + b, 0);
+    const result = (sum % 26) + 96;
+    return String.fromCharCode(result === 96 ? 122 : result);
+}
