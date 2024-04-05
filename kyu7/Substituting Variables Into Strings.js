f81b2534786880478220ef25276820b11f63bff8
jhardoin <jacob_ardoin@icloud.com>
2024-04-04 22:11:21 -0500

    Substituting variables into strings

--- kyu7/Substituting Variables Into Strings.js
+++ kyu7/Substituting Variables Into Strings.js
@@ -1,1 +1,6 @@
 const solution = value => `Value is ${String(value).padStart(5, '0')}`
+
+function solution(value){
+    value = '0'.repeat(5 - value.toString().length) + value
+    return `Value is ${value}`
+  }
