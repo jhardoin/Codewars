c1d02b8eb2a12264fd5b4f60471da7284f457dc1
jhardoin <jacob_ardoin@icloud.com>
2024-04-05 22:18:11 -0500

    Nickname generator

--- kyu7/Nickname Generator.js
+++ kyu7/Nickname Generator.js
@@ -4,3 +4,9 @@ function nicknameGenerator(name){
     else if(vowels.includes(name[2])) return name.substring(0, 4)
     else return name.substring(0, 3)
   }
+
+  
+  function nicknameGenerator(name){
+    return name.length < 4 ? 'Error: Name too short' :
+           'aeiou'.includes(name[2]) ? name.slice(0,4) : name.slice(0,3)
+  }
