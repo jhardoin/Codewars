03fc48aeaf4514a140c43fe792916733da8f3d1a
jhardoin <jacob_ardoin@icloud.com>
2024-04-06 20:01:26 -0500

    automorphic number

--- kyu7/Automorphic Number.js
+++ kyu7/Automorphic Number.js
@@ -3,3 +3,9 @@ function automorphic(n){
     let n2 = (n**2).toString()
     return next.charAt(next.length - 1) == n2.charAt(n2.length - 1) ? "Automorphic" : 'Not!!'
   }
+
+  function automorphic(n){
+    let next = n.toString()
+    let n2 = (n**2).toString()
+    return n2.endsWith(next) ? "Automorphic" : 'Not!!'
+  }
