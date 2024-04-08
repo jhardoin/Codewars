7ef9f3bb0aa01971b9387bf26154d09e1dcedb1d
jhardoin <jacob_ardoin@icloud.com>
2024-04-07 20:58:01 -0500

    Remove all the marked elements of a list

--- kyu7/Remove all the marked elements of a list.js
+++ kyu7/Remove all the marked elements of a list.js
@@ -2,3 +2,7 @@ Array.prototype.remove_ = function(integer_list, values_list){
     return integer_list.filter(e => !values_list.includes(e))
   }
       
+
+  Array.prototype.remove_ = function(integer_list, values_list){
+    return integer_list.filter(e => !values_list.includes(e))
+  }
