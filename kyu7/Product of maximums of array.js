2d1b393b38be8948209f1ad764b3244377e1abe9
jhardoin <jacob_ardoin@icloud.com>
2024-04-09 21:54:40 -0500

    Product of maximims of array

--- kyu7/Product of maximums of array.js
+++ kyu7/Product of maximums of array.js
@@ -1,1 +1,7 @@
 const maxProduct = (numbers, size) => numbers.sort((a,b) => b - a).slice(0, size).reduce((a,b) => a * b)
+
+function maxProduct(numbers, size){
+    numbers.sort((a,b) => b - a)
+    numbers.length = size
+    return numbers.reduce((a,b) => a * b, 1)
+  }
