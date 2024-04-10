2ecf324666ee8cef8e9e0ada6e8a36c8a4917655
jhardoin <jacob_ardoin@icloud.com>
2024-04-09 21:47:45 -0500

    sort out the men from the boys

--- kyu7/Sort out the men from the boys.js
+++ kyu7/Sort out the men from the boys.js
@@ -3,3 +3,10 @@ function menFromBoys(arr){
     let even = [...new Set(arr.filter(e => e % 2 == 0))].sort((a,b) => a - b)
     return even.concat(odd)
   }
+
+  function menFromBoys(arr){
+    let arr2 = [...new Set([...arr])]
+    let men = arr2.filter(e => e % 2 == 0).sort((a,b) => a - b)
+    let boys = arr2.filter(e => e % 2 !== 0).sort((a,b) => b - a)
+    return men.concat(boys)
+  }
