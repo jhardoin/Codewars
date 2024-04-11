c4979da2f9cb9cb5c9a60dce669c80e29a64df5a
jhardoin <jacob_ardoin@icloud.com>
2024-04-10 21:21:34 -0500

    Fixme

--- kyu7/Fixme Get Full Name.js
+++ kyu7/Fixme Get Full Name.js
@@ -10,3 +10,16 @@ class Dinglemouse{
       else return ''
     }
   }
+
+  class Dinglemouse{
+
+    constructor( firstName, lastName ){
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    getFullName(){
+      return `${this.firstName} ${this.lastName}`.trim();
+    }
+    
+  }
