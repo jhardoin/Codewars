9743feb4cff8761a89223099f6bf182cb496c6bd
jhardoin <jacob_ardoin@icloud.com>
2024-04-11 20:20:07 -0500

    sum of integers in a string

--- kyu7/Sum of integers in String.js
+++ kyu7/Sum of integers in String.js
@@ -1,1 +1,8 @@
 const sumOfIntegersInString = s => s.match(/\d+/g) == null ? 0 : s.match(/\d+/g).reduce((a,b) => +a + +b, 0)
+
+function sumOfIntegersInString(s) {
+    const matches = s.match(/\d+/g)
+    if (!matches) return 0
+
+    return matches.reduce((sum, numStr) => sum + parseInt(numStr), 0)
+}
