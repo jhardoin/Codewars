a8a1d4ae30efdea0673815c0284615c294a6e669
jhardoin <jacob_ardoin@icloud.com>
2024-04-14 22:34:47 -0500

    array diff

--- kyu6/Array Diff.js
+++ kyu6/Array Diff.js
@@ -5,3 +5,7 @@ const arrayDiff = (a, b) => a.filter(e => !b.includes(e))
 const arrayDiff = (a, b) => a.filter(e => !b.includes(e))
 
 const arrayDiff = (a, b) => a.filter(e => !b.includes(e))
+
+function arrayDiff(a, b) {
+    return a.filter(e => !b.includes(e))
+  }
