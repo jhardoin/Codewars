66ed191ef581df6dbcba3b10b62f43b5390371bd
jhardoin <jacob_ardoin@icloud.com>
2024-04-15 21:51:50 -0500

    replace with alphabet position

--- kyu6/Replace with Alphabet Position.js
+++ kyu6/Replace with Alphabet Position.js
@@ -3,3 +3,7 @@ const alphabetPosition = (text) => [...text.replace(/[^a-z]/gi, '').toLowerCase(
 function alphabetPosition(text) {
     return [...text.split(' ').join('')].filter(e => parseInt(e, 36) - 9 > 0).map(e => parseInt(e, 36) - 9).join(' ')
   }
+
+  function alphabetPosition(text) {
+    return [...text].map(e => e == ' ' ? '' : e.toLowerCase().charCodeAt() - 96).filter(e => e > 0 && e < 27).join(' ')
+  }
