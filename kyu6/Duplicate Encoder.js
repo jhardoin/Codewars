66d4d859ed9dd4e222e1eff1b94e55785f3e4d28
jhardoin <jacob_ardoin@icloud.com>
2024-04-15 21:41:31 -0500

    duplicate encoder

--- kyu6/Duplicate Encoder.js
+++ kyu6/Duplicate Encoder.js
@@ -6,3 +6,7 @@ function duplicateEncode(word){
     let w = word.toLowerCase()
     return [...w].map(e => w.indexOf(e) == w.lastIndexOf(e) ? '(' : ')').join('')
 }
+
+function duplicateEncode(word){
+    return [...word.toLowerCase()].map((e,i,a) => a.indexOf(e) === a.lastIndexOf(e) ? '(' : ')').join('')
+}
