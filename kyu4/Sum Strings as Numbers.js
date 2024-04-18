d9fc20fe3287fb3c23d5d235065a29be5c82e9c9
jhardoin <jacob_ardoin@icloud.com>
2024-04-17 20:35:24 -0500

    sum strings a numbers

--- kyu4/Sum Strings as Numbers.js
+++ kyu4/Sum Strings as Numbers.js
@@ -1,1 +1,5 @@
 const sumStrings = (a,b) =>`${+a + +b}`.length <= 15 ? `${+a + +b}` : String(BigInt(a) + BigInt(b))
+
+function sumStrings(a,b) { 
+    return (BigInt(a) + BigInt(b)).toString()
+  }
