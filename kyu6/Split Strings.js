1bb7022723694aaae415e5741c04ca1f80e59611
jhardoin <jacob_ardoin@icloud.com>
2024-04-18 22:20:14 -0500

    Split strings

--- kyu6/Split Strings.js
+++ kyu6/Split Strings.js
@@ -15,3 +15,12 @@ function solution(str){
   }
 return result
 }
+
+function solution(str){
+  let arr = []
+  if(str.length % 2 !== 0) str = str + '_'
+  for(let i = 0; i < str.length; i += 2){
+    arr.push(str[i] + str[i + 1])
+  }
+ return arr
+}
