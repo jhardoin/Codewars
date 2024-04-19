37cb6d590ce7dfd96f39ba25908a1d2a9857c687
jhardoin <jacob_ardoin@icloud.com>
2024-04-19 17:55:51 -0500

    Bouncing balls

---
+++ kyu6/Bouncing balls.js
@@ -0,0 +1,11 @@
+function bouncingBall(h,  bounce,  window) {
+    if(h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1
+    let count = 0
+    while(h > window){
+      count++
+      h = h * bounce
+      if (h > window) count++
+    }
+    return count
+  }
+  
