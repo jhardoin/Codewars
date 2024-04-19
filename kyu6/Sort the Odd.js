72d2acf244cb828ca082a895776ffc8dd1d8a01f
jhardoin <jacob_ardoin@icloud.com>
2024-04-18 22:29:11 -0500

    Sort the odd

--- kyu6/Sort the Odd.js
+++ kyu6/Sort the Odd.js
@@ -2,3 +2,8 @@ function sortArray(array) {
     let oddSorted = array.filter(e => e % 2).sort((a,b) => a - b)
     return array.map(e => e % 2 ? oddSorted.shift() : e)
 }
+
+function sortArray(array) {
+    let odd = array.filter(e => e % 2 !== 0).sort((a, b) => a - b)
+    return array.map(e => e % 2 === 0 ? e : odd.shift())
+  }
