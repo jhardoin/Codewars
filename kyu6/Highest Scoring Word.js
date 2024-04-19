88c857d8281db5b73e6ce284e96b2fb55b669a89
jhardoin <jacob_ardoin@icloud.com>
2024-04-19 16:46:00 -0500

    Highest scoring word

--- kyu6/Highest Scoring Word.js
+++ kyu6/Highest Scoring Word.js
@@ -4,3 +4,8 @@ function high(x){
     let index = reducedArray.indexOf(Math.max(...reducedArray))
     return x.split(' ')[index]
   }
+
+  function high(s){
+    let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0))
+    return s.split(' ')[as.indexOf(Math.max(...as))];
+  }
