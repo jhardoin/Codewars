690a056dee88328249b34e7258595261a9953ad9
jhardoin <jacob_ardoin@icloud.com>
2024-04-19 17:05:18 -0500

    Build tower

---
+++ kyu6/build tower.js
@@ -0,0 +1,9 @@
+function towerBuilder(nFloors) {
+    let tower = []
+    for (let i = 1; i <= nFloors; i++) {
+        let spaces = ' '.repeat(nFloors - i)
+        let blocks = '*'.repeat(2 * i - 1)
+        tower.push(spaces + blocks + spaces)
+    }
+    return tower
+}
