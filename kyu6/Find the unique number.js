48039a871b16757a9b782f24134ed435258c1eba
jhardoin <jacob_ardoin@icloud.com>
2024-04-18 21:59:35 -0500

    Find the unique number

--- kyu6/Find the unique number.js
+++ kyu6/Find the unique number.js
@@ -8,3 +8,8 @@ function findUniq(arr) {
     arr.sort((a, b) => a - b);
     return arr[0] !== arr[1] ? arr[0] : arr[arr.length - 1]
   }
+
+  function findUniq(arr) {
+    return +arr.filter(e => arr.indexOf(e) == arr.lastIndexOf(e)).join('')
+  }
+  
