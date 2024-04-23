5ccef581d5f3bc0ff1605def76f42eaffa9b0035
jhardoin <jacob_ardoin@icloud.com>
2024-04-23 15:08:12 -0500

    Array Helpers

--- kyu6/Array Helpers.js
+++ kyu6/Array Helpers.js
@@ -20,3 +20,10 @@ Array.prototype.square = function (){
   Array.prototype.odd = function (){
     return this.filter(e => e % 2)
   }
+
+Array.prototype.square = function square(a){return this.map(e => e ** 2)}
+Array.prototype.cube = function cube(a){return this.map(e => e ** 3)}
+Array.prototype.average = function average(a){return this.reduce((a,b) => a + b, 0)/this.length}
+Array.prototype.sum = function sum(a){return this.reduce((a,b) => a + b, 0)}
+Array.prototype.even = function even(a){return this.filter(e => e % 2 == 0)}
+Array.prototype.odd = function odd(a){return this.filter(e => e % 2 !== 0)}
