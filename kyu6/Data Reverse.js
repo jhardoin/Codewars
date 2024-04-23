697ceb383cd9f929c44b9d6c6896852ea3f9c5b5
jhardoin <jacob_ardoin@icloud.com>
2024-04-22 21:14:33 -0500

    Data result

--- kyu6/Data Reverse.js
+++ kyu6/Data Reverse.js
@@ -5,3 +5,12 @@ function dataReverse(data) {
     }
     return result.reverse().flat()
   }
+
+  function dataReverse(data) {
+    let arr = []
+    
+    for(let i = 0; i < data.length; i += 8){
+      arr.push(data.slice(i, i + 8))
+    }
+    return arr.reverse().flat()
+  }
