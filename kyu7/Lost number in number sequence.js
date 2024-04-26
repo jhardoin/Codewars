3622d8bb00513792f504f949975555fc42a6acba
jhardoin <jacob_ardoin@icloud.com>
2024-04-25 22:11:14 -0500

    Lost number in a sequence

--- kyu7/Lost number in number sequence.js
+++ kyu7/Lost number in number sequence.js
@@ -1,3 +1,7 @@
 function findDeletedNumber(arr, mixArr) {
     return +arr.filter(e => !mixArr.includes(e)).join('') || 0
   }
+
+  function findDeletedNumber(arr, mixArr) {
+    return +arr.filter(e => !mixArr.includes(e)).join('') || 0
+  }
