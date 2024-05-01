c400e5d307a2ce64bd1f6402021737288beb527a
jhardoin <jacob_ardoin@icloud.com>
2024-05-01 18:44:54 -0500

    A rule of divisibility by 7

---
+++ kyu7/A rule of divisibility by 7.js
@@ -0,0 +1,10 @@
+function seven(m) {
+    let count = 0
+    
+    while(m.toString().length > 2){
+      m = (+m.toString().slice(0, -1)) - (2 * +m.toString().slice(-1))
+      count++
+    }
+   
+  return [m, count]
+}
