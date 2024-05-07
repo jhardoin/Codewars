2728f14632d098f38c452f1f4af9c8fcf9d8765d
jhardoin <jacob_ardoin@icloud.com>
2024-05-07 18:23:22 -0500

    Training JS 13

---
+++ kyu8/Training JS 13 Number Object.js
@@ -0,0 +1,18 @@
+function whatNumberIsIt(n){
+    if (isNaN(n)) {
+      if (n === Number.NaN) {
+        return "Input number is Number.NaN"
+      } else {
+        return "Input number is Number.NaN"
+      }
+    } else if (n === Number.MAX_VALUE) {
+      return "Input number is Number.MAX_VALUE"
+    } else if (n === Number.MIN_VALUE) {
+      return "Input number is Number.MIN_VALUE"
+    } else if (n === Number.NEGATIVE_INFINITY) {
+      return "Input number is Number.NEGATIVE_INFINITY"
+    } else if (n === Number.POSITIVE_INFINITY) {
+      return "Input number is Number.POSITIVE_INFINITY"
+    } else {
+      return `Input number is ${n}`  }
+  }
