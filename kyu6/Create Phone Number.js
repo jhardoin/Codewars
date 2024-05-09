64f510d95f5cb82177114ff7b1cd1604a4062492
jhardoin <jacob_ardoin@icloud.com>
2024-05-09 15:12:55 -0500

    Create Phone Number

--- kyu6/Create Phone Number.js
+++ kyu6/Create Phone Number.js
@@ -8,3 +8,7 @@ function createPhoneNumber(numbers){
   function createPhoneNumber(numbers){
     return  `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
   }
+
+  function createPhoneNumber(numbers){
+    return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
+}
