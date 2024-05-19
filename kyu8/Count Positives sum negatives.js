4d7d07ffb0e56fb62e92b9c8961be207709d1b72
jhardoin <jacob_ardoin@icloud.com>
2024-05-19 14:56:42 -0500

    count positives sum negative

--- kyu8/Count Positives sum negatives.js
+++ kyu8/Count Positives sum negatives.js
@@ -15,3 +15,13 @@ function countPositivesSumNegatives(input) {
       return input && input.length ? [input.filter(e => e > 0).length, input.filter(e => e < 0).reduce((a, b) => a + b, 0)] : []
   }
   
+
+  function countPositivesSumNegatives(input) {
+    if(!input || input.length == 0) return []
+    let pos = 0
+    let neg = 0
+    for(let i = 0; i < input.length; i++){
+      input[i] > 0 ? pos++ : neg += input[i] 
+    }
+    return [pos,neg]
+  }
