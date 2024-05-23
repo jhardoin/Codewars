b8c46c8cedc2a028a2146df254a15469a9f131f6
jhardoin <jacob_ardoin@icloud.com>
2024-05-22 19:37:29 -0500

    Personalized Message

--- kyu8/Grasshopper - Personalized Message.js
+++ kyu8/Grasshopper - Personalized Message.js
@@ -11,3 +11,7 @@ const greet = (name, owner) => owner == name ? 'Hello boss' : 'Hello guest'
 function greet (name, owner) {
     return name == owner ? 'Hello boss' : 'Hello guest'
   }
+
+  function greet (name, owner) {
+    return name == owner ? `Hello boss` : 'Hello guest'
+  }
