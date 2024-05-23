e91b6cc6f5a2b5080801f4fc9ac821c40a1a90b0
jhardoin <jacob_ardoin@icloud.com>
2024-05-22 19:35:42 -0500

    If you cant sleep, just count sheep

--- kyu8/If you can't sleep, just count sheep.js
+++ kyu8/If you can't sleep, just count sheep.js
@@ -10,3 +10,12 @@ var countSheep = function (num){
   var countSheep = num => num == 0 ? '' : Array.from({length:num}, (v,i) => (i+1) + ' sheep...').join('')
 
   var countSheep = num => [...Array(num)].map((e,i) => (i+1) + ' sheep...').join('')
+
+  var countSheep = function (num){
+    if(num == 0) return ''
+    let a = []
+    for(let i = 0; i < num; i++){
+      a.push(`${i+1} sheep...`)
+    }
+    return a.join('')
+  }
