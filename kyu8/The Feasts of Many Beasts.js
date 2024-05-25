e41e35bddb4e23308791b613a3c04e7b7747b786
jhardoin <jacob_ardoin@icloud.com>
2024-05-24 20:58:25 -0500

    The feasts of Many beasts

--- kyu8/The Feasts of Many Beasts.js
+++ kyu8/The Feasts of Many Beasts.js
@@ -11,3 +11,7 @@ function feast(beast, dish) {
 function feast(beast, dish) {
     return beast[0] == dish[0] && beast[beast.length - 1] == dish[dish.length - 1]
   }
+
+  function feast(beast, dish) {
+    return beast[0] == dish[0] && beast[beast.length - 1] == dish[dish.length - 1]
+}
