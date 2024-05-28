cc3e2075d80d9c10b9c0615ca2a27faaea598788
jhardoin <jacob_ardoin@icloud.com>
2024-05-28 12:11:27 -0500

    Do i get a bonus

--- kyu8/Do I get a Bonus.js
+++ kyu8/Do I get a Bonus.js
@@ -11,3 +11,7 @@ function bonusTime(salary, bonus) {
 function bonusTime(salary, bonus) {
     return bonus ? '£' + salary * 10 : '£' + salary
   }
+
+  function bonusTime(salary, bonus) {
+    return bonus ? `£${salary * 10}` : `£${salary}`
+}
