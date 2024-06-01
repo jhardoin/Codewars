2c25ee021457fdd9e4922760a1eaa8eda75a72c0
jhardoin <jacob_ardoin@icloud.com>
2024-05-31 19:31:47 -0500

    Name shuffer

--- kyu8/Name Shuffler.js
+++ kyu8/Name Shuffler.js
@@ -12,3 +12,7 @@ function nameShuffler(str){
   function nameShuffler(str){
     return str.split(' ').reverse().join(' ')
 }
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
