5a3b721fb851d1ef8ab0fd79423477d025f17e1b
jhardoin <jacob_ardoin@icloud.com>
2024-06-06 20:58:42 -0500

    Categorize new member

--- kyu7/Categorize New Member.js
+++ kyu7/Categorize New Member.js
@@ -9,3 +9,7 @@ function openOrSenior(data){
   function openOrSenior(data){
     return data.map(e => e[0] >= 55 && e[1] > 7 ? 'Senior' : 'Open')
   }
+
+  function openOrSenior(data){
+    return data.map(e => e[0] >= 55 && e[1] > 7 ? 'Senior' : 'Open') 
+}
