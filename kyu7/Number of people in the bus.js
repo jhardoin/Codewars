3b9becb680aef5971861d54305c2c529b55e3325
jhardoin <jacob_ardoin@icloud.com>
2024-06-07 14:31:58 -0500

    Number of people in the bus

--- kyu7/Number of people in the bus.js
+++ kyu7/Number of people in the bus.js
@@ -5,3 +5,7 @@ var number = busStops => busStops.map(e => e[0] - e[1]).reduce((a,b) => a + b)
 var number = function(busStops){
     return busStops.reduce((a,b) => a + b[0] - b[1], 0)
   }
+
+  var number = function(busStops){
+    return busStops.reduce((a,b) => a + (b[0] - b[1]), 0)
+}
