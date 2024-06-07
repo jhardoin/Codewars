814eee030235ad6b35ee229e0fa9477a35774c01
jhardoin <jacob_ardoin@icloud.com>
2024-06-07 14:30:05 -0500

    Ones and zeros

--- kyu7/Ones and Zeros.js
+++ kyu7/Ones and Zeros.js
@@ -3,3 +3,7 @@ const binaryArrayToNumber = arr => parseInt(arr.join(''), 2)
 const binaryArrayToNumber = arr => {
     return parseInt(arr.join(""), 2)
   }
+
+  const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(''), 2)
+}
