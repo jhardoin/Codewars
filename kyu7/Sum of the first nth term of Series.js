265090adfa59ffd7943833a9bacab998104f98b8
jhardoin <jacob_ardoin@icloud.com>
2024-06-08 17:03:57 -0500

    Sum the first nth term of series

--- kyu7/Sum of the first nth term of Series.js
+++ kyu7/Sum of the first nth term of Series.js
@@ -5,3 +5,11 @@ function SeriesSum(n){
     }
     return result.toFixed(2)
   }
+
+  function SeriesSum(n) {
+    let sum = 0
+    for(let i = 0; i < n; i++){
+      sum += 1 / (3 * i + 1)
+    }
+    return sum.toFixed(2)
+  }
