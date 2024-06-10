379a0eae7ded403b8fcbedf53d4fa77546cdac82
jhardoin <jacob_ardoin@icloud.com>
2024-06-10 13:12:25 -0500

    FActorial

--- kyu7/Factorial0.js
+++ kyu7/Factorial0.js
@@ -1,1 +1,7 @@
 const factorial = n => Array.from({length:n}, (e,i) => i + 1).reduce((a,b) => a * b, 1)
+
+function factorial(n){
+    if (n < 0 || n > 12) throw error
+    if(n == 0) return 1
+    return Array.from({length:n}, (e,i) => i + 1).reduce((a,b) => a * b, 1)
+}
