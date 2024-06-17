5a3692171a8a4f9adfbac98a0bc26a92e1743eb7
jhardoin <jacob_ardoin@icloud.com>
2024-06-17 17:54:12 -0500

    Does my number look big in this

--- kyu6/Does my number look big in this.js
+++ kyu6/Does my number look big in this.js
@@ -5,3 +5,8 @@ function narcissistic(value) {
 function narcissistic(value) {
     return value.toString().split('').map((e,i,a) => e ** a.length).reduce((a,b) => a + b, 0) == value
 } 
+
+function narcissistic(value) {
+    return value.toString().split('').reduce((a,b) => a + (b ** value.toString().length), 0) == value
+  }
+  
