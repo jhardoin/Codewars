1786f0875b57332e10c5dca56ddc4eeb5ba73762
jhardoin <jacob_ardoin@icloud.com>
2024-06-22 19:53:19 -0500

    Tribonacci Sequence

--- kyu6/Tribonacci Sequence.js
+++ kyu6/Tribonacci Sequence.js
@@ -8,3 +8,10 @@ function tribonacci(signature, n) {
     }
     return result
 }
+
+function tribonacci(signature,n){
+    for(let i = 3; i < n; i++){
+      signature.push(signature[i - 3] + signature[i - 2] + signature[i - 1])
+    }
+    return signature.slice(0,n)
+}
