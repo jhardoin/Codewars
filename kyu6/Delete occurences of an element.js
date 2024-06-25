bc20a5a3cb8d4bf574aa138d82588fa75923ef07
jhardoin <jacob_ardoin@icloud.com>
2024-06-25 11:49:34 -0500

    Delete pccurences of an element

--- kyu6/Delete occurences of an element.js
+++ kyu6/Delete occurences of an element.js
@@ -2,3 +2,8 @@ function deleteNth(arr, n) {
     const counts = {};
     return arr.filter(e => (counts[e] = (counts[e] || 0) + 1) <= n);
 }
+
+const deleteNth = (a, x) => {
+    let m = {}
+    return a.filter(e => (m[e] = m[e] + 1 || 1) <= x )
+  }
