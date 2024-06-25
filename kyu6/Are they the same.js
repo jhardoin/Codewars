fed144084d46dd3492c840465953cbebe8f97c2b
jhardoin <jacob_ardoin@icloud.com>
2024-06-24 19:23:15 -0500

    Are they the same

--- kyu6/Are they the same.js
+++ kyu6/Are they the same.js
@@ -4,3 +4,10 @@ function comp(array1, array2){
     let a2 = array2.map(e => Math.sqrt(e)).sort((a,b) => a - b).join('')
     return a1 == a2
   }
+
+  function comp(array1, array2){
+    if(!array1 || !array2) return false
+    let unSqrd = array2.map(e => Math.sqrt(e)).sort((a,b) => a - b)
+    let sorted = array1.sort((a,b) => a - b)
+    return  JSON.stringify(unSqrd) == JSON.stringify(sorted)
+}
--- kyu6/build tower.js
+++ kyu6/build tower.js
@@ -7,3 +7,4 @@ function towerBuilder(nFloors) {
     }
     return tower
 }
+
