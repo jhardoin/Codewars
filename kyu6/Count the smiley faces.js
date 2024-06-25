1d8a117f3738699c2608df8ea858813d39a1e9de
jhardoin <jacob_ardoin@icloud.com>
2024-06-25 11:44:42 -0500

    Count the smiley faces

--- kyu6/Count the smiley faces.js
+++ kyu6/Count the smiley faces.js
@@ -2,3 +2,8 @@ function countSmileys(arr) {
     let smileys = [':)', ':D', ';)', ';D', ':-)', ':-D', ';-)', ';-D', ':~)', ':~D', ';~)', ';~D'];
     return arr.filter(e => smileys.includes(e)).length;
   }
+
+  function countSmileys(arr) {
+    let smiley = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"]
+    return arr.filter(e => smiley.includes(e)).length
+  }
