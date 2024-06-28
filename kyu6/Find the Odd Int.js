17ecd5c382ceda56e84223ac0657c2922b40f1ce
jhardoin <jacob_ardoin@icloud.com>
2024-06-27 19:00:57 -0500

    Find the odd int

--- kyu6/Find the Odd Int.js
+++ kyu6/Find the Odd Int.js
@@ -27,3 +27,7 @@ function findOdd(A) {
   function findOdd(A) {
     return A.find((e,i,a) => a.filter(el => e == el).length % 2 !== 0)
 }
+
+function findOdd(A) {
+  return A.find((e,i,a) => a.filter(el => e == el).length % 2 !== 0)
+}
