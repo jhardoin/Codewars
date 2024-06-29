5324104504de4abdb841dc6cc60e3da7bf863003
jhardoin <jacob_ardoin@icloud.com>
2024-06-28 20:09:45 -0500

    Uniqure in order

--- kyu6/Unique in Order.js
+++ kyu6/Unique in Order.js
@@ -39,3 +39,11 @@ function uniqueInOrder(iterable){
   }
   return arr
 }
+
+var uniqueInOrder=function(iterable){
+  let arr = []
+  for(let i = 0; i < iterable.length; i++){
+    if(iterable[i] !== iterable[i - 1]) arr.push(iterable[i])
+  }
+  return arr
+}
