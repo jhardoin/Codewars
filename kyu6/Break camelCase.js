1bd5b62e503e5aac3293a489fc294523968f8d88
jhardoin <jacob_ardoin@icloud.com>
2024-07-05 20:44:44 -0500

    Break camelCase

--- kyu6/Break camelCase.js
+++ kyu6/Break camelCase.js
@@ -30,3 +30,13 @@ function solution(string) {
     }
     return s
 }
+
+
+function solution(string) {
+  let str = ''
+  for(let i = 0; i < string.length; i++){
+    if(string[i] == string[i].toUpperCase()) str += ' ' + string[i]
+    else str += string[i]
+  }
+  return str
+}
