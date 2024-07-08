ecf268223154a560e7ece9faff10aa535e36b7f5
jhardoin <jacob_ardoin@icloud.com>
2024-07-08 18:09:47 -0500

    Two sum

--- kyu6/Two Sum.js
+++ kyu6/Two Sum.js
@@ -8,3 +8,14 @@ function twoSum(numbers, target) {
     }
     return [];
   }
+
+  function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] + numbers[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+    return [];
+  }
