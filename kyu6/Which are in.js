cdd1d4280cd2d79dab7ac64da0a36fbd82e36c87
jhardoin <jacob_ardoin@icloud.com>
2024-07-08 18:17:19 -0500

    Which are in

--- kyu6/Which are in.js
+++ kyu6/Which are in.js
@@ -2,3 +2,7 @@ function inArray(array1, array2) {
     return array1.filter(e => array2.some(el => el.includes(e))).sort()
   }
   
+
+  function inArray(array1,array2){
+    return array1.filter(e => array2.some(el => el.includes(e))).sort()
+}
