03da7dca76499c73d2e273e9910c9ed7c1db8fa5
jhardoin <jacob_ardoin@icloud.com>
2024-07-11 21:36:49 -0500

    Is a number prime

--- kyu6/Is a number prime.js
+++ kyu6/Is a number prime.js
@@ -40,3 +40,11 @@ function isPrime(num) {
   }
   return num > 1
 }
+
+
+function isPrime(num) {
+  for(let i = 2; i <= Math.sqrt(num); i++){
+    if(num % i == 0) return false
+  }
+  return num > 1
+}
