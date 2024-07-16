f0910adb289735bb1eb95323b78316084d5616eb
jhardoin <jacob_ardoin@icloud.com>
2024-07-16 16:03:14 -0500

    Delete occurences of an element

--- kyu6/Delete occurences of an element.js
+++ kyu6/Delete occurences of an element.js
@@ -12,3 +12,8 @@ const deleteNth = (a, x) => {
     let obj = {}
     return arr.filter(e => (obj[e] = obj[e] + 1 || 1) <= n)
 }
+
+function deleteNth(arr,n){
+  let obj = {}
+  return arr.filter(e => (obj[e] = obj[e] + 1 || 1) <= n )
+}
