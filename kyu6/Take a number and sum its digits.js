f7b65311f6f64c7d846721d491b25a30329e4559
jhardoin <jacob_ardoin@icloud.com>
2024-07-18 10:15:00 -0500

    Take a number and sum its digits

--- kyu6/Take a number and sum its digits.js
+++ kyu6/Take a number and sum its digits.js
@@ -7,3 +7,13 @@ function sumDigPow(a, b) {
         .reduce((a, b, i) => a + Math.pow(Number(b), i + 1), 0)
     })
   }
+
+  function sumDigPow(a, b) {
+    let found = []
+    for(let i = a; i <= b; i++){
+      if(i === i.toString().split('').reduce((a,n,i)=> a + n ** (i + 1), 0)){
+        found.push(i)
+      }
+    }
+    return found
+  }
