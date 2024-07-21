ad8fbaa40c4c0f69d314a8eff0302ec03c5f0390
jhardoin <jacob_ardoin@icloud.com>
2024-07-21 17:24:17 -0500

    Mexican waves

--- kyu6/Mexican Waves.js
+++ kyu6/Mexican Waves.js
@@ -34,3 +34,11 @@ function wave(str) {
     }
     return arr
 }
+
+function wave(str){
+  let arr = []
+  for(let i = 0; i < str.length; i++){
+    if(str[i] !== ' ') arr.push(str.slice(0,i) + str[i].toUpperCase() + str.slice(i + 1))
+  }
+  return arr
+}
