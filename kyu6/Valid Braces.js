7a94cd2df44f6f4a91e345c9ef172bb583337196
jhardoin <jacob_ardoin@icloud.com>
2024-07-21 17:22:35 -0500

    valid braces

--- kyu6/Valid Braces.js
+++ kyu6/Valid Braces.js
@@ -33,3 +33,19 @@ function validBraces(braces) {
     }
     return stack.length === 0
 }
+
+function validBraces(braces) {
+    let stack = []
+    let braceMap = { '(': ')', '{': '}', '[': ']'}
+
+    for (let i = 0; i < braces.length; i++) {
+        if (braceMap[braces[i]]) {
+            stack.push(braces[i])
+        } else {
+            if (braceMap[stack.pop()] !== braces[i]) {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+}
