49abc03ff44592964851272031c8f009e9457aaa
jhardoin <jacob_ardoin@icloud.com>
2024-07-23 19:27:45 -0500

    Weird string case

--- kyu6/Weird string case.js
+++ kyu6/Weird string case.js
@@ -5,3 +5,9 @@ function toWeirdCase(string) {
       }).join('')
     }).join(' ')
   }
+
+  function toWeirdCase(string) {
+    return string.split(' ')
+                 .map(word => word.split('').map((e, i) => i % 2 === 0 ? e.toUpperCase() : e.toLowerCase()).join(''))
+                 .join(' ')
+  }
