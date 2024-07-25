fdd8eaa796a08383a54a99f67e375e2b59264320
jhardoin <jacob_ardoin@icloud.com>
2024-07-25 09:16:28 -0500

    Write a number in expanded form

--- kyu6/Write number in expanded form.js
+++ kyu6/Write number in expanded form.js
@@ -23,3 +23,7 @@ function expandedForm(num) {
 function expandedForm(num) {
     return num.toString().split('').map((e,i,a) => e + '0'.repeat(a.length - i - 1)).filter(e => +e !== 0).join(' + ')
 }
+
+function expandedForm(num) {
+    return num.toString().split('').map((e,i,a) => e + '0'.repeat(a.length - 1 - i)).filter(e => +e !== 0).join(' + ')
+}
