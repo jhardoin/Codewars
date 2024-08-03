c82c84ec7ee11a2a78d2fc7b623763bdf395fdf1
jhardoin <jacob_ardoin@icloud.com>
2024-08-02 20:24:00 -0500

    Counting duplicates

--- kyu6/Counting Duplicates.js
+++ kyu6/Counting Duplicates.js
@@ -50,3 +50,8 @@ function duplicateCount(text){
   let x = text.toLowerCase().split('').filter((e,i,a) => a.indexOf(e) !== a.lastIndexOf(e))
   return [...new Set(x)].length
 }
+
+function duplicateCount(text){
+  let x = text.toLowerCase().split('').filter((e,i,a) => a.indexOf(e) !== a.lastIndexOf(e))
+  return [...new Set(x)].length
+}
