dbc0a170c4c9974306153b1592ae903e77eef1b2
jhardoin <jacob_ardoin@icloud.com>
2024-08-05 21:29:50 -0500

    Equal sides of an array

--- kyu6/Equal sides of an array.js
+++ kyu6/Equal sides of an array.js
@@ -53,3 +53,12 @@ function findEvenIndex(arr){
   }
   return -1
 }
+
+function findEvenIndex(arr){
+  for(let i = 0; i < arr.length; i++){
+    let left = arr.slice(0,i).reduce((a,b) => a + b, 0)
+    let right = arr.slice(i + 1).reduce((a,b) => a + b, 0)
+    if(left == right) return i
+  }
+  return -1
+}
