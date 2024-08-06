5932b1339161c3851cc23684a7ed87c17c18628f
jhardoin <jacob_ardoin@icloud.com>
2024-08-05 21:27:24 -0500

    Find the unique number

--- kyu6/Find the unique number.js
+++ kyu6/Find the unique number.js
@@ -36,3 +36,7 @@ function findUniq(arr) {
 function findUniq(arr) {
   return arr.find((e,i,a) => a.indexOf(e) == a.lastIndexOf(e))
 }
+
+function findUniq(arr) {
+  return arr.find((e,i,a) => a.indexOf(e) == a.lastIndexOf(e))
+}
