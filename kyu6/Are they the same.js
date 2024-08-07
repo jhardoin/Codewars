76cc598fe21e6e986d026712639bd91ac02508db
jhardoin <jacob_ardoin@icloud.com>
2024-08-06 20:21:57 -0500

    Are they the same

--- kyu6/Are they the same.js
+++ kyu6/Are they the same.js
@@ -26,4 +26,4 @@ function comp(array1, array2){
 
 function comp(array1, array2){
   return array1 && array2 ? array1.sort().toString() == array2.map(e => Math.sqrt(e)).sort().toString() : false
-}
+} 
