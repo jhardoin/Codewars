cbcc8d1a54d6b661255e1651f85c1e5162c07d42
jhardoin <jacob_ardoin@icloud.com>
2024-08-14 19:08:55 -0500

    validate pin code

--- kyu7/Regex validate PIN code.js
+++ kyu7/Regex validate PIN code.js
@@ -6,3 +6,7 @@ function validatePIN(pin) {
   function validatePIN(pin) {
     return /^(\d{4}|\d{6})$/.test(pin);
   }
+
+  function validatePIN (pin) {
+    return (pin.length === 4 || pin.length === 6) && pin.split('').every(e => '1234567890'.includes(e))
+}
