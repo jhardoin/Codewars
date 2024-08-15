a131ccc26367448dd057e8d859bc4528a8ea20ab
jhardoin <jacob_ardoin@icloud.com>
2024-08-14 19:01:17 -0500

    Is this a triangle

--- kyu7/Is this a triange.js
+++ kyu7/Is this a triange.js
@@ -17,3 +17,7 @@ function isTriangle(a,b,c){
  function isTriangle(a,b,c){
   return a + b > c && a + c > b && c + b > a;
 }
+
+function isTriangle(a,b,c){
+  return a + b > c && b + c > a && a + c > b
+}
