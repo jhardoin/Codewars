b7f83e91fc1a400f4b431b1d9659e8ed2a1d325a
jhardoin <jacob_ardoin@icloud.com>
2024-08-18 15:53:36 -0500

    Anagram Detection

--- kyu7/Anagram Detection.js
+++ kyu7/Anagram Detection.js
@@ -13,3 +13,9 @@ function isAnagram (test, original) {
 function isAnagram (test, original) {
 	return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
 }
+
+var isAnagram = function(test, original) {
+    let a = test.toLowerCase().split('').sort().join('')
+    let b = original.toLowerCase().split('').sort().join('')
+    return a === b
+};
