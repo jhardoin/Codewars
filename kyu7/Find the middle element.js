fb2bb17f31922a4c70df81a550a2dc55b3acf38a
jhardoin <jacob_ardoin@icloud.com>
2024-08-23 16:41:49 -0500

    Find the middle element

--- kyu7/Find the middle element.js
+++ kyu7/Find the middle element.js
@@ -17,3 +17,7 @@ function gimme(a) {
     return triplet.indexOf([...triplet].sort((a,b) => a - b)[1])
   }
   
+  function gimme (triplet) {
+    return triplet.indexOf([...triplet].sort((a,b) => a - b)[1])
+  }
+  
