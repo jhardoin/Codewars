58d253e60fdb13f1313c7f3efef5629ba1b9d05a
jhardoin <jacob_ardoin@icloud.com>
2024-09-02 15:25:00 -0500

    Title case

--- kyu6/Title Case.js
+++ kyu6/Title Case.js
@@ -11,3 +11,17 @@ function titleCase(title, minorWords) {
     })
     return result.join(' ')
 }
+
+function titleCase(title, minorWords) {
+    if(title.length === 0) return ''
+    let tArr = title.toLowerCase().split(' ')
+    let mArr = minorWords ? minorWords.toLowerCase().split(' ') : []
+    let arr = []
+    
+    for(let i = 0; i < tArr.length; i++){
+      if(i === 0 || !mArr.includes(tArr[i])){
+        arr.push(tArr[i][0].toUpperCase() + tArr[i].slice(1))
+      } else arr.push(tArr[i])
+    }
+    return arr.join(' ')
+}
