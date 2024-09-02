a55c96811518737f67adee0190f73c7676c60843
jhardoin <jacob_ardoin@icloud.com>
2024-09-02 15:16:04 -0500

    Camelcase

--- kyu6/Camelcase method.js
+++ kyu6/Camelcase method.js
@@ -6,3 +6,7 @@ String.prototype.camelCase = function() {
   String.prototype.camelCase=function(){
     return this.trim() == '' ? '' : this.split(' ').map(e => e[0].toUpperCase() + e.slice(1)).join('')
 }
+
+String.prototype.camelCase=function(){
+  return this ? this.split(' ').map(e => e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()).join('') : ''
+}
