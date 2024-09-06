362480421784f28b98f39558a7ca59f33aebe9ff
jhardoin <jacob_ardoin@icloud.com>
2024-09-06 15:09:29 -0500

    two to one

--- kyu7/Two to One.js
+++ kyu7/Two to One.js
@@ -23,3 +23,8 @@ function longest(s1, s2) {
   function longest(s1, s2) {
     return [...new Set(s1.concat(s2))].sort().join('')
 }
+
+function longest(s1, s2) {
+  let a = [...new Set(s1.concat(s2))]
+  return a.sort().join('')
+}
