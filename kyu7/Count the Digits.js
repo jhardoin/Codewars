d449dc7102d185789a42e5a2b76e286229800925
jhardoin <jacob_ardoin@icloud.com>
2024-09-10 08:58:35 -0500

    Count the digits

--- kyu7/Count the Digits.js
+++ kyu7/Count the Digits.js
@@ -16,3 +16,7 @@ function nbDig(n, d) {
   function nbDig(n, d) {
     return Array.from({length:n + 1}, (e,i) => i*i).join('').split(d).length - 1
   }
+
+  function nbDig(n, d) {
+    return Array.from({length: n + 1}, (_,i) => i ** 2).join('').split('').filter(e => e == d).length
+}
