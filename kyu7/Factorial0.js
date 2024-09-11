b488ecb136d14875488d72494049a0d52e1286c6
jhardoin <jacob_ardoin@icloud.com>
2024-09-11 09:27:35 -0500

    Factorial

--- kyu7/Factorial0.js
+++ kyu7/Factorial0.js
@@ -10,3 +10,8 @@ function factorial(n){
     if(n < 0 || n > 12) throwRangeError()
     return Array.from({length:n}, (_, i) => i + 1).reduce((a,b) => a * b, 1)
   }
+
+  function factorial(n){
+    if(n < 0 || n > 12) throw new RangeError()
+    return n <= 1 ? 1 : n * factorial(n - 1)
+}
