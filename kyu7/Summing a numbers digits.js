3c30ac786737c4f4adaaa8f3884396b829ca8a01
jhardoin <jacob_ardoin@icloud.com>
2024-09-12 18:14:36 -0500

    Summing a numbers digits

--- kyu7/Summing a numbers digits.js
+++ kyu7/Summing a numbers digits.js
@@ -21,3 +21,8 @@ function sumDigits(number) {
 function sumDigits(number) {
   return Math.abs(number).toString().split('').reduce((a,b) => +b + a, 0)
 }
+
+
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce((a,b) => +b + a, 0)
+}
