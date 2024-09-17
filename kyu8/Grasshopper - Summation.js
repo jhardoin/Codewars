d030a7cdd80a8fe8e811d3a8c80a149997bae7c4
jhardoin <jacob_ardoin@icloud.com>
2024-09-17 10:25:01 -0500

    summation

--- kyu8/Grasshopper - Summation.js
+++ kyu8/Grasshopper - Summation.js
@@ -16,3 +16,11 @@ var summation = function (num) {
 var summation = function (num) {
   return Array.from({length:num}, (_, i) => i + 1).reduce((a,b) => a + b, 0)
 }
+
+var summation = function (num) {
+  let sum = 0
+  for(let i = 1; i <= num; i++){
+    sum += i
+  }
+  return sum
+}
