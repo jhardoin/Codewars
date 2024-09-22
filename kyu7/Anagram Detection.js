629befb49e68c6058d7ffb0bad36ded69af0dd18
jhardoin <jacob_ardoin@icloud.com>
2024-09-22 15:44:45 -0500

    Anagram Detection

--- kyu7/Anagram Detection.js
+++ kyu7/Anagram Detection.js
@@ -34,3 +34,8 @@ var isAnagram = function(test, original) {
 var isAnagram = function(test, original) {
     return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('')
 };
+
+var isAnagram = function(test, original) {
+    return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('')
+  };
+  
