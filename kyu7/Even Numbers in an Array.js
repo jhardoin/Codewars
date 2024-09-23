8519c6a390fb8aee4ef93a970b586200cfc9c2c3
jhardoin <jacob_ardoin@icloud.com>
2024-09-23 18:58:57 -0500

    Even numbers in an array

--- kyu7/Even Numbers in an Array.js
+++ kyu7/Even Numbers in an Array.js
@@ -3,3 +3,7 @@ const evenNumbers = (a, n) => a.filter(e => !(e % 2)).slice(-n)
 function evenNumbers(array, number) {
     return array.filter(e => e % 2 == 0).slice(-number)
   }
+
+  function evenNumbers(array, number) {
+    return array.filter(e => e % 2 === 0).slice(-number)
+  }
