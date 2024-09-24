b5b415e4811077cb48c9973e6f7dd6f3b33254a9
jhardoin <jacob_ardoin@icloud.com>
2024-09-24 16:25:20 -0500

    build a square

--- kyu7/Build a square.js
+++ kyu7/Build a square.js
@@ -5,3 +5,7 @@ function generateShape(integer){
 function generateShape(integer){
   return `${('+'.repeat(integer))}\n`.repeat(integer).trim()
 }
+
+function generateShape(n){
+  return ("+".repeat(n)+"\n").repeat(n).trim()
+}
