95c06fb0d609e436d826e3cf66c5aa901ff5c3f8
jhardoin <jacob_ardoin@icloud.com>
2024-09-25 18:43:47 -0500

    Sum the minimums

--- kyu7/Sum of Minimums.js
+++ kyu7/Sum of Minimums.js
@@ -1,3 +1,7 @@
 function sumOfMinimums(arr) {
     return arr.reduce((a,b) => a + Math.min(...b), 0)
   }
+
+  function sumOfMinimums(arr) {
+    return arr.map(e => Math.min(...e)).reduce((a,b) => a + b, 0)
+}
