702c49c0230d70f765627e60fd214d940b76c6a7
jhardoin <jacob_ardoin@icloud.com>
2024-09-27 18:08:09 -0500

    Maximum Product

--- kyu7/Maximum Product.js
+++ kyu7/Maximum Product.js
@@ -9,3 +9,11 @@ function adjacentElementsProduct(array) {
   function adjacentElementsProduct(array) {
     return Math.max(...array.map((e, i)=> e * array[i - 1]).slice(1));
   }
+
+  function adjacentElementsProduct(array) {
+    let max = Number.NEGATIVE_INFINITY 
+    for(let i = 0; i < array.length - 1; i++){
+      if(array[i] * array[i + 1] > max) max = array[i] * array[i + 1]
+    } 
+    return max
+  }
