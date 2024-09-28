8d9e32437d6dc2731bab88b70d14d04ffeb4ae00
jhardoin <jacob_ardoin@icloud.com>
2024-09-28 12:26:30 -0500

    sum of all arguments

--- kyu7/Sum of all arguments.js
+++ kyu7/Sum of all arguments.js
@@ -3,3 +3,7 @@ const sum = () => [...arguments].filter(e => e !== undefined).reduce((a,b) => a 
 function sum(...a) {
     return a.reduce((a,b) => a + b, 0)
   }
+
+  function sum(...nums) {
+    return nums.reduce((a,b) => a + b, 0)
+  }
