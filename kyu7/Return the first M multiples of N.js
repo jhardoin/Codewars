86a8dec702e95ebc5a77ee78ec3974f1e93f9ad0
jhardoin <jacob_ardoin@icloud.com>
2024-09-29 15:20:09 -0500

    Return the first M multiples

--- kyu7/Return the first M multiples of N.js
+++ kyu7/Return the first M multiples of N.js
@@ -1,3 +1,11 @@
 function multiples(m, n){
     return Array.from({length:m}, (_, i) => (i + 1) * n)
   }
+
+  function multiples(m, n){
+    let arr = []
+    for(let i = n; arr.length < m; i += n){
+      arr.push(i)
+    }
+    return arr
+}
