7ee7b15ef2076c975fadcd012a20a99d32dd640f
jhardoin <jacob_ardoin@icloud.com>
2024-09-29 15:23:44 -0500

    Collatz conjecture

--- kyu7/Collatz Conjecture Length.js
+++ kyu7/Collatz Conjecture Length.js
@@ -7,3 +7,14 @@ function collatz(n) {
     }
     return count
   }
+
+  function collatz(n) {
+    let arr = []
+    while(n !== 1){
+      arr.push(n)
+      n = n % 2 === 0 ? n/2 : n * 3 + 1
+      if(n === 1) arr.push(n)
+    }
+    
+    return arr.length
+}
