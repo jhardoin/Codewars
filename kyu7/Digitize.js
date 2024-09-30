7a154336e4ad465ed4b33ab267b1948c544053e6
jhardoin <jacob_ardoin@icloud.com>
2024-09-30 14:20:32 -0500

    Digitize

--- kyu7/Digitize.js
+++ kyu7/Digitize.js
@@ -3,3 +3,7 @@ const digitize = n => n.toString().split('').map(e => +e)
 function digitize(n) {
     return [...n.toString()].map(e => +e)
   }
+
+  function digitize(n) {
+    return n.toString().split('').map(e => +e)
+}
