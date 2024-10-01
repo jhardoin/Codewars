11595617732cfc4e3a981919bdf3b1c22e653778
jhardoin <jacob_ardoin@icloud.com>
2024-10-01 16:22:36 -0500

    remove consecutive duplicate words

--- kyu7/Remove consecutive duplicate words.js
+++ kyu7/Remove consecutive duplicate words.js
@@ -1,3 +1,12 @@
 function removeConsecutiveDuplicates(string) {
     return string.split(' ').filter((e,i,a) => e !== a[i + 1]).join(' ')
   }
+
+  function removeConsecutiveDuplicates(string) {
+    let arr = string.split(' ')
+    let arr2 = []
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] !== arr[i - 1]) arr2.push(arr[i])
+    }
+    return arr2.join(' ')
+}
