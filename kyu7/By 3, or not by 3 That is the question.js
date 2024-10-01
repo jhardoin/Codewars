2319286b97b0a5d03766bb5323c71f68a8b56aec
jhardoin <jacob_ardoin@icloud.com>
2024-10-01 16:27:47 -0500

    By 3 or not by 3

--- kyu7/By 3, or not by 3 That is the question.js
+++ kyu7/By 3, or not by 3 That is the question.js
@@ -1,3 +1,7 @@
 function divisibleByThree(str){
     return Number.isInteger(str.split('').reduce((a,b) => (+a + +b)) / 3)
   }
+
+  function divisibleByThree(str){
+    return str.split('').reduce((a,b) => +b + a, 0) % 3 === 0
+}
