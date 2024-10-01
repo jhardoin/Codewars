0725bcdb12d0ffa1f84b081e04bcb042a9f04ec1
jhardoin <jacob_ardoin@icloud.com>
2024-10-01 16:26:42 -0500

    Complete the pattern

--- kyu7/Complete the pattern 2.js
+++ kyu7/Complete the pattern 2.js
@@ -5,3 +5,11 @@ function pattern(n){
     return arr.join('\n')
     
 }
+
+function pattern(n){
+    let x = Array.from({length:n}, (_,i) => i + 1).reverse()
+    let arr = []
+    x.forEach((e,i,a) => arr.push(a.slice(0, a.length - i).join('')))
+    return arr.join('\n')
+  
+}
