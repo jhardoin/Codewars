b74f787ad9c52d6a7a32b7286f520d6d318045b4
jhardoin <jacob_ardoin@icloud.com>
2024-10-02 11:29:57 -0500

    Smallest value of an array

--- kyu7/Smallest Value of an Array.js
+++ kyu7/Smallest Value of an Array.js
@@ -3,3 +3,10 @@ const min = (arr, toReturn) => (toReturn == 'value') ? Math.min(...arr) : arr.in
 function min(arr, toReturn) {
     return toReturn == 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr))
   }
+
+  function min(arr, toReturn) {
+    if(toReturn === 'value'){
+      return Math.min(...arr)
+    }
+    else return arr.indexOf(Math.min(...arr))
+}
