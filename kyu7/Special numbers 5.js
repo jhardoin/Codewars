bc482db996f3362e8c6abb221d20b225124ae603
jhardoin <jacob_ardoin@icloud.com>
2024-10-04 16:46:29 -0500

    Special Numbers 5

--- kyu7/Special numbers 5.js
+++ kyu7/Special numbers 5.js
@@ -2,3 +2,7 @@ function specialNumber(n){
     let special = ['0','1','2','3','4','5']
     return [...n.toString()].every(e => special.includes(e)) ? 'Special!!' : 'NOT!!'
   }
+
+  function specialNumber(n){
+    return n.toString().split('').every(e => '012345'.includes(e)) ? 'Special!!' : 'NOT!!'
+}
