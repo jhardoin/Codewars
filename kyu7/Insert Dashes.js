db661660ea405456c78abeca0fa1c8fde2d710c4
jhardoin <jacob_ardoin@icloud.com>
2024-10-04 16:35:14 -0500

    Insert Dashes

--- kyu7/Insert Dashes.js
+++ kyu7/Insert Dashes.js
@@ -1,3 +1,16 @@
 function insertDash(num) {
     return num.toString().replace(/([13579])(?=[13579])/g, '$1-');
   }
+
+  function insertDash(num) {
+    num = num.toString().split('')
+     
+     for(let i = 0; i < num.length - 1; i++){
+       if (num[i] % 2 !== 0 && num[i + 1] % 2 !== 0){
+         num[i] = num[i] + '-'
+       }
+     }
+    
+     return num.join('')
+     
+   }
