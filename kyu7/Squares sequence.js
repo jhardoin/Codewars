7c46581744819f3c70bd313d3ca09af5effa7d17
jhardoin <jacob_ardoin@icloud.com>
2024-10-04 16:41:31 -0500

    Square sequence

--- kyu7/Squares sequence.js
+++ kyu7/Squares sequence.js
@@ -2,3 +2,12 @@ function squares(x, n) {
     if(n <= 0) return []
     return Array.from({length:n}, (_, i) => i === 0 ? x : x *= x)
   }
+
+  function squares(x, n) {
+    let arr = []
+    for(let i = x; arr.length < n; i **= 2){
+      arr.push(i)
+    }
+    return arr
+
+}
