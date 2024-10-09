7405ad07ec6ffd64517f0a9f890a238f8d10b26a
jhardoin <jacob_ardoin@icloud.com>
2024-10-09 16:27:38 -0500

    Sort array by string length

--- kyu7/Sort array by string length.js
+++ kyu7/Sort array by string length.js
@@ -39,3 +39,7 @@ function sortByLength (array) {
 function sortByLength (array) {
   return array.sort((a,b) => a.length - b.length)
 }
+
+function sortByLength (array) {
+  return array.sort((a,b) => a.length - b.length)
+}
