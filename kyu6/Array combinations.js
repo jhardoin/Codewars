f18e9e49049c317006f781368ebc89c993760560
jhardoin <jacob_ardoin@icloud.com>
2024-10-18 16:33:46 -0500

    Array combinations

--- kyu6/Array combinations.js
+++ kyu6/Array combinations.js
@@ -7,3 +7,8 @@ const solve = arr => arr.map(e => new Set(e).size).reduce((a,b) => a * b)
 const solve = arr => arr.map(e => new Set(e).size).reduce((a,b) => a * b)
 
 const solve = arr => arr.reduce((a,b) => a * new Set(b).size, 1)
+
+function solve(arr) {
+    let a = arr.map(e => [...new Set(e)])
+    return a.reduce((a,b) => a * (b.length), 1)
+};
