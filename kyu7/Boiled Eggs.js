ad159c2da02ef6f203e0409bb969948e1028883a
jhardoin <jacob_ardoin@icloud.com>
2024-10-20 21:49:53 -0500

    Boiled eggs

--- kyu7/Boiled Eggs.js
+++ kyu7/Boiled Eggs.js
@@ -3,3 +3,7 @@ const cookingTime = eggs => Math.ceil(eggs/8) * 5
 function cookingTime(eggs) {
     return Math.ceil(eggs/8) * 5
   }
+
+  function cookingTime(eggs) {
+    return Math.ceil(eggs/8) * 5
+}
