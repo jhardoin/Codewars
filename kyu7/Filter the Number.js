eb91a616a405eafe18cea187867586701350b5b4
jhardoin <jacob_ardoin@icloud.com>
2024-10-20 21:40:45 -0500

    Filter the number

--- kyu7/Filter the Number.js
+++ kyu7/Filter the Number.js
@@ -3,3 +3,7 @@ const filterString = v => Number(v.replace(/[a-zA-Z]/gi, ''))
 var filterString = function(value) {
     return +[...value].filter(e => '1234567890'.includes(e)).join('')
   }
+
+  var filterString = function(value) {
+    return +value.split('').filter(e => '1234567890'.includes(e)).join('')
+}
