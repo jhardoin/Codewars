e7b24b27eae69d00a38b2faa924347ab63b29aa3
jhardoin <jacob_ardoin@icloud.com>
2024-10-21 19:29:34 -0500

    Speed control

--- kyu7/Speed Control.js
+++ kyu7/Speed Control.js
@@ -1,3 +1,12 @@
 function gps(s, x) {
 	return Math.max(...x.slice(1).map((e, i) => (e - x[i]) / s * 3600)) | 0
 }
+
+function gps(s, x) {
+    let arr = []
+    for(let i = 0; i < x.length - 1; i++){
+      arr.push(x[i + 1] - x[i])
+    }
+    let result = (arr.map(e => 3600 * e / s))
+    return x.length > 1 ? Math.floor(Math.max(...result)) : 0
+}
