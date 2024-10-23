931dca132284ce8ace0b40619b16318b8baea88c
jhardoin <jacob_ardoin@icloud.com>
2024-10-23 11:49:57 -0500

    Vapor code

--- kyu7/Vapor Code.js
+++ kyu7/Vapor Code.js
@@ -1,3 +1,7 @@
 function vaporcode(string) {
     return [...string.toUpperCase()].filter(e => e!== ' ').join('  ')
   }
+
+  function vaporcode(string) {
+    return string.toUpperCase().split('').filter(e => e !== ' ').join('  ')
+}
