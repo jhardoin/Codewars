c457751c742120cbbf0f1c5e51b4b609d6385aa6
jhardoin <jacob_ardoin@icloud.com>
2024-10-23 11:46:33 -0500

    Simple remopve duplicates

--- kyu7/Simple remove duplicates.js
+++ kyu7/Simple remove duplicates.js
@@ -3,3 +3,11 @@ const solve = arr => [...new Set(arr.reverse())].reverse()
 function solve(arr) {
     return [...new Set(arr.reverse())].reverse()
   }
+
+  function solve(arr) {
+    let a = []
+    for(let i = arr.length - 1; i >= 0; i--){
+      if(!a.includes(arr[i])) a.unshift(arr[i])
+    }
+    return a
+}
