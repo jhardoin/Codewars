ea2c03537ec0f1af55d59dfb2de74547873f744d
jhardoin <jacob_ardoin@icloud.com>
2024-10-25 16:39:27 -0500

    build strings from a hash

--- kyu7/Building Strings from a hash.js
+++ kyu7/Building Strings from a hash.js
@@ -3,3 +3,9 @@ function solution(pairs){
       .map(([key, value]) => `${key} = ${value}`)
       .join(',');
   }
+
+  function solution(pairs){
+    return Object.entries(pairs)
+      .map(([key, value]) => `${key} = ${value}`)
+      .join(',');
+  }
