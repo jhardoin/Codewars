4efde7c30181958c97477ba0b9f1e9488aafeb21
jhardoin <jacob_ardoin@icloud.com>
2024-10-28 21:39:26 -0500

    Building blocks

--- kyu7/Building blocks.js
+++ kyu7/Building blocks.js
@@ -25,3 +25,31 @@ class Block {
         return 2 * (this.width * this.length + this.length * this.height + this.height * this.width);
     }
 }
+
+class Block {
+    constructor(dimensions) {
+        this.width = dimensions[0];
+        this.length = dimensions[1];
+        this.height = dimensions[2];
+    }
+
+    getWidth() {
+        return this.width;
+    }
+
+    getLength() {
+        return this.length;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    getVolume() {
+        return this.width * this.length * this.height;
+    }
+
+    getSurfaceArea() {
+        return 2 * (this.width * this.length + this.length * this.height + this.height * this.width);
+    }
+}
