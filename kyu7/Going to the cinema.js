afe3d6245bd2b2427cf4f07e0a8978ead1b47faa
jhardoin <jacob_ardoin@icloud.com>
2024-10-28 21:44:59 -0500

    Going to the cinema

--- kyu7/Going to the cinema.js
+++ kyu7/Going to the cinema.js
@@ -11,3 +11,17 @@ function movie(card, ticket, perc) {
 
     return times;
 }
+
+function movie(card, ticket, perc) {
+    let times = 0;
+    let costWithoutCard = 0;
+    let costWithCard = card;
+
+    while (Math.ceil(costWithCard) >= costWithoutCard) {
+        times++;
+        costWithoutCard += ticket;
+        costWithCard += ticket * perc ** times;
+    }
+
+    return times;
+}
