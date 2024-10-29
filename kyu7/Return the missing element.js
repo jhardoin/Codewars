880d0235f09e1691b2b058f8d5fe254ce03cd8a7
jhardoin <jacob_ardoin@icloud.com>
2024-10-29 16:42:05 -0500

    Return the missing element

--- kyu7/Return the missing element.js
+++ kyu7/Return the missing element.js
@@ -8,3 +8,10 @@ function getMissingElement(superImportantArray){
   function getMissingElement(superImportantArray) {
     return superImportantArray.reduce((a, b) => a - b, 45)
   }
+
+  function getMissingElement(superImportantArray){
+    let sorted = superImportantArray.sort((a,b) => a - b)
+    for(let i = 0; i <= sorted.length; i++){
+      if(sorted[i] !== i) return i
+    }
+  }
