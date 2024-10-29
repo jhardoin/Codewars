103393ae8f95ff4ea7592dd4d2408b248e9b9462
jhardoin <jacob_ardoin@icloud.com>
2024-10-29 16:35:48 -0500

    All unique

--- kyu7/All unique.js
+++ kyu7/All unique.js
@@ -3,3 +3,7 @@ const hasUniqueChars = str => [...new Set(str)].join('') == str
 function hasUniqueChars(str){
     return JSON.stringify([...str]) == JSON.stringify([...new Set([...str])])
   }
+
+  function hasUniqueChars(str){
+    return [...new Set(str)].length === str.length
+}
