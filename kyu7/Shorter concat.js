eb5ffe2fab6a8466286c12eaa93095247756df76
jhardoin <jacob_ardoin@icloud.com>
2024-10-30 16:45:32 -0500

    Shorter concat

--- kyu7/Shorter concat.js
+++ kyu7/Shorter concat.js
@@ -9,3 +9,7 @@ function shorterReverseLonger(a,b){
     return a.length >= b.length ? b + [...a].reverse().join('') + b : 
            a + [...b].reverse().join('') + a
   }
+
+  function shorterReverseLonger(a,b){
+       return a.length >= b.length ? b + [...a].reverse().join('') + b : a + [...b].reverse().join('') + a
+     }
