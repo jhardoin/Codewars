346cf9bf4ac63d13109fb0b02f1a1fdd5fc9e97a
jhardoin <jacob_ardoin@icloud.com>
2024-10-31 16:37:56 -0500

    Refactored greeting

--- kyu7/Refactored Greeting.js
+++ kyu7/Refactored Greeting.js
@@ -7,3 +7,12 @@ class Person {
     }
   }
   
+
+  class Person {
+    constructor(name) {
+      this.name = name
+    }
+    greet(name2) {
+      return `Hello ${name2}, my name is ${this.name}`
+    }
+  }
