f6f07030580ac01b3ee59910a71d42d248d5a33b
jhardoin <jacob_ardoin@icloud.com>
2024-10-31 16:40:06 -0500

    Incrementer

--- kyu7/Incrementer.js
+++ kyu7/Incrementer.js
@@ -5,3 +5,8 @@ function incrementer(nums) {
   function incrementer(nums) { 
     return nums.map((e,i) => (e + (i + 1)).toString().length > 1 ? +(e + (i + 1)).toString().slice(-1) : e + (i + 1))
   }
+
+
+  function incrementer(nums) { 
+    return nums.map((e,i) => (e + i + 1) % 10)
+}
