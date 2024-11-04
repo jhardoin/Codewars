d640a295177ef6454a0fac2443e02870735dead3
jhardoin <jacob_ardoin@icloud.com>
2024-11-03 21:56:44 -0600

    Find the unique number

--- kyu6/Find the unique number.js
+++ kyu6/Find the unique number.js
@@ -45,3 +45,8 @@ function findUniq(arr) {
 function findUniq(arr) {
   return arr.find((e,i,a) => a.indexOf(e) === a.lastIndexOf(e))
 }
+
+
+function findUniq(arr) {
+  return arr.find(e => arr.indexOf(e) === arr.lastIndexOf(e))
+}
