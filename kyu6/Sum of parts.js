037354c93c152e3be0c8eedf6b3bf13a8618692c
jhardoin <jacob_ardoin@icloud.com>
2024-11-08 15:50:16 -0600

    Sum of parts

--- kyu6/Sum of parts.js
+++ kyu6/Sum of parts.js
@@ -22,3 +22,13 @@ function partsSums(ls) {
     let sum = ls.reduce((p, c) => p + c, 0);
     return ls.map(v => sum = sum - v);
 }
+
+function partsSums(ls) {
+    let sum = ls.reduce((a, b) => a + b, 0);
+    let result = [sum];
+    for (let num of ls) {
+        sum -= num;
+        result.push(sum);
+    }
+    return result;
+}
