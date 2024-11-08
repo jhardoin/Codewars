5c8f6a99aa893a8d5b16768f51bd689783a38d72
jhardoin <jacob_ardoin@icloud.com>
2024-11-08 15:48:47 -0600

    Rectangle into squares

--- kyu6/Rectangle Into Squares.js
+++ kyu6/Rectangle Into Squares.js
@@ -14,3 +14,20 @@ function sqInRect(lng, wdth) {
     arr.push(lng)
     return arr
 }
+
+function sqInRect(lng, wdth) {
+    if (lng === wdth) return null
+    let arr = []
+
+    while (lng !== wdth) {
+        if (lng > wdth) {
+            arr.push(wdth)
+            lng = lng - wdth
+        } else {
+            arr.push(lng)
+            wdth = wdth - lng
+        }
+    }
+    arr.push(lng)
+    return arr
+}
