eb99451b372084c2e2a4f407c114d2e8cf10d97a
jhardoin <jacob_ardoin@icloud.com>
2024-11-08 15:49:38 -0600

    Good vs evil

--- kyu6/Good vs Evil.js
+++ kyu6/Good vs Evil.js
@@ -13,3 +13,19 @@ function goodVsEvil(good, evil) {
         return 'Battle Result: No victor on this battle field'
     }
 }
+
+function goodVsEvil(good, evil) {
+    const goodWorth = [1, 2, 3, 3, 4, 10]
+    const evilWorth = [1, 2, 2, 2, 3, 5, 10]
+    
+    const goodTotalWorth = good.split(' ').reduce((a, b, i) => a + (b * goodWorth[i]), 0)
+    const evilTotalWorth = evil.split(' ').reduce((a, b, i) => a + (b * evilWorth[i]), 0)
+    
+    if (goodTotalWorth > evilTotalWorth) {
+        return 'Battle Result: Good triumphs over Evil'
+    } else if (evilTotalWorth > goodTotalWorth) {
+        return 'Battle Result: Evil eradicates all trace of Good'
+    } else {
+        return 'Battle Result: No victor on this battle field'
+    }
+}
