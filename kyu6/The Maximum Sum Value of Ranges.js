7aeecf81b2d539ae2e839a99c9cff6d0257d41c0
jhardoin <jacob_ardoin@icloud.com>
2024-11-09 16:00:00 -0600

    Maximum Sum

--- kyu6/The Maximum Sum Value of Ranges.js
+++ kyu6/The Maximum Sum Value of Ranges.js
@@ -1,1 +1,3 @@
 const maxSum = (arr,range) => Math.max(...range.map(e => arr.slice(e[0], e[1] + 1).reduce((a, c) => a + c)))
+
+const maxSum = (arr,range) => Math.max(...range.map(e => arr.slice(e[0], e[1] + 1).reduce((a, c) => a + c)))
