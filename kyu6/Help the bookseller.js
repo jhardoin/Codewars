eca3b364a5c52f459ed470412433d9fc8bc6000f
jhardoin <jacob_ardoin@icloud.com>
2024-11-10 18:22:29 -0600

    bookseller

--- kyu6/Help the bookseller.js
+++ kyu6/Help the bookseller.js
@@ -31,3 +31,20 @@ function stockList(listOfArt, listOfCat) {
 
     return result
 }
+
+function stockList(listOfArt, listOfCat) {
+    if (listOfArt.length === 0 || listOfCat.length === 0) return ''
+    let obj = {}
+    listOfCat.forEach(e => obj[e] = 0)
+
+    listOfArt.forEach(book => {
+        let [code, quantity] = book.split(' ')
+        let category = code[0]
+        quantity = +quantity
+
+        if (obj.hasOwnProperty(category)) obj[category] = obj[category] + quantity
+    })
+    let result = listOfCat.map(category => `(${category} : ${obj[category]})`).join(' - ')
+
+    return result
+}
