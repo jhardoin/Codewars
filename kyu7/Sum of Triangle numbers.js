230a842d7d98f8c740d9665469cf7739b8e20dd6
jhardoin <jacob_ardoin@icloud.com>
2024-11-17 18:51:22 -0600

    Sum of triangle

--- kyu7/Sum of Triangle numbers.js
+++ kyu7/Sum of Triangle numbers.js
@@ -6,3 +6,7 @@ function sumTriangularNumbers(n) {
   function sumTriangularNumbers(n) {
     return n < 0 ? 0 : n * (n + 1) * (n + 2) / 6;
   }
+
+  function sumTriangularNumbers(n) {
+    return n < 0 ? 0 : n * (n + 1) * (n + 2) / 6;
+  }
