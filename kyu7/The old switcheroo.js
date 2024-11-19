ec2551f72979fcf60b8e7f944fdd4ad90a3bb96e
jhardoin <jacob_ardoin@icloud.com>
2024-11-18 18:52:18 -0600

    The old switcheroo

--- kyu7/The old switcheroo.js
+++ kyu7/The old switcheroo.js
@@ -2,3 +2,7 @@ function vowel2index(str) {
     return str.split('').map((e,i) => 'aeiouAEIOU'.includes(e) ? i + 1 : e).join('')
  }
  
+ function vowel2index(str) {
+    return str.split('').map((e,i) => 'aeiouAEIOU'.includes(e) ? i + 1 : e).join('')
+ }
+ 
