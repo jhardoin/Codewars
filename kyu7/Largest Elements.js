d1bdf6907b7f5e1642532589fc5dae54d758c80e
jhardoin <jacob_ardoin@icloud.com>
2024-11-19 16:35:38 -0600

    Largest Elements

--- kyu7/Largest Elements.js
+++ kyu7/Largest Elements.js
@@ -17,3 +17,9 @@ function largest(n, array) {
     array.length = n
     return array.reverse()
   }
+
+  function largest(n, array) {
+    array.sort((a,b) => b - a)
+    array.length = n
+    return array.reverse()
+  }
