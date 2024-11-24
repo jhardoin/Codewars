88bb4666e63f394fbb375f3fa234c43ba63c1377
jhardoin <jacob_ardoin@icloud.com>
2024-11-23 22:59:08 -0600

    Are the numbers in order

--- kyu7/Are the numbers in order.js
+++ kyu7/Are the numbers in order.js
@@ -19,3 +19,7 @@ function inAscOrder(arr) {
 function inAscOrder(arr) {
   return arr.toString() === arr.sort((a,b) => a - b).toString()
 }
+
+function inAscOrder(arr) {
+  return arr.toString() === arr.sort((a,b) => a - b).toString()
+}
