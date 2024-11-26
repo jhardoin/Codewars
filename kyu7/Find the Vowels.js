7c1aaf08338c6a96b3f6d4c180cefabfaec97da9
jhardoin <jacob_ardoin@icloud.com>
2024-11-26 14:35:28 -0600

    Find the vowels

--- kyu7/Find the Vowels.js
+++ kyu7/Find the Vowels.js
@@ -23,3 +23,11 @@ function vowelIndices(word){
   }
   return arr
 }
+
+function vowelIndices(word){
+  let arr = []
+  for(let i = 0; i < word.length; i++){
+    if('aeiouyAEIOUY'.includes(word[i])) arr.push(i + 1)
+  }
+  return arr
+}
