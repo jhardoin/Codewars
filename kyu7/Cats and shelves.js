223f8d103aa5918cf955660dca5c9d31f5b49456
jhardoin <jacob_ardoin@icloud.com>
2024-11-28 22:42:53 -0600

    Cats and shelves

--- kyu7/Cats and shelves.js
+++ kyu7/Cats and shelves.js
@@ -5,3 +5,11 @@ function solution(start, finish) {
     }
     return count
   }
+
+  function solution(start, finish) {
+    let count = 0
+    for(let i = start; i < finish; finish - i >= 3 ? i += 3 : i++){
+      count++
+    }
+    return count
+  }
