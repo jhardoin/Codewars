f416037582b93e0f333c509b39b76385c5bf9d7d
jhardoin <jacob_ardoin@icloud.com>
2024-11-29 23:24:01 -0600

    Find the divisors

--- kyu7/Find the divisors.js
+++ kyu7/Find the divisors.js
@@ -42,3 +42,11 @@ function divisors(integer) {
   }
   return arr.length === 0 ? `${integer} is prime` : arr
 }
+
+function divisors(integer) {
+  let arr = []
+  for(let i = 2; i < integer; i++){
+    if(integer % i === 0) arr.push(i)
+  }
+  return arr.length === 0 ? `${integer} is prime` : arr
+}
