629456bf0c149572213f2e81b9be6d4a41ac91c7
jhardoin <jacob_ardoin@icloud.com>
2024-11-30 00:48:20 -0600

    Largest pair sum in array

--- kyu7/Largest pair sum in Array.js
+++ kyu7/Largest pair sum in Array.js
@@ -29,3 +29,8 @@ function largestPairSum (numbers) {
   return numbers[0] + numbers[1]
 }
 
+function largestPairSum (numbers) {
+  numbers.sort((a,b) => b - a)
+  return numbers[0] + numbers[1]
+}
+
