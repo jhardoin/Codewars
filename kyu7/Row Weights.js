61aabfa1a416d726346313fdc01c7daf18ac5d81
jhardoin <jacob_ardoin@icloud.com>
2024-12-04 20:28:20 -0600

    Row weights

--- kyu7/Row Weights.js
+++ kyu7/Row Weights.js
@@ -29,3 +29,9 @@ function rowWeights(array){
   let team2 = array.filter((e,i) => i % 2 !== 0).reduce((a,b) => a + b, 0)
   return [team1,team2]
 }
+
+function rowWeights(array){
+  let team1 = array.filter((e,i) => i % 2 === 0).reduce((a,b) => a + b, 0)
+  let team2 = array.filter((e,i) => i % 2 !== 0).reduce((a,b) => a + b, 0)
+  return [team1,team2]
+}
