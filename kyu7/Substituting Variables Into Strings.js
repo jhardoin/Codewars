cd9bca57f4842f7b437c440819cd40564c16c770
jhardoin <jacob_ardoin@icloud.com>
2024-12-08 18:00:50 -0600

    Substituting Variables

--- kyu7/Substituting Variables Into Strings.js
+++ kyu7/Substituting Variables Into Strings.js
@@ -12,3 +12,7 @@ function solution(value){
 function solution(value){
   return `Value is ${value.toString().padStart(5, '0')}`
 }
+
+function solution(value){
+  return `Value is ${value.toString().padStart(5, '0')}`
+}
