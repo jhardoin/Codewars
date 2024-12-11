40d6611c3766282aa55359d83292ec86c2e2e6cb
jhardoin <jacob_ardoin@icloud.com>
2024-12-10 20:29:21 -0600

    Shortest word

--- kyu7/Shortest Word.js
+++ kyu7/Shortest Word.js
@@ -29,3 +29,7 @@ function findShort(s){
 function findShort(s){
   return s.split(' ').reduce((a,b) => b.length < a.length ? b : a).length
 }
+
+function findShort(s){
+  return s.split(' ').reduce((a,b) => b.length < a.length ? b : a).length
+}
