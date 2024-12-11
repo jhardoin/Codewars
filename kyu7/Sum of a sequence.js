252774bfd5799e7d6921e7961fe2c00fb99df335
jhardoin <jacob_ardoin@icloud.com>
2024-12-10 20:28:29 -0600

    Sum of a sequence

--- kyu7/Sum of a sequence.js
+++ kyu7/Sum of a sequence.js
@@ -60,3 +60,11 @@ const sequenceSum = (begin, end, step) => {
   }
   return sum
 };
+
+const sequenceSum = (begin, end, step) => {
+  let sum = 0
+  for(let i = begin; i <= end; i += step){
+    sum += i
+  }
+  return sum
+};
