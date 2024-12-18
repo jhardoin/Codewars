09fa36c351a4a940e9ca34202b56e4cab61be6f4
jhardoin <jacob_ardoin@icloud.com>
2024-12-17 18:29:52 -0600

    Debug sum of digits of a number

--- kyu7/Debug sum of digits of a number.js
+++ kyu7/Debug sum of digits of a number.js
@@ -1,3 +1,7 @@
 function getSumOfDigits(integer) {
     return [...integer.toString()].reduce((a,b) => +b + a, 0)
   }
+
+  function getSumOfDigits(integer) {
+    return [...integer.toString()].reduce((a,b) => +b + a, 0)
+  }
