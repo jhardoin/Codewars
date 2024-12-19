bd5c40a3447335e79f02dc95f5a8977533e5582f
jhardoin <jacob_ardoin@icloud.com>
2024-12-19 01:37:10 -0600

    List filtering

--- kyu7/List Filtering.js
+++ kyu7/List Filtering.js
@@ -31,3 +31,7 @@ function filter_list(l) {
 function filter_list(l) {
   return l.filter(e => typeof e === 'number')
 }
+
+function filter_list(l) {
+  return l.filter(e => typeof e === 'number')
+}
