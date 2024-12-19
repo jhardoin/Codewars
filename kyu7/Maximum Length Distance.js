7075a160f40c77783b95b41c9b070c6200e28447
jhardoin <jacob_ardoin@icloud.com>
2024-12-19 01:37:42 -0600

    Maximum length distance

--- kyu7/Maximum Length Distance.js
+++ kyu7/Maximum Length Distance.js
@@ -23,3 +23,10 @@ function mxdiflg(a1, a2) {
     if(a1.length === 0 || a2.length === 0) return -1
     return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
 }
+
+function mxdiflg(a1, a2) {
+    let l1 = a1.map(e => e.length)
+    let l2 = a2.map(e => e.length)
+    if(a1.length === 0 || a2.length === 0) return -1
+    return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
