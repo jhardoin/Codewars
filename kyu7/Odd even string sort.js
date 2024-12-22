1207c07b675178ff11b22f39c95acbb958cbab7e
jhardoin <jacob_ardoin@icloud.com>
2024-12-22 11:28:09 -0600

    Odd even String sort

--- kyu7/Odd even string sort.js
+++ kyu7/Odd even string sort.js
@@ -37,3 +37,10 @@ function sortMyString(S) {
     S.split('').forEach((e,i) => i % 2 === 0 ? even.push(e) : odd.push(e))
     return [even.join(''), odd.join('')].join(' ')
 }
+
+function sortMyString(S) {
+    let even = []
+    let odd = []
+    S.split('').forEach((e,i) => i % 2 === 0 ? even.push(e) : odd.push(e))
+    return [even.join(''), odd.join('')].join(' ')
+}
