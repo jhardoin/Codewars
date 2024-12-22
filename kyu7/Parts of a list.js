26ad1561344f7e6cce693ff94e8ad946b10acef8
jhardoin <jacob_ardoin@icloud.com>
2024-12-22 11:27:27 -0600

    Parts of a list

--- kyu7/Parts of a list.js
+++ kyu7/Parts of a list.js
@@ -1,3 +1,7 @@
 function partlist(arr) {
     return arr.map((e, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
 }
+
+function partlist(arr) {
+    return arr.map((e, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
+}
