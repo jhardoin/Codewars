23dbcf7c04354f3a0d5f19fc7390a2ac232d272b
jhardoin <jacob_ardoin@icloud.com>
2024-12-24 18:50:11 -0600

    Data reverse

--- kyu6/Data Reverse.js
+++ kyu6/Data Reverse.js
@@ -14,3 +14,12 @@ function dataReverse(data) {
     }
     return arr.reverse().flat()
   }
+
+  function dataReverse(data) {
+    let arr = []
+    
+    for(let i = 0; i < data.length; i += 8){
+      arr.push(data.slice(i, i + 8))
+    }
+    return arr.reverse().flat()
+  }
