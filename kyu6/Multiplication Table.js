0c672112cc5dcaf3c89044e5114b2fe54e661020
jhardoin <jacob_ardoin@icloud.com>
2024-12-25 23:48:22 -0600

    multiplication table

--- kyu6/Multiplication Table.js
+++ kyu6/Multiplication Table.js
@@ -5,3 +5,7 @@ let multiplicationTable = function(size) {
   let multiplicationTable = function(size) {
     return Array.from({ length: size }, (_, i) => Array.from({ length: size }, (_, j) => (i + 1) * (j + 1)))
   }
+
+  let multiplicationTable = function(size) {
+    return Array.from({ length: size }, (_, i) => Array.from({ length: size }, (_, j) => (i + 1) * (j + 1)))
+  }
