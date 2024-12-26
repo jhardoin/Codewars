97bbc5fe52639f1bb689eb8fa5baa0fae1cecea1
jhardoin <jacob_ardoin@icloud.com>
2024-12-25 23:47:46 -0600

    Give me a diamond

--- kyu6/Give me a diamond.js
+++ kyu6/Give me a diamond.js
@@ -23,3 +23,16 @@ function diamond(n) {
     
     return diamondStr
 }
+
+function diamond(n) {
+    if (n % 2 === 0 || n < 0) return null
+    
+    let diamondStr = ''
+    for (let i = 0; i < n; i++) {
+        const spaces = Math.abs(Math.floor(n / 2) - i)
+        const diamonds = n - 2 * spaces
+        diamondStr += " ".repeat(spaces) + "*".repeat(diamonds) + "\n"
+    }
+    
+    return diamondStr
+}
