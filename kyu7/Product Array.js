8a18b7f2e5cbfa75441c1cba0d6cdac66ed4bfb2
jhardoin <jacob_ardoin@icloud.com>
2024-12-29 21:30:33 -0600

    Product array

--- kyu7/Product Array.js
+++ kyu7/Product Array.js
@@ -2,3 +2,8 @@ function productArray(numbers) {
     let totalProduct = numbers.reduce((acc, num) => acc * num, 1)
     return numbers.map(num => totalProduct / num)
   }
+
+  function productArray(numbers) {
+    let totalProduct = numbers.reduce((acc, num) => acc * num, 1)
+    return numbers.map(num => totalProduct / num)
+  }
