756fc9310464e35db3f738d63375b494110f4c2b
jhardoin <jacob_ardoin@icloud.com>
2025-01-05 21:49:14 -0600

    NBA full 48 minute average

--- kyu8/NBA full 48 minutes average.js
+++ kyu8/NBA full 48 minutes average.js
@@ -3,3 +3,7 @@ const pointsPer48 = (ppg, mpg) => (ppg === 0 || mpg === 0) ? 0 : Number((ppg * 4
 function pointsPer48(ppg, mpg) {
     return ppg && mpg ? +(ppg * 48/mpg).toFixed(1) : 0
   }
+
+  function pointsPer48(ppg, mpg) {
+    return ppg && mpg ? +(ppg * 48/mpg).toFixed(1) : 0
+  }
