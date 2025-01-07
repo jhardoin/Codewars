16364aed3bb78e5bddceea8baf5448113c41b38d
jhardoin <jacob_ardoin@icloud.com>
2025-01-07 10:18:03 -0600

    The if function

--- kyu8/The if function.js
+++ kyu8/The if function.js
@@ -5,3 +5,7 @@ const _if = (bool, func1, func2) => bool ? func1() : func2()
 function _if(bool, func1, func2) {
     return bool ? func1() : func2()
 }
+
+function _if(bool, func1, func2) {
+    return bool ? func1() : func2()
+}
