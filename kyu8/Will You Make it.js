38c46e9a45ef60dbd8509d059b18778ad7f088cf
jhardoin <jacob_ardoin@icloud.com>
2025-01-09 10:37:06 -0600

    Will you make it

--- kyu8/Will You Make it.js
+++ kyu8/Will You Make it.js
@@ -20,3 +20,7 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return distanceToPump/mpg <=  fuelLeft
 }
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump/mpg <=  fuelLeft
+}
