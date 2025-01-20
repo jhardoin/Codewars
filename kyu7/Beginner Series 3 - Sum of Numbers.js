5c1bb0571b0e02edcbea0fa2a039172855df22c1
jhardoin <jacob_ardoin@icloud.com>
2025-01-20 11:57:34 -0600

    Sum of numbers

--- kyu7/Beginner Series 3 - Sum of Numbers.js
+++ kyu7/Beginner Series 3 - Sum of Numbers.js
@@ -45,3 +45,11 @@ function getSum(a, b){
   }
   return count
 }
+
+function getSum(a, b){
+  let count = 0
+  for(let i = Math.min(a,b); i <= Math.max(a,b); i++){
+    count += i
+  }
+  return count
+}
