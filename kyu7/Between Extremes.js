e96e00134b0c6ca815def3a32f22852d834e72e5
jhardoin <jacob_ardoin@icloud.com>
2025-01-21 23:04:10 -0600

    Between extremes

--- kyu7/Between Extremes.js
+++ kyu7/Between Extremes.js
@@ -13,3 +13,7 @@ function betweenExtremes(numbers) {
 function betweenExtremes(numbers) {
     return Math.max(...numbers) - Math.min(...numbers)
 }
+
+function betweenExtremes(numbers) {
+    return Math.max(...numbers) - Math.min(...numbers)
+}
