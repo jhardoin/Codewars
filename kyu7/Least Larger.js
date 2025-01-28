2e79336a59f1ca84ad3fef05b92851f53c889c1f
jhardoin <jacob_ardoin@icloud.com>
2025-01-27 21:31:27 -0600

    Least larger

--- kyu7/Least Larger.js
+++ kyu7/Least Larger.js
@@ -1,3 +1,7 @@
 function leastLarger(a,i) {
     return a.indexOf(Math.min(...a.filter(e => e > a[i])))
   }
+
+  function leastLarger(a,i) {
+    return a.indexOf(Math.min(...a.filter(e => e > a[i])))
+  }
