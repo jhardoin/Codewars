3aa76fdf94e8acc5450a8b62e46fa117acf3b2f8
jhardoin <jacob_ardoin@icloud.com>
2025-01-31 20:44:52 -0600

    Trianglar Treasure

--- kyu7/Triangular treasure.js
+++ kyu7/Triangular treasure.js
@@ -3,3 +3,7 @@ const triangular = n => (n < 1) ? 0 : (n * (n + 1)) / 2
 function triangular( n ) {
     return n > 0 ? n * (n + 1) / 2 : 0;
   }
+
+  function triangular( n ) {
+    return n > 0 ? n * (n + 1) / 2 : 0;
+  }
