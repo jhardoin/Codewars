42eda96e880b16e6c617b96120ed747b0d8de292
jhardoin <jacob_ardoin@icloud.com>
2025-02-09 20:54:56 -0600

    Elapsed seconds

--- kyu7/Elapsed Seconds.js
+++ kyu7/Elapsed Seconds.js
@@ -1,3 +1,7 @@
 function elapsedSeconds(startDate, endDate){
     return (endDate - startDate)/1000
   }
+
+  function elapsedSeconds(startDate, endDate){
+    return (endDate - startDate)/1000
+  }
