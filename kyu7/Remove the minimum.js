3a9fad6bd91e33e54ed4abb02c7171cf0d9c50ff
jhardoin <jacob_ardoin@icloud.com>
2025-02-11 17:37:24 -0600

    Remove the minimum

--- kyu7/Remove the minimum.js
+++ kyu7/Remove the minimum.js
@@ -25,3 +25,7 @@ function removeSmallest(numbers) {
 function removeSmallest(numbers) {
   return numbers.filter((e,i) => i !== numbers.indexOf(Math.min(...numbers)))
 }
+
+function removeSmallest(numbers) {
+  return numbers.filter((e,i) => i !== numbers.indexOf(Math.min(...numbers)))
+}
