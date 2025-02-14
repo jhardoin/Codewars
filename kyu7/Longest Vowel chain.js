49a7ab9cbb316c2e6b89fafeef58f6daffea93c1
jhardoin <jacob_ardoin@icloud.com>
2025-02-13 22:11:12 -0600

    Longest vowel chain

--- kyu7/Longest Vowel chain.js
+++ kyu7/Longest Vowel chain.js
@@ -16,3 +16,8 @@ function solve(s){
     let arr = s.split('').map(e => 'aeiou'.includes(e) ? e : ' ').join('').split(' ')
     return Math.max(...arr.map(e => e.length))
   }
+
+  function solve(s){
+    let arr = s.split('').map(e => 'aeiou'.includes(e) ? e : ' ').join('').split(' ')
+    return Math.max(...arr.map(e => e.length))
+  }
