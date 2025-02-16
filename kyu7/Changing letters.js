9ca33e2db3df50e82a3a3e24aa063ad0a127016a
jhardoin <jacob_ardoin@icloud.com>
2025-02-15 18:54:30 -0600

    Changing letters

--- kyu7/Changing letters.js
+++ kyu7/Changing letters.js
@@ -9,3 +9,7 @@ function swap (string) {
   function swap (string) {
     return [...string].map(e => 'aeiou'.includes(e) ? e.toUpperCase() : e).join('')
   }
+
+  function swap (string) {
+    return [...string].map(e => 'aeiou'.includes(e) ? e.toUpperCase() : e).join('')
+  }
