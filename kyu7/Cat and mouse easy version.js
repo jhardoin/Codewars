4bc80fbb86d267185a95561f09e605264c56c328
jhardoin <jacob_ardoin@icloud.com>
2025-02-16 11:16:17 -0600

    cat and mouse

--- kyu7/Cat and mouse easy version.js
+++ kyu7/Cat and mouse easy version.js
@@ -5,3 +5,7 @@ function catMouse(x){
   function catMouse(x){
     return x.slice(1,-1).length > 3 ? 'Escaped!' : 'Caught!'
   }
+
+  function catMouse(x){
+    return x.slice(1,-1).length > 3 ? 'Escaped!' : 'Caught!'
+  }
