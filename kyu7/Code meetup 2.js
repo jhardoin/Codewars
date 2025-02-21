8c0a7cb1c5df3e61b935170c19fc477f15f1c1c6
jhardoin <jacob_ardoin@icloud.com>
2025-02-20 20:49:40 -0600

    Code meetup

--- kyu7/Code meetup 2.js
+++ kyu7/Code meetup 2.js
@@ -18,3 +18,10 @@ function greetDevelopers(list) {
     }
     return list
   }
+
+  function greetDevelopers(list) {
+    for(let i = 0; i < list.length; i++){
+      list[i].greeting = `Hi ${list[i].firstName}, what do you like the most about ${list[i].language}?`
+    }
+    return list
+  }
