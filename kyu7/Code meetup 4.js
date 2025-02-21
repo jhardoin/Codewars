c0cb53d8d484fb146bc8293a9dd9227620ba06e6
jhardoin <jacob_ardoin@icloud.com>
2025-02-20 20:50:07 -0600

    Code meetup 4

--- kyu7/Code meetup 4.js
+++ kyu7/Code meetup 4.js
@@ -12,3 +12,8 @@ function getFirstPython(list) {
     let a = list.find(x => x.language === "Python")
     return a ? `${a.firstName}, ${a.country}` : "There will be no Python developers"
   }
+
+  function getFirstPython(list) {
+    let a = list.find(x => x.language === "Python")
+    return a ? `${a.firstName}, ${a.country}` : "There will be no Python developers"
+  }
