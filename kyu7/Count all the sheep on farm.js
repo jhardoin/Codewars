224bad97f521b8e03fc142ca9a5cbf38b4121a22
jhardoin <jacob_ardoin@icloud.com>
2025-02-20 20:50:24 -0600

    Count all the sheep on farm

--- kyu7/Count all the sheep on farm.js
+++ kyu7/Count all the sheep on farm.js
@@ -7,3 +7,8 @@ function lostSheep(friday,saturday,total){
     let returned = friday.reduce((a,b) => a + b, 0) + saturday.reduce((a,b) => a + b, 0)
     return total - returned
 }
+
+function lostSheep(friday,saturday,total){
+    let returned = friday.reduce((a,b) => a + b, 0) + saturday.reduce((a,b) => a + b, 0)
+    return total - returned
+}
