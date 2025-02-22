a1b14114f3ff5a37608351b375013354a4f8d35e
jhardoin <jacob_ardoin@icloud.com>
2025-02-21 19:43:45 -0600

    Count the divisors of a number

--- kyu7/Count the divisors of a number.js
+++ kyu7/Count the divisors of a number.js
@@ -62,3 +62,14 @@ function getDivisorsCnt(n){
   }
   return count
 }
+
+function getDivisorsCnt(n){
+  let count = 0
+  if(n % Math.sqrt(n) == 0) count++
+  for(let i = 0; i < Math.sqrt(n); i++){
+    if(n % i == 0){
+      count += 2
+    }
+  }
+  return count
+}
