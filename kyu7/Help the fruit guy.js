4c1784ffc6222c4636f34875db935318bbaf265b
jhardoin <jacob_ardoin@icloud.com>
2025-02-22 21:01:37 -0600

    Help the fruit guy

--- kyu7/Help the fruit guy.js
+++ kyu7/Help the fruit guy.js
@@ -5,3 +5,7 @@ function removeRotten(bagOfFruits){
 function removeRotten(bagOfFruits){
     return bagOfFruits ? bagOfFruits.map(e => e.includes('rotten') ? e.slice(6).toLowerCase() : e) : []
 }
+
+function removeRotten(bagOfFruits){
+    return bagOfFruits ? bagOfFruits.map(e => e.includes('rotten') ? e.slice(6).toLowerCase() : e) : []
+}
