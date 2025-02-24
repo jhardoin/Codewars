bc708ab04f967c456fac1573502bd77d03991981
jhardoin <jacob_ardoin@icloud.com>
2025-02-24 11:58:52 -0600

    Word Values

--- kyu7/Word Values.js
+++ kyu7/Word Values.js
@@ -1,3 +1,7 @@
 function wordValue(a) {
     return a.map((e, i) => [...e].reduce((a, b) => b !== ' ' ? a + b.charCodeAt(0) - 96 : a, 0) * (i + 1));
   }
+
+  function wordValue(a) {
+    return a.map((e, i) => [...e].reduce((a, b) => b !== ' ' ? a + b.charCodeAt(0) - 96 : a, 0) * (i + 1));
+  }
