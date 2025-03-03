e45bf73e7abb9aba3ae94bad3898aed85368a4d6
jhardoin <jacob_ardoin@icloud.com>
2025-03-02 18:33:15 -0600

    Sort by last char

--- kyu7/Sort by last char.js
+++ kyu7/Sort by last char.js
@@ -9,3 +9,7 @@ function last(x){
 function last(x){
     return x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length-1))
 }
+
+function last(x){
+    return x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length-1))
+}
