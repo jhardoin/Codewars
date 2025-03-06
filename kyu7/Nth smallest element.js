16b3ee5a9273d46d862fa923afd9cf20ac1be440
jhardoin <jacob_ardoin@icloud.com>
2025-03-05 19:53:02 -0600

    nth smallest element

--- kyu7/Nth smallest element.js
+++ kyu7/Nth smallest element.js
@@ -21,3 +21,9 @@ function nthSmallest(arr, pos){
   let sorted = arr.sort((a,b) => a - b)
   return sorted[pos - 1]
 }
+
+
+function nthSmallest(arr, pos){
+  let sorted = arr.sort((a,b) => a - b)
+  return sorted[pos - 1]
+}
