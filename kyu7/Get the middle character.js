31efa58fa61d2ff8029ef8061bdf74852a0c9f1c
jhardoin <jacob_ardoin@icloud.com>
2025-03-06 21:57:48 -0600

    Get the middle character

--- kyu7/Get the middle character.js
+++ kyu7/Get the middle character.js
@@ -33,3 +33,7 @@ function getMiddle(s) {
   function getMiddle(s){
     return s.slice((s.length-1)/2, s.length/2+1)
   }
+
+  function getMiddle(s){
+    return s.slice((s.length-1)/2, s.length/2+1)
+  }
