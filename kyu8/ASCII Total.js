eb71008c53c77e26366cf7339898a0afeee7d769
jhardoin <jacob_ardoin@icloud.com>
2025-03-13 19:58:05 -0500

    ASCII total

--- kyu8/ASCII Total.js
+++ kyu8/ASCII Total.js
@@ -1,1 +1,3 @@
 const uniTotal = (string) => string.length < 1 ? 0 : string.split('').map(e => e.charCodeAt()).reduce((a,b) => a + b)
+
+const uniTotal = (string) => string.length < 1 ? 0 : string.split('').map(e => e.charCodeAt()).reduce((a,b) => a + b)
