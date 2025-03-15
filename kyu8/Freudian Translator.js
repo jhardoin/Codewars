dee43fdfb0589c32fb9c29c9147cd90a471db093
jhardoin <jacob_ardoin@icloud.com>
2025-03-14 23:29:25 -0500

    Freudian translator

--- kyu8/Freudian Translator.js
+++ kyu8/Freudian Translator.js
@@ -1,3 +1,7 @@
 function toFreud(string) {
     return string.length == 0 ? '' : string.split(' ').map(e => 'sex').join(' ')
     }
+
+    function toFreud(string) {
+        return string.length == 0 ? '' : string.split(' ').map(e => 'sex').join(' ')
+        }
