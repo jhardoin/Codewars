084c873b105ee1e9ddf0ef600ba74a1edb6b68c6
jhardoin <jacob_ardoin@icloud.com>
2025-03-14 23:29:03 -0500

    Nearest square

--- kyu8/Find the Nearest Square.js
+++ kyu8/Find the Nearest Square.js
@@ -3,3 +3,7 @@ const nearestSq = (n) => Math.pow(Math.round(Math.sqrt(n)), 2);
 function nearestSq(n){
     return Math.round(Math.sqrt(n)) ** 2
 }
+
+function nearestSq(n){
+    return Math.round(Math.sqrt(n)) ** 2
+}
