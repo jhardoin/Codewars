a2c18a070fc82e6b18c512eb173c82d7b7bc525d
jhardoin <jacob_ardoin@icloud.com>
2025-03-20 21:28:42 -0500

    Find the difference in age between oldest and youngest

--- kyu8/Find difference in age between oldest and youngest.js
+++ kyu8/Find difference in age between oldest and youngest.js
@@ -10,3 +10,8 @@ function differenceInAges(ages){
     return [Math.min(...ages),  Math.max(...ages), Math.max(...ages) - Math.min(...ages)]
   }
   
+
+  function differenceInAges(ages){
+    return [Math.min(...ages),  Math.max(...ages), Math.max(...ages) - Math.min(...ages)]
+  }
+  
