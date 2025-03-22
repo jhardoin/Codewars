6a2710246b43fcbfa0816fa5dbda62e4978c7ce0
jhardoin <jacob_ardoin@icloud.com>
2025-03-22 18:44:03 -0500

    Multiply the number

--- kyu8/Multiply the Number.js
+++ kyu8/Multiply the Number.js
@@ -3,3 +3,7 @@ const multiply = (number) => number  * (5 ** Math.abs(number).toString().length)
 function multiply(number){
     return 5 ** (Math.abs(number).toString().length) * number
   }
+
+  function multiply(number){
+    return 5 ** (Math.abs(number).toString().length) * number
+  }
