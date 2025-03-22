92739d0e86236ff5870c32c8c5ea77a8852b5c8c
jhardoin <jacob_ardoin@icloud.com>
2025-03-22 18:43:25 -0500

    Improving math round

--- kyu8/Improving Math round.js
+++ kyu8/Improving Math round.js
@@ -5,3 +5,7 @@ Math.roundTo = function (number, precision) {
   Math.roundTo = function (number, precision) {
     return +number.toFixed(precision)
   }
+
+  Math.roundTo = function (number, precision) {
+    return +number.toFixed(precision)
+  }
