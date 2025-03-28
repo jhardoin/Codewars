d191b2856b54764fa30159359130bc94fba32512
jhardoin <jacob_ardoin@icloud.com>
2025-03-27 20:01:55 -0500

    Miles per gallon

--- kyu8/Miles per gallon to kilometers per liter.js
+++ kyu8/Miles per gallon to kilometers per liter.js
@@ -3,3 +3,7 @@ const converter = (mpg) => Number((mpg * 1.609344/4.54609188).toFixed(2));
 function converter (mpg) {
     return +(mpg/4.54609188 * 1.609344).toFixed(2)
   }
+
+  function converter (mpg) {
+    return +(mpg/4.54609188 * 1.609344).toFixed(2)
+  }
