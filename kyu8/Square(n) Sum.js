1cc2c09d94a279c2b5252ba0acc26b827bcf9fd5
jhardoin <jacob_ardoin@icloud.com>
2025-03-31 21:03:30 -0500

    Square n sum

--- kyu8/Square(n) Sum.js
+++ kyu8/Square(n) Sum.js
@@ -27,3 +27,7 @@ function squareSum(numbers){
 function squareSum(numbers){
   return numbers ? numbers.reduce((a,b) => a + b ** 2, 0) : 0
 }
+
+function squareSum(numbers){
+  return numbers ? numbers.reduce((a,b) => a + b ** 2, 0) : 0
+}
