98127784be3c2ad1af26c68b58f50623c443fe98
jhardoin <jacob_ardoin@icloud.com>
2025-04-02 11:44:24 -0500

    Get the mean of an array

--- kyu8/Get the mean of an array.js
+++ kyu8/Get the mean of an array.js
@@ -23,3 +23,7 @@ function getAverage(marks){
 function getAverage(marks){
   return Math.floor(marks.reduce((a,b) => a + b, 0)/marks.length)
 }
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((a,b) => a + b, 0)/marks.length)
+}
