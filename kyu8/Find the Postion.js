624bb23e1057e0397b1b7c14f1c22db6cf02a49c
jhardoin <jacob_ardoin@icloud.com>
2025-04-03 15:39:07 -0500

    Find the position

--- kyu8/Find the Postion.js
+++ kyu8/Find the Postion.js
@@ -3,3 +3,7 @@ const position = (letter) => `Position of alphabet: ${parseInt(letter, 36) - 9}`
 function position(letter){
     return `Position of alphabet: ${letter.charCodeAt() - 96}`
 }
+
+function position(letter){
+    return `Position of alphabet: ${letter.charCodeAt() - 96}`
+}
