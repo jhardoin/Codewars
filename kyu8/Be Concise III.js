775916ce4c4027c4b18cfdd94e194e7272b16b92
jhardoin <jacob_ardoin@icloud.com>
2025-04-04 16:45:41 -0500

    Be concise

--- kyu8/Be Concise III.js
+++ kyu8/Be Concise III.js
@@ -1,3 +1,5 @@
 const sumSquares = a => a.map(e => e ** 2).reduce((a,b) => a+b)
 
 const sumSquares = a => a.map(e => e ** 2).reduce((a,b) => a+b)
+
+const sumSquares = a => a.map(e => e ** 2).reduce((a,b) => a+b)
