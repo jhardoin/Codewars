d9361cf8febbda65b68f9070340b3863fe0aacec
jhardoin <jacob_ardoin@icloud.com>
2025-04-06 22:22:45 -0500

    Making 6 toast

--- kyu8/Basic making 6 toast.js
+++ kyu8/Basic making 6 toast.js
@@ -3,3 +3,7 @@ const sixToast = (num) => (num - 6 === 0) ? 0 : Math.abs(num - 6)
 function sixToast(num) {
     return Math.abs(num - 6)
   }
+
+  function sixToast(num) {
+    return Math.abs(num - 6)
+  }
