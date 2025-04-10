6aa038bbcb01b21a99380b66b400a918c3ef8943
jhardoin <jacob_ardoin@icloud.com>
2025-04-10 15:57:01 -0500

    Gravity Flip

--- kyu8/Gravity Flip.js
+++ kyu8/Gravity Flip.js
@@ -21,3 +21,7 @@ const flip=(d, a)=>{
   const flip=(d, a)=>{
     return d == 'R' ? a.sort((a,b) => a - b) : a.sort((a,b) => b - a)
   }
+
+  const flip=(d, a)=>{
+    return d == 'R' ? a.sort((a,b) => a - b) : a.sort((a,b) => b - a)
+  }
