08cb10623933f96e3f009f4b7048665f57da2529
jhardoin <jacob_ardoin@icloud.com>
2025-04-14 18:33:56 -0500

    if else

--- kyu8/Training Js 7 if else.js
+++ kyu8/Training Js 7 if else.js
@@ -9,3 +9,7 @@ function saleHotdogs(n){
   function saleHotdogs(n){
     return n < 5 ? n * 100 : n >= 5 && n < 10 ? 95 * n : 90 * n
   }
+
+  function saleHotdogs(n){
+    return n < 5 ? n * 100 : n >= 5 && n < 10 ? 95 * n : 90 * n
+  }
