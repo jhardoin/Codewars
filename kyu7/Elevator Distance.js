2162d753af7b07012d06726067eb2ca72dbe9283
jhardoin <jacob_ardoin@icloud.com>
2025-04-20 18:17:07 -0500

    Elevator distance

--- kyu7/Elevator Distance.js
+++ kyu7/Elevator Distance.js
@@ -13,3 +13,11 @@ function elevatorDistance(array) {
   }
    return count
 }
+
+function elevatorDistance(array) {
+  let count = 0
+  for(let i = 0; i < array.length - 1; i++){
+    count = count + Math.abs(array[i] - array[i + 1])
+  }
+   return count
+}
