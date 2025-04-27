3771be4c082f7d8f3969f11f8de41f2e24aa6437
jhardoin <jacob_ardoin@icloud.com>
2025-04-27 16:58:17 -0500

    Enumerable magic

--- kyu8/Enumerable Magic 4.js
+++ kyu8/Enumerable Magic 4.js
@@ -2,3 +2,8 @@ function none(arr, fun){
     if(arr.length == 0) return true
     return arr.every(item => !fun(item))
   }
+
+  function none(arr, fun){
+    if(arr.length == 0) return true
+    return arr.every(item => !fun(item))
+  }
