91fd5395b64daf423d72658b5c0019f4b43afb64
jhardoin <jacob_ardoin@icloud.com>
2025-05-02 16:58:32 -0500

    Do you speak english

--- kyu8/Do you speak English.js
+++ kyu8/Do you speak English.js
@@ -3,3 +3,7 @@ const spEng = (sentence) => sentence.toLowerCase().includes('english')
 function spEng(sentence){
     return sentence.toLowerCase().includes('english')
     }
+
+    function spEng(sentence){
+        return sentence.toLowerCase().includes('english')
+        }
