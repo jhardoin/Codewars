cf097521678d26263c196d39993e3fb2a58fb7a6
jhardoin <jacob_ardoin@icloud.com>
2025-05-11 18:03:40 -0500

    Quadratic Coefficients Solver

--- kyu8/Quadratic Coefficients Solver.js
+++ kyu8/Quadratic Coefficients Solver.js
@@ -3,3 +3,7 @@ const quadratic = (x1, x2) => [1, -x1 + -x2, x1 * x2]
 function quadratic(x1, x2){
     return [1, -(x2 + x1), (x1 * x2)]
  }
+
+ function quadratic(x1, x2){
+    return [1, -(x2 + x1), (x1 * x2)]
+ }
