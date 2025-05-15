130c14b7debc53e401fcdd9e7570d10c2999c06e
jhardoin <jacob_ardoin@icloud.com>
2025-05-15 06:10:16 -0500

    Pillars

--- kyu8/Pillars.js
+++ kyu8/Pillars.js
@@ -19,3 +19,7 @@ function pillars(numPill, dist, width) {
   function pillars(num_pill, dist, width) {
     return num_pill <= 1 ? 0 : (num_pill - 1) * dist * 100 + (num_pill - 2) * width;
   }
+
+  function pillars(num_pill, dist, width) {
+    return num_pill <= 1 ? 0 : (num_pill - 1) * dist * 100 + (num_pill - 2) * width;
+  }
