3cc6721b55a9f21fe139a514977f8d3eda3f51f3
jhardoin <jacob_ardoin@icloud.com>
2025-05-17 23:21:51 -0500

    All unique

--- kyu7/All unique.js
+++ kyu7/All unique.js
@@ -39,3 +39,7 @@ function hasUniqueChars(str){
 function hasUniqueChars(str){
   return [...new Set(str)].length === str.length
 }
+
+function hasUniqueChars(str){
+  return [...new Set(str)].length === str.length
+}
