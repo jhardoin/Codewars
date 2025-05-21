5e819514b14ce5c2b4a4745d1566dccb75a4cc22
jhardoin <jacob_ardoin@icloud.com>
2025-05-20 23:14:09 -0500

    How many are smaller than me

--- kyu7/How many are smaller than me.js
+++ kyu7/How many are smaller than me.js
@@ -17,3 +17,7 @@ function smaller(nums) {
 function smaller(nums) {
     return nums.map((e,i,a) => a.slice(i).filter(x => e > x).length)
 }
+
+function smaller(nums) {
+    return nums.map((e,i,a) => a.slice(i).filter(x => e > x).length)
+}
