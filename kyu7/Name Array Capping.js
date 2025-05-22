2b7b4716316171b5fd2be23814f8a222b2313780
jhardoin <jacob_ardoin@icloud.com>
2025-05-22 08:55:38 -0500

    Name array capping

--- kyu7/Name Array Capping.js
+++ kyu7/Name Array Capping.js
@@ -1,3 +1,5 @@
 const capMe = names => names.map(e => e[0].toUpperCase() + e.slice(1).toLowerCase())
 
 const capMe = names => names.map(e => e[0].toUpperCase() + e.slice(1).toLowerCase())
+
+const capMe = names => names.map(e => e[0].toUpperCase() + e.slice(1).toLowerCase())
