973f64bfe877c41852ae2954285afc06fe5e43f4
jhardoin <jacob_ardoin@icloud.com>
2025-05-23 14:31:47 -0500

    Remove duplicate words

--- kyu7/Remove duplicate words.js
+++ kyu7/Remove duplicate words.js
@@ -32,3 +32,7 @@ function removeDuplicateWords (s) {
 function removeDuplicateWords (s) {
   return [...new Set(s.split(' '))].join(' ')
 }
+
+function removeDuplicateWords (s) {
+  return [...new Set(s.split(' '))].join(' ')
+}
