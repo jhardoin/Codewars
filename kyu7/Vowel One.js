10b307df27bff34cb21a31c84deef54bb149102a
jhardoin <jacob_ardoin@icloud.com>
2025-05-25 20:49:38 -0500

    Vowel One

--- kyu7/Vowel One.js
+++ kyu7/Vowel One.js
@@ -22,3 +22,7 @@ function vowelOne(s){
 function vowelOne(s){
   return s.split('').map(e => 'aeiouAEIOU'.includes(e) ? 1 : 0).join('')
 }
+
+function vowelOne(s){
+  return s.split('').map(e => 'aeiouAEIOU'.includes(e) ? 1 : 0).join('')
+}
