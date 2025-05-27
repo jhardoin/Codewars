efa399a358d7d33475cf9c9f4475adbac3bd80bb
jhardoin <jacob_ardoin@icloud.com>
2025-05-26 21:13:12 -0500

    Return substring instance count

--- kyu7/Return substring instance count.js
+++ kyu7/Return substring instance count.js
@@ -1,3 +1,7 @@
 function solution(fullText, searchText){
     return fullText.split(searchText).length - 1
   }
+
+  function solution(fullText, searchText){
+    return fullText.split(searchText).length - 1
+  }
