5bb3a02c23f5c4f8a9df61dd75bafe99ce81c478
jhardoin <jacob_ardoin@icloud.com>
2025-06-01 22:01:37 -0500

    Ensure question

--- kyu8/Ensure Question.js
+++ kyu8/Ensure Question.js
@@ -9,3 +9,7 @@ function ensureQuestion(s) {
   function ensureQuestion(s) {
     return s.endsWith('?') ? s : `${s}?`
   }
+
+  function ensureQuestion(s) {
+    return s.endsWith('?') ? s : `${s}?`
+  }
