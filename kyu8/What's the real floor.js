fa9daf39d057500d2c3b02c8a219a9b30cd03925
jhardoin <jacob_ardoin@icloud.com>
2025-06-09 17:48:49 -0500

    Whats the real floor

--- kyu8/What's the real floor.js
+++ kyu8/What's the real floor.js
@@ -21,3 +21,7 @@ function getRealFloor(n) {
   function getRealFloor(n) {
     return n > 13 ? n - 2 : n > 0 ? n - 1 : n
   }
+
+  function getRealFloor(n) {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n
+  }
