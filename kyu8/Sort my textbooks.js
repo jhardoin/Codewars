1287769403552c224f991b0d45a9a33b68584c8a
jhardoin <jacob_ardoin@icloud.com>
2025-06-11 18:03:23 -0500

    Sort my textbooks

--- kyu8/Sort my textbooks.js
+++ kyu8/Sort my textbooks.js
@@ -2,3 +2,7 @@ function sorter(arr) {
     return arr.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1);
   }
   
+  function sorter(arr) {
+    return arr.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1);
+  }
+  
