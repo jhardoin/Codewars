7a5c1058d634fbd87a6dd836d6b5531375dd8ea9
jhardoin <jacob_ardoin@icloud.com>
2025-06-13 21:50:30 -0500

    Area or perimeter

--- kyu8/Area or Perimeter.js
+++ kyu8/Area or Perimeter.js
@@ -28,3 +28,7 @@ const areaOrPerimeter = function(l , w) {
 const areaOrPerimeter = function(l , w) {
   return l === w ? l * w : (2 * l) + (2 * w)
 };
+
+const areaOrPerimeter = function(l , w) {
+  return l === w ? l * w : (2 * l) + (2 * w)
+};
