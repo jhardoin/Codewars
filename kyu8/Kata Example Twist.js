ea41c317aa0f51ab93dcb63d516d7ae79452a4a1
jhardoin <jacob_ardoin@icloud.com>
2025-06-22 00:41:55 -0500

    Kata Example twist

--- kyu8/Kata Example Twist.js
+++ kyu8/Kata Example Twist.js
@@ -3,3 +3,5 @@ let websites = Array.from({length: 1000}).fill('codewars')
 var websites = Array.from({length:1000}, (e,i) => 'codewars')
 
 var websites = Array.from({length:1000}, (e,i) => 'codewars')
+
+var websites = Array.from({length:1000}, (e,i) => 'codewars')
