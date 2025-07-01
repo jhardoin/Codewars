595ebe25c2d50e5af7c5fe4c6842b0d0d15e2849
jhardoin <jacob_ardoin@icloud.com>
2025-06-30 23:49:58 -0500

    Filter out the geese

--- kyu8/Filter out the geese.js
+++ kyu8/Filter out the geese.js
@@ -41,3 +41,8 @@ function gooseFilter (birds) {
     var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
     return birds.filter(e => !geese.includes(e))
   }
+
+  function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(e => !geese.includes(e))
+  }
