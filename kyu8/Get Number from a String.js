03ec17c0308b21b0d9023434cd45c87c7d1a4e80
jhardoin <jacob_ardoin@icloud.com>
2025-07-02 23:46:16 -0500

    Get number from a string

--- kyu8/Get Number from a String.js
+++ kyu8/Get Number from a String.js
@@ -9,3 +9,7 @@ function getNumberFromString(s) {
    function getNumberFromString(s) {
     return +[...s].filter(e => e >= '0' && e <= '9').join('')
    }
+
+   function getNumberFromString(s) {
+    return +[...s].filter(e => e >= '0' && e <= '9').join('')
+   }
