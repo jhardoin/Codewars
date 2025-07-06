77005a177a193a0c37d330b34f03868e15fd3ef0
jhardoin <jacob_ardoin@icloud.com>
2025-07-05 22:29:11 -0500

    Bumps in the road

--- kyu7/Bumps in the road.js
+++ kyu7/Bumps in the road.js
@@ -25,3 +25,8 @@ function bump(x){
   let bumps = x.split('').filter(e => e === 'n').length
   return bumps <= 15 ? 'Woohoo!' : 'Car Dead'
 }
+
+function bump(x){
+  let bumps = x.split('').filter(e => e === 'n').length
+  return bumps <= 15 ? 'Woohoo!' : 'Car Dead'
+}
