947cfded0f19dd6b45dc3f16763c9321721465db
jhardoin <jacob_ardoin@icloud.com>
2025-07-08 22:22:26 -0500

    Square every digit

--- kyu7/Square every digit.js
+++ kyu7/Square every digit.js
@@ -45,3 +45,7 @@ function squareDigits(num){
 function squareDigits(num){
   return +num.toString().split('').map(e => e ** 2).join('')
 }
+
+function squareDigits(num){
+  return +num.toString().split('').map(e => e ** 2).join('')
+}
