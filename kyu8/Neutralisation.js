75b098dd62059b96cb634daa00aa3657a483494b
jhardoin <jacob_ardoin@icloud.com>
2025-07-13 22:09:52 -0500

    Neutralisation

--- kyu8/Neutralisation.js
+++ kyu8/Neutralisation.js
@@ -9,3 +9,7 @@ function neutralise(s1, s2) {
   function neutralise(s1, s2) {
     return [...s1].map((e,i) => e == s2[i] ? e : 0).join('')
   }
+
+  function neutralise(s1, s2) {
+    return [...s1].map((e,i) => e == s2[i] ? e : 0).join('')
+  }
