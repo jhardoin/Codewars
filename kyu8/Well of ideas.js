c38f1208d25d362771d9409c7137f65fd4ba165b
jhardoin <jacob_ardoin@icloud.com>
2025-07-18 21:59:26 -0500

    Well of ideas

--- kyu8/Well of ideas.js
+++ kyu8/Well of ideas.js
@@ -21,3 +21,7 @@ function well(x){
   function well(x){
     return x.filter(e => e == 'good').length > 2 ? 'I smell a series!' : x.filter(e => e == 'good').length > 0 ? 'Publish!' : 'Fail!'
   }
+
+  function well(x){
+    return x.filter(e => e == 'good').length > 2 ? 'I smell a series!' : x.filter(e => e == 'good').length > 0 ? 'Publish!' : 'Fail!'
+  }
