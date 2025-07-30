b18b1a883623b37001a22566aff1c36fc71ddd47
jhardoin <jacob_ardoin@icloud.com>
2025-07-29 21:25:15 -0500

    Find multiples of a number

--- kyu8/Find Multiples of a Number.js
+++ kyu8/Find Multiples of a Number.js
@@ -41,3 +41,7 @@ function findMultiples(integer, limit) {
     return Array.from({length:Math.floor(limit/integer)}, (_, i) => (i + 1) * integer)
   }
   
+  function findMultiples(integer, limit) {
+    return Array.from({length:Math.floor(limit/integer)}, (_, i) => (i + 1) * integer)
+  }
+  
