030ff239a46b9957794c83703cac59920b58a4f1
jhardoin <jacob_ardoin@icloud.com>
2025-07-30 13:20:46 -0500

    Find the next perfect square

--- kyu7/Find the next perfect square.js
+++ kyu7/Find the next perfect square.js
@@ -25,3 +25,8 @@ function findNextSquare(sq) {
 function findNextSquare(sq) {
   return Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2)
 }
+
+
+function findNextSquare(sq) {
+  return Math.sqrt(sq) % 1 !== 0 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2)
+}
