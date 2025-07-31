79e832e0bf5241733a829aaf7316bb49a16d0cc1
jhardoin <jacob_ardoin@icloud.com>
2025-07-31 16:44:56 -0500

    Reverse words

--- kyu8/Reversed Words.js
+++ kyu8/Reversed Words.js
@@ -27,3 +27,7 @@ function reverseWords(str){
   function reverseWords(str){
     return str.split(' ').reverse().join(' ')
   }
+
+  function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+  }
