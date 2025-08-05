5d782fa9bf4f9b0000cf0f54fc10267c67a4fb6e
jhardoin <jacob_ardoin@icloud.com>
2025-08-05 16:51:51 -0500

    Simple Multiplication

--- kyu8/Simple Multiplication.js
+++ kyu8/Simple Multiplication.js
@@ -34,3 +34,7 @@ function simpleMultiplication(number) {
 function simpleMultiplication(number) {
   return number % 2 === 0 ? number * 8 : number * 9
 }
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9
+}
