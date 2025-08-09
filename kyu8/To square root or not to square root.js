07de1a4b1dc79d2269127dea8b90c2506b1aa24f
jhardoin <jacob_ardoin@icloud.com>
2025-08-09 16:44:34 -0500

    Square root

--- kyu8/To square root or not to square root.js
+++ kyu8/To square root or not to square root.js
@@ -13,3 +13,7 @@ function squareOrSquareRoot(array) {
   function squareOrSquareRoot(array) {
     return array.map(e => Math.sqrt(e) % 1 == 0 ? Math.sqrt(e) : e ** 2)
   }
+
+  function squareOrSquareRoot(array) {
+    return array.map(e => Math.sqrt(e) % 1 == 0 ? Math.sqrt(e) : e ** 2)
+  }
