54bb6f64be3490003e3faf5d9b34bec41904b2b2
jhardoin <jacob_ardoin@icloud.com>
2025-08-09 16:45:21 -0500

    Twice as old

--- kyu8/Twice as old.js
+++ kyu8/Twice as old.js
@@ -29,3 +29,7 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(sonYearsOld * 2 - dadYearsOld)
 }
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(sonYearsOld * 2 - dadYearsOld)
+}
